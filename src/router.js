0da416a7e51d510525eb1f9ee79e0f364f535c0e
Pirijan <pirijan@gmail.com>
2019-05-17 12:10:21 -0400

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,17 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+// import Home from './views/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    }
-  ]
+  base: process.env.BASE_URL
+  // routes: [
+  //   {
+  //     path: '/',
+  //     name: 'home',
+  //     component: Home
+  //   }
+  // ]
 })
