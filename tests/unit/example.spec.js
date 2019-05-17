dd2430a4ccd50dfd900578ff8920ff5287fbe859
Pirijan <pirijan@gmail.com>
2019-05-17 11:44:05 -0400

    fix test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import Space from '@/components/Space.vue'
 
-describe('HelloWorld.vue', () => {
+describe('Space.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(Space, {
       propsData: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
