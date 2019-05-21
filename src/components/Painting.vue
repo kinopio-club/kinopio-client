002b3b7845f0d36ab9e337b27658bf1bda28d98d
Pirijan <pirijan@gmail.com>
2019-05-21 11:35:22 -0400

    Update Painting.vue

--- src/components/Painting.vue
+++ src/components/Painting.vue
@@ -1,12 +1,37 @@
 <template lang="pug">
-  canvas#painting
+  canvas#painting.painting
 </template>
 
 <script>
 export default {
-  name: 'Painting'
+  name: 'Painting',
+  data () {
+    return {
+      initialBrushSize: 25,
+      maxBrushSize: 20,
+      consecutiveBrushSizeMultiplier: 0.05,
+      canvas: undefined,
+      context: undefined
+    }
+  },
+  mounted () {
+    this.canvas = document.getElementById('painting')
+    this.context = this.canvas.getContext('2d')
+    this.updateCanvasSize()
+    window.addEventListener('resize', this.updateCanvasSize)
+  },
+  methods: {
+    updateCanvasSize () {
+      this.canvas.width = window.innerWidth
+      this.canvas.height = window.innerHeight
+      this.context.clearRect(0, 0, window.innerWidth, window.innerHeight)
+      console.log('🌹', this.canvas, this.context)
+    }
+  }
 }
 </script>
 
 <style lang="stylus">
+.painting
+  background-color pink
 </style>
