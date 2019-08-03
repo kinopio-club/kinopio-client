be1699eb32c6005005cde552d9da3acfceb7eca4
Pirijan <pirijan@gmail.com>
2019-08-03 19:01:44 -0400

    route change
    legal(-stuff?) for eventual terms and privacy policy

--- src/router.js
+++ src/router.js
@@ -14,12 +14,12 @@ export default new Router({
       component: Space
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/legal',
+      name: 'legal',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "newsletter" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "newsletter" */ './views/Legal.vue')
     }
   ]
 })
---
+++ src/views/Legal.vue
@@ -0,0 +1,49 @@
+<template lang='pug'>
+.meta-page
+  h1 legal stuff
+  p covers terms of service, privacy policy, maybe grdp
+  // https://overcast.fm/privacy and /terms
+  // yuck example https://glitch.com/legal/#privacy
+</template>
+
+<script>
+export default {
+  // data () {
+  //   return {
+  //     notSent: true
+  //   }
+  // },
+  // methods: {
+  //   sendForm () {
+  //     this.notSent = false
+  //   }
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus">
+.meta-page
+  max-width 600px
+  width 95%
+  margin 8px
+  // margin-top 60px
+  // position absolute
+  // pointer-events none
+  // img
+  //   max-width 100%
+  //   border-radius: 3px
+  // form
+  //   padding 8px
+  //   background-color var(--secondary-background)
+  //   border-radius 3px
+  //   display inline-card
+  // input
+  //   margin-right 5px
+  // a,
+  // form,
+  // iframe
+  //   pointer-events all
+  // .iframe-wrap
+  //   max-width 100% !important
+</style>
