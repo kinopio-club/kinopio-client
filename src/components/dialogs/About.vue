1bbe1c5da56cc3176a7e18ce5cd7d0d800f3e19f
Pirijan <pirijan@gmail.com>
2019-08-15 17:06:22 -0400

    stub changelog / new stuff dialog

--- src/components/dialogs/About.vue
+++ src/components/dialogs/About.vue
@@ -9,17 +9,26 @@ dialog.about(v-if="visible" :open="visible" @click="closeDialogs")
     .button-wrap
       button(@click.stop="toggleBetaNotesIsVisible" :class="{active: betaNotesIsVisible}") Beta Notes
       BetaNotes(:visible="betaNotesIsVisible")
+  section
+    .button-wrap
+      button(@click.stop="toggleNewStuffIsVisible" :class="{active: newStuffIsVisible}")
+        span New Stuff
+        template(v-if="newStuffIsNew")
+          img.newStuff-status(src="@/assets/new.gif")
+      NewStuff(:visible="newStuffIsVisible" :newStuff="newStuff")
 </template>
 
 <script>
 import Feedback from '@/components/dialogs/Feedback.vue'
 import BetaNotes from '@/components/dialogs/BetaNotes.vue'
+import NewStuff from '@/components/dialogs/NewStuff.vue'
 
 export default {
   name: 'About',
   components: {
     Feedback,
-    BetaNotes
+    BetaNotes,
+    NewStuff
   },
   props: {
     visible: Boolean
@@ -27,7 +36,10 @@ export default {
   data () {
     return {
       feedbackIsVisible: false,
-      betaNotesIsVisible: false
+      betaNotesIsVisible: false,
+      newStuffIsVisible: false,
+      newStuffIsNew: true,
+      newStuff: []
     }
   },
   created () {
@@ -37,6 +49,9 @@ export default {
         this.betaNotesIsVisible = false
       }
     })
+  },
+  mounted () {
+    this.getNewStuffs()
   },
   methods: {
     toggleFeedbackIsVisible () {
@@ -49,9 +64,21 @@ export default {
       this.closeDialogs()
       this.betaNotesIsVisible = !isVisible
     },
+    toggleNewStuffIsVisible () {
+      const isVisible = this.newStuffIsVisible
+      this.closeDialogs()
+      this.newStuffIsVisible = !isVisible
+    },
+    getNewStuff () {
+      console.log('🌹 fetch are.na and (normalize?) and then pass it through to newStuffs, and then see if its new news')
+      // https://api.are.na/v2/channels/kinopio-new-stuff/contents
+      // last item index is most recent (maybe i can flip the order on the board tho, then i can use per param)
+      // otherwise i'll just slice the result from the end and maybe array flip it on my end before doing stuff w it
+    },
     closeDialogs () {
       this.feedbackIsVisible = false
       this.betaNotesIsVisible = false
+      this.newStuffIsVisible = false
     }
   }
 }
@@ -64,4 +91,7 @@ export default {
     display none
   .kaomoji-section
     padding-top 14px
+  .newStuff-status
+    vertical-align -1px
+    margin-left 3px
 </style>
---
+++ src/components/dialogs/NewStuff.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+dialog.changelog(v-if="visible" :open="visible" @click.stop)
+  section
+    p changelogggg
+</template>
+
+<script>
+export default {
+  name: 'Changelog',
+  props: {
+    visible: Boolean,
+    newStuff: Array
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="stylus">
+.changelog
+  overflow auto
+  max-height calc(100vh - 110px)
+  // img
+  //   border-radius 3px
+  // ul
+  //   margin 0
+  //   margin-top 2px
+  //   padding-left 15px
+  //   list-style-type square
+  //   li
+  //     padding-top 10px
+  //     margin-left 5px
+  //     user-select text
+  @media(max-width 340px)
+    left calc(-100% + 16px)
+    max-height calc(100vh - 200px)
+
+</style>
