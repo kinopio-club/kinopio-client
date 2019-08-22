537a147bc42c654797ed88c0cbc65bcfc3d3a9d6
Pirijan <pirijan@gmail.com>
2019-08-22 17:04:01 -0400

    garden-leaf asset

--- src/components/CardFrames.vue
+++ src/components/CardFrames.vue
@@ -2,11 +2,12 @@
   img.garden-leaf(src="@/assets/garden-leaf.gif")
 </template>
 
-<style lang="stylus">
+<style lang="stylus" scoped>
+img
+  position absolute
 .garden-leaf
   left -13px
   top 5px
-  position absolute
   width 16px
   transform rotate(15deg)
 </style>
