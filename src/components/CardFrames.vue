7eaa6edf90d6129667b827ccc881be73e0e4e6c8
Pirijan <pirijan@gmail.com>
2019-08-26 12:21:57 -0400

    garden frame assets

--- src/components/CardFrames.vue
+++ src/components/CardFrames.vue
@@ -1,13 +1,26 @@
 <template lang="pug">
-  img.garden-leaf(src="@/assets/garden-leaf.gif")
+aside.garden-leaf
+  img.leaves(src="@/assets/frames/garden/leaves.png")
+  img.ivy(src="@/assets/frames/garden/ivy.png")
+  img.flower(src="@/assets/frames/garden/flower.png")
+
 </template>
 
 <style lang="stylus" scoped>
+// aside
+//   pointer-events none
 img
   position absolute
 .garden-leaf
-  left -13px
-  top 5px
-  width 16px
-  transform rotate(15deg)
+  .leaves
+    left -20px
+    width 25px
+  .ivy
+    right -23px
+    top -5px
+    width 32px
+  .flower
+    width 20px
+    top -10px
+    right 22px
 </style>
