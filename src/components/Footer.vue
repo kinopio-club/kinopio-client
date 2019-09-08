be1ac85c06aee940417e2b69d9c1b1b54c0d6f02
Pirijan <pirijan@gmail.com>
2019-09-08 19:18:00 -0400

    stub undo button

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
 footer(v-if="!cardDialogsVisible")
-  span Beta {{buildHash}}
+  //span Beta {{buildHash}}
+  .botton-wrap
+    button Undo
 </template>
 
 <script>
@@ -10,16 +12,16 @@ export default {
     console.log('🐢 kinopio-client', this.buildHash)
   },
   computed: {
-    buildHash () {
-      const regex = /(app\.)([a-z0-9])\w+/
-      const scripts = Array.from(document.querySelectorAll('script'))
-      const path = scripts.find(script => {
-        const src = script.src
-        return src.includes('app')
-      })
-      let hash = path.src.match(regex)[0] // app.768db305407f4c847d44
-      return hash.replace('app.', '') // 768db305407f4c847d44
-    },
+    // buildHash () {
+    //   const regex = /(app\.)([a-z0-9])\w+/
+    //   const scripts = Array.from(document.querySelectorAll('script'))
+    //   const path = scripts.find(script => {
+    //     const src = script.src
+    //     return src.includes('app')
+    //   })
+    //   let hash = path.src.match(regex)[0] // app.768db305407f4c847d44
+    //   return hash.replace('app.', '') // 768db305407f4c847d44
+    // },
     cardDialogsVisible () {
       return Boolean(this.$store.state.cardDetailsIsVisibleForCard || this.$store.state.multipleCardActionsIsVisible)
     }
@@ -34,13 +36,13 @@ footer
   right 8px
   bottom 8px
   pointer-events none
-  // > .button-wrap
-  //   pointer-events all
-  //   margin-left 6px
-  //   display inline-block
-  //   dialog
-  //     left initial
-  //     right 8px
-  //     top initial
-  //     bottom calc(100% - 4px)
+  > .button-wrap
+    pointer-events all
+    margin-left 6px
+    display inline-block
+    dialog
+      left initial
+      right 8px
+      top initial
+      bottom calc(100% - 4px)
 </style>
