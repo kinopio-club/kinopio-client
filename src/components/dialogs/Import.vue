763370d2aa0550f3756a0acef8097cf5680f897b
Pirijan <pirijan@gmail.com>
2019-09-12 13:28:13 -0400

    stub import dialog

---
+++ src/components/dialogs/Import.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+dialog.narrow(v-if="visible" :open="visible" @click.stop ref="dialog")
+  section
+    p Import
+  section
+    p To paste into other apps
+    button
+      span.badge txt
+      span Card Names
+</template>
+
+<script>
+import scrollIntoView from 'smooth-scroll-into-view-if-needed' // polyfil
+
+export default {
+  name: 'Import',
+  props: {
+    visible: Boolean
+  },
+  methods: {
+    scrollIntoView () {
+      const element = this.$refs.dialog
+      scrollIntoView(element, {
+        behavior: 'smooth',
+        scrollMode: 'if-needed'
+      })
+    }
+  },
+  watch: {
+    visible (visible) {
+      this.$nextTick(() => {
+        if (visible) {
+          this.scrollIntoView()
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
