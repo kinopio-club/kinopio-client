8a7afd82ff80fc6aa868b39aada37e501373b434
Pirijan <pirijan@gmail.com>
2019-09-13 11:03:36 -0400

    add valid-space parsing , errors, and importing

--- src/components/dialogs/Import.vue
+++ src/components/dialogs/Import.vue
@@ -3,18 +3,26 @@ dialog.import.narrow(v-if="visible" :open="visible" @click.stop ref="dialog")
   section
     p Import Space
   section
-    p Upload an exported Kinopio space file
+    p Upload an exported space backup file
     button(@click="selectFile")
       span.badge json
       span Select file
     Loader(:visible="loading")
     input.hidden(type="file" ref="input" accept=".json" @change="readFile")
+
+    .errors(v-if="errors.length")
+      span.badge.danger File Errors
+      ul
+        li(v-for="(error in errors") {{error}}
+
 </template>
 
 <script>
 import scrollIntoView from 'smooth-scroll-into-view-if-needed' // polyfil
+import nanoid from 'nanoid'
 
 import Loader from '@/components/Loader.vue'
+import cache from '@/cache.js'
 
 export default {
   name: 'Import',
@@ -26,8 +34,8 @@ export default {
   },
   data () {
     return {
-      loading: false
-      // spaceParsingErrors: []
+      loading: false,
+      errors: []
     }
   },
   methods: {
@@ -48,13 +56,53 @@ export default {
         this.importSpace(space)
       }
     },
+    uniqueName (space) {
+      const spaces = cache.getAllSpaces()
+      const spaceNames = spaces.map(space => space.name)
+      if (spaceNames.includes(space.name)) {
+        return `${space.name}-${space.id}`
+      } else {
+        return space.name
+      }
+    },
     importSpace (space) {
-      // if (this.isValidSpace(space)) {} else (show user err)
-      console.log(space) // randomize id
+      if (this.isValidSpace(space)) {
+        space.id = nanoid()
+        space.name = this.uniqueName(space)
+        cache.saveSpace(space)
+        this.$store.dispatch('currentSpace/changeSpace', space)
+        this.$emit('shouldUpdateSpaces')
+        this.$emit('closeDialog')
+      }
+    },
+    typeCheck (value, type) {
+      if (type === 'array') {
+        return Array.isArray(value)
+      } else {
+        return typeof value === type // eslint-disable-line valid-typeof
+      }
     },
     isValidSpace (space) {
-
-      // check the space and return true if space has all core fields / types req'd
+      this.errors = []
+      const schema = {
+        'name': 'string',
+        'users': 'array',
+        'cards': 'array',
+        'connections': 'array',
+        'connectionTypes': 'array',
+        'customFields': 'array'
+      }
+      Object.keys(schema).forEach(field => {
+        if (!this.typeCheck(space[field], schema[field])) {
+          let error = `Expected ${field} but didn't get a ${schema[field]}`
+          this.errors.push(error)
+        }
+      })
+      if (this.errors.length) {
+        return false
+      } else {
+        return true
+      }
     },
     scrollIntoView () {
       const element = this.$refs.dialog
@@ -82,4 +130,17 @@ export default {
     display none
   .loader
     margin-left 6px
+  .errors
+    margin-top 10px
+  ul
+    margin 0
+    margin-top 2px
+    padding-left 15px
+    li
+      padding-top 10px
+      margin-left 5px
+      user-select text
+  ul
+    list-style-type square
+
 </style>
