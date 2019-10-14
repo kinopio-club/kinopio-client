8fde766b53316b82b7e4072ca2c9da9393d6e92d
Pirijan <pirijan@gmail.com>
2019-10-14 17:29:55 -0400

    remove unused routes
    using metapage (or notion) instead

--- src/router.js
+++ src/router.js
@@ -12,14 +12,14 @@ export default new Router({
       path: '/',
       name: 'space',
       component: Space
-    },
-    {
-      path: '/legal',
-      name: 'legal',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "newsletter" */ './views/Legal.vue')
     }
+    // {
+    //   path: '/legal',
+    //   name: 'legal',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "legal" */ './views/Legal.vue')
+    // }
   ]
 })
