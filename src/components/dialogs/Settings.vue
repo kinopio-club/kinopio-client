0298fc0b25e5939300b270b911972debecea4cfa
Pirijan <hi@pirijan.com>
2019-11-13 14:17:28 -0500

    move 'remove all' into settings

--- src/components/dialogs/Settings.vue
+++ src/components/dialogs/Settings.vue
@@ -1,44 +1,67 @@
 <template lang="pug">
-dialog.settings.narrow(v-if="visible" :open="visible")
+dialog.settings.narrow(v-if="visible" :open="visible" @click.stop)
   section
-    p yo
+    button(v-if="!removeAllConfirmationVisible" @click="toggleRemoveAllConfirmationVisible")
+      img.icon(src="@/assets/remove.svg")
+      span Remove All Your Data
+    span(v-if="removeAllConfirmationVisible")
+      p
+        span.badge.danger Permanently remove
+        span(v-if="isSignedIn") all your spaces and user data from this computer and Kinopio's servers?
+        span(v-else) all your spaces and user data from this computer?
+      .segmented-buttons
+        button(@click="toggleRemoveAllConfirmationVisible")
+          span Cancel
+        button.danger(@click="removeUserPermanent")
+          img.icon(src="@/assets/remove.svg")
+          span Remove All
+          Loader(:visible="loading.removeUserPermanent")
 
 </template>
 
 <script>
+import api from '@/api.js'
+import Loader from '@/components/Loader.vue'
+
 export default {
   name: 'Settings',
+  components: {
+    Loader
+  },
   props: {
     visible: Boolean
   },
   data () {
     return {
-      xyz: []
+      removeAllConfirmationVisible: false,
+      loading: {
+        removeUserPermanent: false
+      }
     }
   },
-  // created () {
-  //   this.$store.subscribe((mutation, state) => {
-  //     if (mutation.type === 'closeAllDialogs') {
-  //       this.supportIsVisible = false
-  //       this.betaNotesIsVisible = false
-  //       this.whatsNewIsVisible = false
-  //     }
-  //   })
-  // },
+  computed: {
+    isSignedIn () {
+      return this.$store.getters['currentUser/isSignedIn']
+    }
+  },
   methods: {
-    // closeDialogs () {
-    //   this.supportIsVisible = false
-    //   this.betaNotesIsVisible = false
-    //   this.whatsNewIsVisible = false
-    // }
+    toggleRemoveAllConfirmationVisible () {
+      this.removeAllConfirmationVisible = !this.removeAllConfirmationVisible
+    },
+    async removeUserPermanent () {
+      this.loading.removeUserPermanent = true
+      await api.removeUserPermanent()
+      this.loading.removeUserPermanent = false
+      this.$emit('removeUser')
+    }
+  },
+  watch: {
+    visible (value) {
+      if (value) {
+        this.removeAllConfirmationVisible = false
+      }
+    }
   }
-  // watch: {
-  //   visible (visible) {
-  //     if (visible) {
-  //       this.checkNewStuffIsUpdated(this.newStuff[0].id)
-  //     }
-  //   }
-  // }
 }
 </script>
 
--- src/components/dialogs/UserDetails.vue
+++ src/components/dialogs/UserDetails.vue
@@ -16,41 +16,22 @@ dialog.narrow.user-details(
   section(v-if="isCurrentUser")
     .button-wrap
       button(@click.stop="toggleSettingsIsVisible" :class="{active: settingsIsVisible}") Settings
-      Settings(:visible="settingsIsVisible")
+      Settings(:visible="settingsIsVisible" @removeUser="signOut")
 
     button(v-if="isSignedIn" @click="signOut") Sign Out
 
-    //- button(v-if="!removeAllConfirmationVisible" @click="toggleRemoveAllConfirmationVisible")
-    //-   img.icon(src="@/assets/remove.svg")
-    //-   span Remove All Your Data
-    //- span(v-if="removeAllConfirmationVisible")
-    //-   p
-    //-     span.badge.danger Permanently remove
-    //-     span(v-if="isSignedIn") all your spaces and user data from this computer and Kinopio's servers?
-    //-     span(v-else) all your spaces and user data from this computer?
-    //-   .segmented-buttons
-    //-     button(@click="toggleRemoveAllConfirmationVisible")
-    //-       span Cancel
-    //-     button.danger(@click="removeUserPermanent")
-    //-       img.icon(src="@/assets/remove.svg")
-    //-       span Remove All
-    //-       Loader(:visible="loading.removeUserPermanent")
 </template>
 
 <script>
 import ColorPicker from '@/components/dialogs/ColorPicker.vue'
 import Settings from '@/components/dialogs/Settings.vue'
-
 import cache from '@/cache.js'
-import api from '@/api.js'
-import Loader from '@/components/Loader.vue'
 
 export default {
   name: 'UserDetails',
   components: {
     ColorPicker,
-    Settings,
-    Loader
+    Settings
   },
   props: {
     user: Object,
@@ -60,11 +41,7 @@ export default {
   data () {
     return {
       colorPickerIsVisible: false,
-      // removeAllConfirmationVisible: false,
-      settingsIsVisible: false,
-      loading: {
-        removeUserPermanent: false
-      }
+      settingsIsVisible: false
     }
   },
   computed: {
@@ -115,26 +92,15 @@ export default {
         newColor
       })
     },
-    toggleRemoveAllConfirmationVisible () {
-      this.removeAllConfirmationVisible = !this.removeAllConfirmationVisible
-    },
     signOut () {
       cache.removeAll()
       location.reload()
-    },
-    async removeUserPermanent () {
-      this.loading.removeUserPermanent = true
-      await api.removeUserPermanent()
-      this.loading.removeUserPermanent = false
-      this.signOut()
     }
-
   },
   watch: {
     visible (value) {
       if (value) {
         this.colorPickerIsVisible = false
-        this.removeAllConfirmationVisible = false
       }
     }
   }
