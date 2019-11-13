ca8ddff4919e6efd66de4c98ff591e601ed5cfdc
Pirijan <hi@pirijan.com>
2019-11-13 13:56:20 -0500

    stub settings dialog

---
+++ src/components/dialogs/Settings.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+dialog.settings.narrow(v-if="visible" :open="visible")
+  section
+    p yo
+
+</template>
+
+<script>
+export default {
+  name: 'Settings',
+  props: {
+    visible: Boolean
+  },
+  data () {
+    return {
+      xyz: []
+    }
+  },
+  // created () {
+  //   this.$store.subscribe((mutation, state) => {
+  //     if (mutation.type === 'closeAllDialogs') {
+  //       this.supportIsVisible = false
+  //       this.betaNotesIsVisible = false
+  //       this.whatsNewIsVisible = false
+  //     }
+  //   })
+  // },
+  methods: {
+    // closeDialogs () {
+    //   this.supportIsVisible = false
+    //   this.betaNotesIsVisible = false
+    //   this.whatsNewIsVisible = false
+    // }
+  }
+  // watch: {
+  //   visible (visible) {
+  //     if (visible) {
+  //       this.checkNewStuffIsUpdated(this.newStuff[0].id)
+  //     }
+  //   }
+  // }
+}
+</script>
+
+<style lang="stylus">
+.about
+  top calc(100% - 8px)
+  .hidden
+    display none
+  .kaomoji-section
+    padding-top 14px
+  .updated
+    margin 0
+    margin-left 3px
+</style>
--- src/components/dialogs/UserDetails.vue
+++ src/components/dialogs/UserDetails.vue
@@ -2,7 +2,7 @@
 dialog.narrow.user-details(
   v-if="visible"
   :open="visible"
-  @click="closeColorPicker"
+  @click="closeDialogs"
   :class="{'right-side': detailsOnRight}"
 )
   section(v-if="isCurrentUser")
@@ -17,25 +17,31 @@ dialog.narrow.user-details(
     button(@click="signOut") Sign Out
 
   section(v-if="isCurrentUser")
-    button(v-if="!removeAllConfirmationVisible" @click="toggleRemoveAllConfirmationVisible")
-      img.icon(src="@/assets/remove.svg")
-      span Remove All Your Data
-    span(v-if="removeAllConfirmationVisible")
-      p
-        span.badge.danger Permanently remove
-        span(v-if="isSignedIn") all your spaces and user data from this computer and Kinopio's servers?
-        span(v-else) all your spaces and user data from this computer?
-      .segmented-buttons
-        button(@click="toggleRemoveAllConfirmationVisible")
-          span Cancel
-        button.danger(@click="removeUserPermanent")
-          img.icon(src="@/assets/remove.svg")
-          span Remove All
-          Loader(:visible="loading.removeUserPermanent")
+    .button-wrap
+      button(@click.stop="toggleSettingsIsVisible" :class="{active: settingsIsVisible}") Settings
+      Settings(:visible="settingsIsVisible")
+
+    //- button(v-if="!removeAllConfirmationVisible" @click="toggleRemoveAllConfirmationVisible")
+    //-   img.icon(src="@/assets/remove.svg")
+    //-   span Remove All Your Data
+    //- span(v-if="removeAllConfirmationVisible")
+    //-   p
+    //-     span.badge.danger Permanently remove
+    //-     span(v-if="isSignedIn") all your spaces and user data from this computer and Kinopio's servers?
+    //-     span(v-else) all your spaces and user data from this computer?
+    //-   .segmented-buttons
+    //-     button(@click="toggleRemoveAllConfirmationVisible")
+    //-       span Cancel
+    //-     button.danger(@click="removeUserPermanent")
+    //-       img.icon(src="@/assets/remove.svg")
+    //-       span Remove All
+    //-       Loader(:visible="loading.removeUserPermanent")
 </template>
 
 <script>
 import ColorPicker from '@/components/dialogs/ColorPicker.vue'
+import Settings from '@/components/dialogs/Settings.vue'
+
 import cache from '@/cache.js'
 import api from '@/api.js'
 import Loader from '@/components/Loader.vue'
@@ -44,6 +50,7 @@ export default {
   name: 'UserDetails',
   components: {
     ColorPicker,
+    Settings,
     Loader
   },
   props: {
@@ -54,7 +61,8 @@ export default {
   data () {
     return {
       colorPickerIsVisible: false,
-      removeAllConfirmationVisible: false,
+      // removeAllConfirmationVisible: false,
+      settingsIsVisible: false,
       loading: {
         removeUserPermanent: false
       }
@@ -88,11 +96,19 @@ export default {
     }
   },
   methods: {
-    toggleColorPicker () {
-      this.colorPickerIsVisible = !this.colorPickerIsVisible
+    toggleSettingsIsVisible () {
+      const isVisible = this.settingsIsVisible
+      this.closeDialogs()
+      this.settingsIsVisible = !isVisible
     },
-    closeColorPicker () {
+    toggleColorPicker () {
+      const isVisible = this.colorPickerIsVisible
+      this.closeDialogs()
+      this.colorPickerIsVisible = !isVisible
+    },
+    closeDialogs () {
       this.colorPickerIsVisible = false
+      this.settingsIsVisible = false
     },
     updateUserColor (newColor) {
       this.$store.dispatch('updateUserColor', {
