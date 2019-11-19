7a3d226f6d3e69a6462d4902b55d8731098b9739
Pirijan <hi@pirijan.com>
2019-11-19 09:51:12 -0500

    add beta notification

--- src/router.js
+++ src/router.js
@@ -21,6 +21,7 @@ export default new Router({
       component: Space,
       beforeEnter: (to, from, next) => {
         store.commit('isBeta', true)
+        store.commit('addNotification', { message: 'No features currently in Beta' })
         next()
       }
       // redirect: to => {
