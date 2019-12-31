9347faad9fbbe9411d72ab1e2ac9ed52d8707db6
Pirijan <hi@pirijan.com>
2019-12-31 11:45:12 -0500

    replace roadmap w link

--- src/components/dialogs/About.vue
+++ src/components/dialogs/About.vue
@@ -13,7 +13,7 @@ dialog.about(v-if="visible" :open="visible" @click="closeDialogs")
   section
     .button-wrap
       button(@click.stop="toggleWhatsNewIsVisible" :class="{active: whatsNewIsVisible}")
-        span What's New?
+        span What's New
         img.updated.icon(src="@/assets/updated.gif" v-if="newStuffIsUpdated")
       WhatsNew(:visible="whatsNewIsVisible" :newStuff="newStuff")
 
