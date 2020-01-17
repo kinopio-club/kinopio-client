e845c019363d9dc719ab3258e3aab81f498ef95d
Pirijan <hi@pirijan.com>
2020-01-17 14:51:51 -0500

    add ios install instructions

--- src/components/dialogs/AddToHomescreen.vue
+++ src/components/dialogs/AddToHomescreen.vue
@@ -1,14 +1,25 @@
 <template lang="pug">
 dialog.add-to-homescreen(v-if="visible" :open="visible" @click.stop)
-  //- span(v-if="isIOS")
   section
-    p Add to Homescreen
-  section
-    p sldkf
-    p
-      .badge.info yo
-    p fdjskl sdf
+    //- span(v-if="isIOS")
+    p [img]
+    //- icon in frame for ios
+    //- just icon for android
+    p Kinopio is a web-app which you can add directly to your iPhone or iPad
+      ol
+        li
+          span Tap the Share button
+          .badge.info
+            img.icon(src="@/assets/share.svg")
+          span at the bottom of the screen
+        li
+          span Then tap
+            .badge.info
+              span Add to Home Screen
+              img.icon.add(src="@/assets/add.svg")
+      p (You might need to scroll down)
 
+  //- section isAndroid
 </template>
 
 <script>
@@ -29,8 +40,6 @@ export default {
     this.isIOS = utils.isIOS()
     this.isAndroid = utils.isAndroid()
   }
-  // computed: {
-  // }
 }
 </script>
 
@@ -41,5 +50,19 @@ export default {
   @media(max-width 340px)
     max-height calc(100vh - 200px)
   .badge
-    display inline-block
+    display inline
+    margin-left 6px
+  .add
+    padding-left 5px
+
+  // copied from WhatsNew
+  ol
+    margin 0
+    margin-top 2px
+    padding-left 15px
+    li
+      padding-top 10px
+      margin-left 5px
+      user-select text
+
 </style>
