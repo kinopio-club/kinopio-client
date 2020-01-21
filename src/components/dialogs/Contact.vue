7da4a9b3171326827c167c5d29d2aef3c2eba0b6
pirijan <hi@pirijan.com>
2020-01-21 18:53:48 -0500

    remove unused fromFooter references

--- src/components/dialogs/Contact.vue
+++ src/components/dialogs/Contact.vue
@@ -1,47 +1,42 @@
 <template lang="pug">
-dialog.support(v-if="visible" :open="visible" @click.stop :class="{'from-footer': fromFooter}")
+dialog.contact.narrow(v-if="visible" :open="visible" @click.stop)
   section
     p Contact
   section
-    img.froggo(src="@/assets/froggo.png")
-    p Please send questions, comments, and requests to
-      a(href="mailto:hi@kinopio.club") hi@kinopio.club
+    img.froggo(src="@/assets/froggo.png" width=100)
+
+    p I made Kinopio to help connect your thoughts, ideas, and plans together.
+    p Hit me up with any questions, comments, or requests at hi@kinopio.club
+    //- p Please send questions, comments, and requests to hi@kinopio.club
     .button-wrap
       a(href="mailto:hi@kinopio.club")
-        button Email
+        button Email Me
     .button-wrap
       a(href="https://twitter.com/KinopioClub")
         button Twitter →
-    .button-wrap
-      a(href="https://www.are.na/kinopio")
-        button Are.na →
+    //- .button-wrap
+    //-   a(href="https://www.are.na/kinopio")
+    //-     button Are.na →
 
 </template>
 
 <script>
 export default {
-  name: 'Support',
+  name: 'Contact',
   props: {
-    visible: Boolean,
-    fromFooter: Boolean
-  },
-  methods: {
+    visible: Boolean
   }
 }
 </script>
 
 <style lang="stylus">
-.support
+.contact
   overflow auto
   max-height calc(100vh - 175px)
   img
     border-radius 3px
   @media(max-width 340px)
     max-height calc(100vh - 220px)
-  &.from-footer
-    max-height calc(100vh - 75px)
-    @media(max-width 340px)
-      max-height calc(100vh - 150px)
   .froggo
     width 100px
 </style>
