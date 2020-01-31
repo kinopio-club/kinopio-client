19f869dbba89cdc04c9f6a768fa3cb50d8844810
Pirijan <hi@pirijan.com>
2020-01-31 15:31:17 -0500

    stub loading and displaying new community spaces

---
+++ src/components/CommunitySpaces.vue
@@ -0,0 +1,62 @@
+<template lang="pug">
+.community-spaces(v-if="visible" :open="visible" @click.stop ref="dialog")
+  section
+    Loader(:visible="loading")
+    p hi
+  //- section.results-section
+  //-   .filter-wrap
+  //-     img.icon.search(src="@/assets/search.svg" @click="focusFilterInput")
+  //-     input(placeholder="Search" v-model="spaceFilter" ref="filterInput")
+  //-     button.borderless.clear-input-wrap(@click="clearFilter")
+  //-       img.icon(src="@/assets/add.svg")
+
+  //-   ul.results-list
+  //-     template(v-for="(space in spacesFiltered")
+  //-       a(:href="space.spaceId")
+  //-         li(:data-full-name="space.fullName" tabindex="0")
+  //-           //- ^TODO^ v-on:keyup.enter="select(space) method
+  //-           .badge.info {{space.category}}
+  //-           span {{space.name}}
+</template>
+
+<script>
+import Loader from '@/components/Loader.vue'
+
+export default {
+  name: 'CommunitySpaces',
+  components: {
+    Loader
+  },
+  props: {
+    visible: Boolean,
+    loading: Boolean,
+    spaces: Array
+  }
+  // data () {
+  //   return {
+  //     spaces: [],
+  //   }
+  // },
+
+  // watch: {
+  // visible (visible) {
+  //   this.clearFilter()
+  //   this.filter = ''
+  //   this.templateCategoryPickerIsVisible = false
+  // }
+  // }
+}
+</script>
+
+<style lang="stylus">
+.templates
+  .categories
+    border-top 1px solid var(--primary)
+  .category-wrap
+    > button
+      .badge
+        margin 0
+  a
+    color var(--primary)
+    text-decoration none
+</style>
--- src/components/dialogs/Community.vue
+++ src/components/dialogs/Community.vue
@@ -4,29 +4,37 @@ dialog.community(v-if="visible" :open="visible")
     .segmented-buttons
       button(@click.stop="hideTemplates" :class="{ active: !templatesIsVisible }")
         span New Spaces
-        //- Loader(:visible="loading.cards")
-
+        Loader(:visible="loadingNewSpaces")
       button(@click.stop="showTemplates" :class="{ active: templatesIsVisible }")
         span Templates
 
-  Templates(:visible="templatesIsVisible")
+    //- temp
+    button(@click="toggleLoad") toggle load
 
+  CommunitySpaces(:visible="!templatesIsVisible" :loading="loadingNewSpaces" :spaces="spaces")
+  Templates(:visible="templatesIsVisible")
 </template>
 
 <script>
 import Templates from '@/components/Templates.vue'
+import CommunitySpaces from '@/components/CommunitySpaces.vue'
+import Loader from '@/components/Loader.vue'
 
 export default {
   name: 'Explore',
   components: {
-    Templates
+    Templates,
+    Loader,
+    CommunitySpaces
   },
   props: {
     visible: Boolean
   },
   data () {
     return {
-      templatesIsVisible: false
+      templatesIsVisible: false,
+      loadingNewSpaces: false,
+      spaces: []
     }
   },
   methods: {
@@ -35,8 +43,25 @@ export default {
     },
     hideTemplates () {
       this.templatesIsVisible = false
+    },
+    toggleLoad () {
+      const value = this.loadingNewSpaces
+      this.loadingNewSpaces = !value
+    },
+    getNewSpaces () {
+      if (this.spaces.length || this.loadingNewSpaces) { return }
+      this.loadingNewSpaces = true
+      console.log('🍄🍄🍄🍄🍄🍄 get spaces now', this.spaces)
+    }
+  },
+  watch: {
+    visible (visible) {
+      // if (!this.spaces.length) {
+      this.getNewSpaces()
+      // }
     }
   }
+
 }
 </script>
 
