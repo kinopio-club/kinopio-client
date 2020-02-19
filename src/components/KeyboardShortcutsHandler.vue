262cd6cfdabd6dde0cc618694ae24a785492a594
Pirijan <hi@pirijan.com>
2020-02-19 18:13:42 -0500

    stub meta key detection for copy/paste

--- src/components/KeyboardShortcutsHandler.vue
+++ src/components/KeyboardShortcutsHandler.vue
@@ -4,34 +4,51 @@
 <script>
 export default {
   mounted () {
-    window.addEventListener('keyup', this.handleKeyboardShortcuts)
+    window.addEventListener('keyup', this.handleShortcuts)
+    // event.metaKey only works on keydown
+    window.addEventListener('keydown', this.handleMetaKeyShortcuts)
   },
   methods: {
-    handleKeyboardShortcuts (event) {
+    handleShortcuts (event) {
       const key = event.key
-      const isMeta = event.metaKey || event.ctrlKey
-      console.log('🌹', key, event.target.tagName, event)
       if (event.target.tagName !== 'BODY') { return }
       if (event.shiftKey && key === 'Enter') {
         console.log('add child card')
       } else if (key === 'Enter') {
         console.log('add parent or sibling card')
       } else if (key === 'Escape') {
-        // console.log('close dialogs, reset parent card')
+        console.log('reset parent card')
         this.$store.commit('closeAllDialogs')
       } else if (key === '?') {
         this.$store.commit('triggerKeyboardShortcutsIsVisible')
       } else if (key === 'Backspace') {
         console.log('remove selected , or currentcard/connection w details open')
         this.$store.commit('closeAllDialogs')
-      } else if (isMeta && key === 'c') {
-        console.log('copy selected cards')
       }
     },
 
-    addCard () {
-      console.log('🏡 add card')
+    handleMetaKeyShortcuts (event) {
+      // get selected card ids from this.$store.state.multipleCardsSelectedIds
+      // save copied/cut card info (utils.clone, change id first) to a new store.js [{}] value (copiedCards: [])
+      // to paste, add copiedCards to the currentSpace, then clearCopiedCards
+      const key = event.key
+      const isMeta = event.metaKey || event.ctrlKey
+      if (event.target.tagName !== 'BODY') { return }
+      if (isMeta && key === 'c') {
+        event.preventDefault()
+        console.log('copy selected cards', this.$store.state.multipleCardsSelectedIds)
+      } else if (isMeta && key === 'x') {
+        event.preventDefault()
+        console.log('cut selected cards', this.$store.state.multipleCardsSelectedIds)
+      } else if (isMeta && key === 'v') {
+        event.preventDefault()
+        console.log('paste selected cards')
+      }
     }
+
+    // addCard () {
+    //   console.log('🏡 add card')
+    // }
 
   }
 }
