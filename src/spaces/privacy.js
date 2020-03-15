be1f819dc1282bb3f6a4f0749406641f778a67f5
Pirijan <hi@pirijan.com>
2020-03-15 17:19:54 -0400

    add open privacy setting

--- src/spaces/privacy.js
+++ src/spaces/privacy.js
@@ -2,28 +2,19 @@ export default {
 
   states () {
     return [
-      // {
-      //   name: 'open',
-      //   shortDescription: 'anyone can add',
-      //   longDescription: 'everyone can view and edit',
-      //   everyone: 'everyone can view and edit',
-      //   collaborators: ''
-      //   color: 'success'
-      // },
+      {
+        name: 'open',
+        description: 'Everyone can edit',
+        color: 'success'
+      },
       {
         name: 'closed',
-        // shortDescription: 'only you can add', // only collaborators can add
-        // everyone: 'everyone can view',
-        // collaborators: 'and only you can edit',
-        description: 'everyone can view and only you can edit', // only you and your collaborators
+        description: 'everyone can view, only you and collaborators can edit',
         color: 'info'
       },
       {
         name: 'private',
-        // shortDescription: 'only you can view', // only collaborators can add
-        description: 'only you can view and edit', // only you and your collaborators
-        // everyone: 'only you can view and edit',
-        // collaborators: ''
+        description: 'only you and collaborators can view and edit',
         color: 'danger'
       }
     ]
