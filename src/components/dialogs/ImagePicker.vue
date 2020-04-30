525e0f0ccaced502c075225d8484e84e89e07b2b
Pirijan <hi@pirijan.com>
2020-04-30 11:56:41 -0400

    add img service toggle, add arena search results

--- src/components/dialogs/ImagePicker.vue
+++ src/components/dialogs/ImagePicker.vue
@@ -1,37 +1,104 @@
 <template lang="pug">
 dialog.image-picker(v-if="visible" :open="visible" @click.stop ref="dialog")
   section
-    p hello
-  //- section.results-section
-  //-   Loader(:visible="loading")
-    //- SpaceList(:spaces="spaces" :showUserIfCurrentUserIsCollaborator="showUserIfCurrentUserIsCollaborator" :selectedSpace="selectedSpace" @selectSpace="selectSpace")
-    //- imagelist
+    .row
+      .segmented-buttons
+        button(@click.stop="toggleServiceIsArena" :class="{active : serviceIsArena}")
+          //- img.icon.arena(src="@/assets/arena.svg")
+          span Are.na
+        button(@click.stop="toggleServiceIsGiphy" :class="{active : serviceIsGiphy}")
+          //- img.icon.giphy(src="@/assets/giphy.svg")
+          span Giphy
+        //- button
+        //-   span Unsplash
+          // dont do if it's the only service that needs a server proxy to use
+      //- button.upload-button Upload
+
+  section.results-section
+    .search-wrap
+      img.icon.search(v-if="!loading" src="@/assets/search.svg" @click="focusSearchInput")
+      Loader(:visible="loading")
+      input(
+        :placeholder="placeholder"
+        v-model="searchInput"
+        ref="searchInput"
+        @keyup.stop.backspace
+        @keyup.stop.enter
+      )
+      button.borderless.clear-input-wrap(@click="clearSearch")
+        img.icon(src="@/assets/add.svg")
+
+    p(v-if="isNoSearchResults") Nothing found on {{service}} for {{search}}
+    ul.results-list.image-list
+      template(v-for="(image in images")
+        li
+          img(:src="image.url")
+
 </template>
 
 <script>
 import scrollIntoView from 'smooth-scroll-into-view-if-needed' // polyfil
+import debounce from 'lodash-es/debounce'
 
-// import cache from '@/cache.js'
-// import Loader from '@/components/Loader.vue'
+import Loader from '@/components/Loader.vue'
 
 export default {
   name: 'ImagePicker',
   components: {
-    // Loader,
-    // SpaceList: () => import('@/components/SpaceList.vue')
+    Loader
   },
   props: {
     visible: Boolean
-    // selectedSpace: Object,
-    // loading: Boolean,
   },
   data () {
     return {
-      images: []
+      images: [],
+      search: '',
+      service: 'Are.na',
+      loading: false
     }
   },
-  // computed: {
+  computed: {
+    searchInput: {
+      get () {
+        return this.search
+      },
+      set (newValue) {
+        this.search = newValue
+        if (newValue) {
+          this.loading = true
+          this.searchService()
+        } else {
+          this.clearSearch()
+        }
+      }
+    },
+    placeholder () { return `Search ${this.service}` },
+    serviceIsArena () {
+      if (this.service === 'Are.na') {
+        return true
+      } else {
+        return false
+      }
+    },
+    serviceIsGiphy () {
+      if (this.service === 'Giphy') {
+        return true
+      } else {
+        return false
+      }
+    },
+    isNoSearchResults () {
+      if (this.search && !this.loading && !this.images.length) {
+        return true
+      } else {
+        return false
+      }
+    }
   // maxHeight () {
+  //   const dialog = this.$refs.dialog
+  //   console.log(dialog)
+  // }
   //   const favoritesDialog = document.querySelector('dialog.favorites')
   //   let height = 120
   //   if (favoritesDialog) {
@@ -42,32 +109,61 @@ export default {
   //   }
   //   return height
   // }
-  // },
+  },
   methods: {
-    // excludeCurrentSpace () {
-    //   if (!this.shouldExcludeCurrentSpace) { return }
-    //   const currentSpace = this.$store.state.currentSpace
-    //   this.spaces = this.spaces.filter(space => space.id !== currentSpace.id)
-    // },
-    // updateSpaces () {
-    //   if (this.userSpaces) {
-    //     this.spaces = this.userSpaces
-    //   } else {
-    //     this.spaces = cache.getAllSpaces()
-    //     this.updateWithRemoteSpaces()
-    //   }
-    //   this.excludeCurrentSpace()
-    // },
-    // async updateWithRemoteSpaces () {
-    //   const spaces = await this.$store.dispatch('api/getUserSpaces')
-    //   if (!spaces) { return }
-    //   this.spaces = spaces
-    //   this.excludeCurrentSpace()
-    // },
+    toggleServiceIsArena () { this.service = 'Are.na' },
+    toggleServiceIsGiphy () { this.service = 'Giphy' },
+    searchService: debounce(async function () {
+      if (this.serviceIsArena) {
+        const url = new URL('https://api.are.na/v2/search/blocks')
+        const params = { q: this.search }
+        url.search = new URLSearchParams(params).toString()
+        const response = await fetch(url)
+        const data = await response.json()
+        this.normalizeResults(data, 'Are.na')
+      } else if (this.serviceIsGiphy) {
+        console.log('Giphy', this.search)
+      }
+      this.loading = false
+    }, 250),
+    normalizeResults (data, service) {
+      console.log(service, this.service)
+      if (service === 'Are.na' && this.service === service) {
+        const images = data.blocks.filter(block => block._type === 'image')
+        this.images = images.map(image => {
+          return {
+            sourceUrl: `https://www.are.na/block/${image.id}`,
+            sourceUserName: image.user.username,
+            sourceUserUrl: `https://www.are.na/${image.user.slug}`,
+            url: image.image.large.url
+          }
+        })
+      }
+      this.$nextTick(() => {
+        this.scrollIntoView()
+      })
+    },
+    focusSearchInput () {
+      const element = this.$refs.searchInput
+      const length = this.search.length
+      element.focus()
+      element.setSelectionRange(length, length)
+    },
+    clearSearch () {
+      this.search = ''
+      this.loading = false
+      this.images = []
+      this.$nextTick(() => {
+        this.scrollIntoView()
+      })
+    },
     selectImage (image) {
       // TODO wire up to carddetails
+      // if service is unsplash , then send a download req https://help.unsplash.com/en/articles/2511258-guideline-triggering-a-download
       this.$emit('selectImage', image)
     },
+    // TODO might have issues w ios, see carddetails ismobile handling
+    // TODO2 if copying carddetails, ignore pinchzoom stuff, always zoom/focus
     scrollIntoView () {
       const element = this.$refs.dialog
       scrollIntoView(element, {
@@ -82,6 +178,7 @@ export default {
         if (visible) {
           // this.updateSpaces()
           this.scrollIntoView()
+          this.focusSearchInput()
         }
       })
     }
@@ -93,6 +190,40 @@ export default {
 </script>
 
 <style lang="stylus">
+.image-picker
+  .search-wrap
+    margin-left 2px
+    padding-top 4px
+    display flex
+    .search
+      margin-top -11px
+      padding-right 5px
+      cursor text
+    .loader
+      width 16px
+      height 14px
+      margin-right 3px
+  // .arena
+  //   width 18px
+  // .giphy
+  //   height 12px
+  // .segmented-button + button
+  //   margin-right 6px
+  .upload-button
+    margin-left 6px
+
+  .results-section
+    padding 8px
+    padding-top 0
+    padding-bottom 0
+    p
+      margin-top 0
+      padding-bottom 8px
+
+  .image-list
+    img
+      position relative
+
 // .space-picker
 //   .results-section
 //     padding-top 4px
