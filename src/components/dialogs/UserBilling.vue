3b5988da016518fca6fe960610578ab9a9c5af7d
Pirijan <hi@pirijan.com>
2020-06-24 21:48:37 -0400

    stub segmented cancel button - re-add loading cuz i need to show '$0/mo' to free users

--- src/components/dialogs/UserBilling.vue
+++ src/components/dialogs/UserBilling.vue
@@ -3,31 +3,52 @@ dialog.narrow.user-billing(v-if="visible" :open="visible" @click.stop)
   section
     p Billing
   section
-    .summary
+    .loading-stripe(v-if="!loading.stripeInfo")
+      Loader(:visible="true")
+    .summary(v-if="loading.stripeInfo")
       User(:user="user" :isClickable="false" :hideYouLabel="true" :key="user.id")
       p
         span You are paying
           .badge.info $4/month
 
     p Thanks for supporting Kinopio!
-    button
-      img.icon(src="@/assets/remove.svg")
-      span Cancel Subscription
+
+    .row
+      button(v-if="!cancelSubscriptionVisible" @click="toggleCancelSubscriptionVisible")
+        img.icon(src="@/assets/remove.svg")
+        span Downgrade to Free
+      span(v-if="cancelSubscriptionVisible")
+        p
+          span blah blah blah 150 cards
+        .segmented-buttons
+          button(@click="toggleCancelSubscriptionVisible")
+            span Cancel
+          button.danger(@click="cancelSubscription")
+            img.icon(src="@/assets/remove.svg")
+            span Downgrade
+            Loader(:visible="loading.isCancelling")
 
 </template>
 
 <script>
+import Loader from '@/components/Loader.vue'
+
 export default {
   name: 'UserBilling',
   components: {
-    User: () => import('@/components/User.vue')
+    User: () => import('@/components/User.vue'),
+    Loader
   },
   props: {
     visible: Boolean
   },
   data () {
     return {
-      cancelSubscriptionVisible: true
+      cancelSubscriptionVisible: false,
+      loading: {
+        stripeInfo: true, // todo default = false
+        isCancelling: false
+      }
     }
   },
   computed: {
@@ -35,7 +56,15 @@ export default {
   },
   methods: {
     toggleCancelSubscriptionVisible () {
-      this.toggleCancelSubscriptionVisible = !this.toggleCancelSubscriptionVisible
+      if (this.loading.isCancelling) { return }
+      this.cancelSubscriptionVisible = !this.cancelSubscriptionVisible
+    },
+    cancelSubscription () {
+      this.loading.isCancelling = true
+      // todo call stripe
+      // this.$store.commit('currentUser/isUpgraded', false)
+      this.$emit('closeDialog')
+      // this.isCancelling = false
     }
   },
   watch: {
@@ -59,4 +88,8 @@ export default {
       display inline-block
     p
       margin 0
+  .loading-stripe
+    margin-bottom 10px
+  .row
+    margin-top 10px
 </style>
