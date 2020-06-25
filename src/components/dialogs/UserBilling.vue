b01fa5807bf9755dbb83852540c0476e45ea7dfa
Pirijan <hi@pirijan.com>
2020-06-24 21:03:14 -0400

    stub user billing dialog for upgraded current user

---
+++ src/components/dialogs/UserBilling.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+dialog.narrow(v-if="visible" :open="visible" @click.stop)
+  section
+    p User Billing
+    //- billing info, from stripe?
+  section
+    p yolo yolo
+
+</template>
+
+<script>
+import Loader from '@/components/Loader.vue'
+
+export default {
+  name: 'UserBilling',
+  components: {
+    Loader
+  },
+  props: {
+    visible: Boolean
+  },
+  data () {
+    return {
+      loading: {
+        stripeInfo: false
+      }
+    }
+  },
+  computed: {
+    // isSignedIn () {
+    //   return this.$store.getters['currentUser/isSignedIn']
+    // }
+  },
+  methods: {
+    // toggleRemoveAllConfirmationVisible () {
+    //   this.removeAllConfirmationVisible = !this.removeAllConfirmationVisible
+    // },
+    // async removeUserPermanent () {
+    //   this.loading.removeUserPermanent = true
+    //   await this.$store.dispatch('api/removeUserPermanent')
+    //   this.loading.removeUserPermanent = false
+    //   this.$emit('removeUser')
+    // }
+  }
+  // watch: {
+  //   visible (value) {
+  //     if (value) {
+  //     }
+  //   }
+  // }
+}
+</script>
+
+<style lang="stylus">
+</style>
--- src/components/dialogs/UserSettings.vue
+++ src/components/dialogs/UserSettings.vue
@@ -1,39 +1,49 @@
 <template lang="pug">
-dialog.narrow(v-if="visible" :open="visible" @click.stop)
+dialog.narrow(v-if="visible" :open="visible" @click.stop="closeDialogs")
   section
     p User Settings
   section
-    button.danger(v-if="!removeAllConfirmationVisible" @click="toggleRemoveAllConfirmationVisible")
-      img.icon(src="@/assets/remove.svg")
-      span Remove All Your Data
-    span(v-if="removeAllConfirmationVisible")
-      p
-        span.badge.danger Permanently remove
-        span(v-if="isSignedIn") all your spaces and user data from this computer and Kinopio's servers?
-        span(v-else) all your spaces and user data from this computer?
-      .segmented-buttons
-        button(@click="toggleRemoveAllConfirmationVisible")
-          span Cancel
-        button.danger(@click="removeUserPermanent")
-          img.icon(src="@/assets/remove.svg")
-          span Remove All
-          Loader(:visible="loading.removeUserPermanent")
+    .row(v-if="isUpgraded")
+      .button-wrap
+        button(@click.stop="toggleUserBillingIsVisible" :class="{active: userBillingIsVisible}")
+          span Billing
+        UserBilling(:visible="userBillingIsVisible")
+
+    .row
+      button.danger(v-if="!removeAllConfirmationVisible" @click="toggleRemoveAllConfirmationVisible")
+        img.icon(src="@/assets/remove.svg")
+        span Remove All Your Data
+      span(v-if="removeAllConfirmationVisible")
+        p
+          span.badge.danger Permanently remove
+          span(v-if="isSignedIn") all your spaces and user data from this computer and Kinopio's servers?
+          span(v-else) all your spaces and user data from this computer?
+        .segmented-buttons
+          button(@click="toggleRemoveAllConfirmationVisible")
+            span Cancel
+          button.danger(@click="removeUserPermanent")
+            img.icon(src="@/assets/remove.svg")
+            span Remove All
+            Loader(:visible="loading.removeUserPermanent")
 
 </template>
 
 <script>
+import UserBilling from '@/components/dialogs/UserBilling.vue'
 import Loader from '@/components/Loader.vue'
 
 export default {
   name: 'UserSettings',
   components: {
-    Loader
+    Loader,
+    UserBilling
   },
   props: {
     visible: Boolean
   },
   data () {
     return {
+      userBillingIsVisible: false,
       removeAllConfirmationVisible: false,
       loading: {
         removeUserPermanent: false
@@ -43,9 +53,15 @@ export default {
   computed: {
     isSignedIn () {
       return this.$store.getters['currentUser/isSignedIn']
+    },
+    isUpgraded () {
+      return this.$store.state.currentUser.isUpgraded
     }
   },
   methods: {
+    closeDialogs () {
+      this.userBillingIsVisible = false
+    },
     toggleRemoveAllConfirmationVisible () {
       this.removeAllConfirmationVisible = !this.removeAllConfirmationVisible
     },
@@ -54,6 +70,12 @@ export default {
       await this.$store.dispatch('api/removeUserPermanent')
       this.loading.removeUserPermanent = false
       this.$emit('removeUser')
+    },
+    toggleUserBillingIsVisible () {
+      const isVisible = this.userBillingIsVisible
+      this.closeDialogs()
+      this.userBillingIsVisible = !isVisible
+      console.log('this.userBillingIsVisible', this.userBillingIsVisible)
     }
   },
   watch: {
