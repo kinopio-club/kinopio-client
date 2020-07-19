e1c1254fcad57241ee77541c83f4513b0969f64d
Pirijan <hi@pirijan.com>
2020-07-19 14:11:08 -0400

    add temp diary link

--- src/components/dialogs/WhatsNew.vue
+++ src/components/dialogs/WhatsNew.vue
@@ -5,6 +5,9 @@ dialog.whats-new(v-if="visible" :open="visible" @click.stop)
     .button-wrap
       a(href="https://kinopio.club/-kinopio-roadmap-6TRE21gchHI7alHLuwzd5")
         button Roadmap →
+    .button-wrap
+      a(href="https://www.are.na/kinopio/diary-q6l8pa4a4ye")
+        button Diary →
 
   section(v-if="!newStuff.length")
     Loader(:visible="true")
