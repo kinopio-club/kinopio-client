d859e5793e10ac98bbfa5244714f8b3380564478
Pirijan <hi@pirijan.com>
2020-08-01 17:34:30 -0400

    stub errors: notificationsWithPosition

---
+++ src/components/NotificationsWithPosition.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+aside.notifications-with-position
+  //- ^ click to force hide all notifications
+  .item.badge.danger
+    img.icon.cancel(src="@/assets/add.svg")
+    span cray crayyy
+</template>
+<script>
+export default {
+  name: 'NotificationsWithPosition',
+  created () {
+    // this.$store.subscribe((mutation, state) => {
+  }
+}
+</script>
+
+<style lang="stylus">
+.notifications-with-position
+  .item
+    position absolute
+    top 310px // 0
+    left 604px // 0
+    z-index var(--max-z)
+    pointer-events none
+</style>
