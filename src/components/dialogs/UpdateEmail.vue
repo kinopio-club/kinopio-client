14f27bdaf0f52cbb993902aee6605973c6bfe26d
Pirijan <hi@pirijan.com>
2021-01-26 08:29:21 -0800

    stub dialog ui

--- src/components/dialogs/UpdateEmail.vue
+++ src/components/dialogs/UpdateEmail.vue
@@ -1,9 +1,23 @@
+
 <template lang="pug">
 dialog.narrow.update-email(v-if="visible" :open="visible" @click.left.stop ref="dialog" :style="{'max-height': dialogHeight + 'px'}")
   section
     p Update Email
-
-//- in dialog success badge
+  section
+    template(v-if="!currentUserIsSignedIn")
+      p After you sign up you'll be able to update your email here
+      button(@click.left="triggerSignUpOrInIsVisible") Sign Up or In
+    template(v-else)
+      form(@submit.prevent="updateEmail")
+      input(type="text" placeholder="Email" required autocomplete="email" v-model="email")
+      button(type="submit" :class="{active : loading}")
+        span Update
+        Loader(:visible="loading")
+    //- status
+    .row(v-if="success")
+      span.badge.success Email Changed
+    .row(v-if="error")
+      span.badge.danger (シ_ _)シ Something went wrong, Please try again or contact support
 </template>
 
 <script>
@@ -27,10 +41,15 @@ export default {
   },
   data () {
     return {
-      dialogHeight: null
+      dialogHeight: null,
+      email: '',
+      loading: false,
+      success: false,
+      error: false
     }
   },
   computed: {
+    currentUserIsSignedIn () { return this.$store.getters['currentUser/isSignedIn'] }
   },
   methods: {
     updateDialogHeight () {
@@ -39,13 +58,42 @@ export default {
         let element = this.$refs.dialog
         this.dialogHeight = utils.elementHeight(element)
       })
-    }
+    },
+    triggerSignUpOrInIsVisible () {
+      this.$store.dispatch('closeAllDialogs', 'UpdateEmail.triggerSignUpOrInIsVisible')
+      this.$store.commit('triggerSignUpOrInIsVisible')
+    },
+    async updateEmail () {
+      if (this.loading) { return }
+      if (!this.email) { return }
+      this.loading = true
+      // const response = await this.$store.dispatch('api/signIn', { email, password })
+      // const result = await response.json()
+      // this.loading = false
 
+      // if (this.isSuccess(response)) {
+      //      this.clearStatus()
+      //   // show success
+      // } else {
+      //      this.clearStatus()
+      //   this.error = true
+      // }
+    },
+    isSuccess (response) {
+      const success = [200, 201, 202, 204]
+      return Boolean(success.includes(response.status))
+    },
+    clearStatus () {
+      this.error = false
+      this.success = false
+    }
   },
   watch: {
     visible (visible) {
       if (visible) {
         this.updateDialogHeight()
+        this.email = this.$store.state.currentUser.email
+        this.clearStatus()
       }
     }
   }
@@ -56,6 +104,6 @@ export default {
 .update-email
   max-height calc(100vh - 190px)
   overflow auto
-  // .row
-  //   margin-top 10px
+  .row
+    margin-top 10px
 </style>
