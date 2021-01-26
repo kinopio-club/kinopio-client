f382a3db6391a8425f5c5a4070207937ec01805e
Pirijan <hi@pirijan.com>
2021-01-26 09:32:42 -0800

    error handling
    qa
    x blank email,
    x invalid email,
    x duplicate/existing email

--- src/components/dialogs/UpdateEmail.vue
+++ src/components/dialogs/UpdateEmail.vue
@@ -20,6 +20,9 @@ dialog.narrow.update-email(v-if="visible" :open="visible" @click.left.stop ref="
       span.badge.success A confirmation email has been sent to both your previous and new addresses
     .row(v-if="error.unknownServerError")
       span.badge.danger (シ_ _)シ Something went wrong, Please try again or contact support
+    .row(v-else-if="error.accountAlreadyExists")
+      span.badge.danger An account with this email already exists
+
 </template>
 
 <script>
@@ -71,19 +74,22 @@ export default {
     async updateEmail () {
       if (this.loading) { return }
       if (!this.email) { return }
+      if (this.email === this.$store.state.currentUser.email) { return }
       this.clearStatus()
-      let result
-      try {
-        this.loading = true
-        const response = await this.$store.dispatch('api/updateEmail', this.email)
-        result = await response.json()
+      this.loading = true
+      const response = await this.$store.dispatch('api/updateEmail', this.email)
+      const result = await response.json()
+      if (this.isSuccess(response)) {
         this.success = true
         this.$store.commit('currentUser/email', this.email)
-      } catch (error) {
-        console.warn('updateEmail', error)
+      } else {
         await this.handleErrors(result)
       }
       this.loading = false
+    },
+    isSuccess (response) {
+      const success = [200, 201, 202, 204]
+      return Boolean(success.includes(response.status))
     },
     async handleErrors (response) {
       if (!response) {
@@ -92,6 +98,8 @@ export default {
       }
       if (response.type === 'unique violation') {
         this.error.accountAlreadyExists = true
+      } else {
+        this.error.unknownServerError = true
       }
     },
     clearStatus () {
