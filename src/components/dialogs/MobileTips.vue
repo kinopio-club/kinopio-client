041f3dc79be006ce6055505f7028465f5f53a93e
Pirijan <hi@pirijan.com>
2021-05-13 11:07:53 -0400

    mobile tips content

--- src/components/dialogs/MobileTips.vue
+++ src/components/dialogs/MobileTips.vue
@@ -1,65 +1,32 @@
 <template lang="pug">
-dialog.narrow.background(v-if="visible" :open="visible" @click.left="closeDialogs")
+dialog.narrow.background(v-if="visible" :open="visible")
   section
     p Mobile Tips
-
   section(
     @mouseup.stop
     @touchend.stop
   )
-    p blah blah
+    p
+      img.icon(src="@/assets/press-and-hold.svg")
+      span Press and hold to drag cards
+    video(autoplay loop muted playsinline src="https://kinopio-updates.us-east-1.linodeobjects.com/mobile-press-hold-drag.mp4" height="83")
+
+    p
+      img.icon(src="@/assets/press-and-hold.svg")
+      span Press and hold to paint cards
+    video(autoplay loop muted playsinline src="https://kinopio-updates.us-east-1.linodeobjects.com/mobile-press-hold-paint.mp4" height="83")
 
 </template>
 
 <script>
-// import utils from '@/utils.js'
 
 export default {
   name: 'MobileTips',
   props: {
     visible: Boolean
   }
-  // computed: {
-  // currentSpace () { return this.$store.state.currentSpace },
-  // },
-  // methods: {
-  // },
-  // watch: {
-  //   visible (visible) {
-  //     if (visible) {
-  //       this.closeDialogs()
-  //       this.clearErrors()
-  //     } else {
-  //       if (this.error.isNotImageUrl) {
-  //         this.removeBackground()
-  //       }
-  //     }
-  //   },
-  // }
 }
 </script>
 
 <style lang="stylus">
-// .mobile-tips
-//   @media(max-width 435px)
-//     left -100px
-//   &.narrow
-//     width 215px
-//   textarea
-//     margin-bottom 6px
-//   .error-container
-//     margin-bottom 10px
-//   .uploading-container
-//     position relative
-//     img
-//       border-radius 3px
-//     .badge
-//       display inline-block
-//       &.absolute
-//         position absolute
-//         top 6px
-//         left 6px
-//   .hidden
-//     display none
-
 </style>
