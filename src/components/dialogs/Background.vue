ac4742eafe80cf2e6c4dcaa77b39b06557872a33
Pirijan <hi@pirijan.com>
2021-06-15 14:49:21 -0400

    lightenColors only if no background image

--- src/components/dialogs/Background.vue
+++ src/components/dialogs/Background.vue
@@ -62,7 +62,7 @@ dialog.narrow.background(v-if="visible" :open="visible" @click.left.stop="closeD
         button.change-color(:disabled="!canEditSpace" @click.left.stop="toggleColorPicker" :class="{active: colorPickerIsVisible}")
           .current-color(v-if="backgroundTint" :style="{ background: backgroundTint }")
           span(v-if="!backgroundTint") Tint
-        ColorPicker(:currentColor="backgroundTint || '#fff'" :visible="colorPickerIsVisible" @selectedColor="updateBackgroundTint" :removeIsVisible="true" @removeColor="removeBackgroundTint" :lightenColors="true")
+        ColorPicker(:currentColor="backgroundTint || '#fff'" :visible="colorPickerIsVisible" @selectedColor="updateBackgroundTint" :removeIsVisible="true" @removeColor="removeBackgroundTint" :lightenColors="lightenColors")
       .button-wrap
         button(:disabled="!canEditSpace" @click.left.stop="toggleImagePickerIsVisible" :class="{active : imagePickerIsVisible}")
           img.icon.flower(src="@/assets/flower.svg")
@@ -132,6 +132,13 @@ export default {
       },
       set (url) {
         this.updateSpaceBackground(url)
+      }
+    },
+    lightenColors () {
+      if (this.background) {
+        return false
+      } else {
+        return true
       }
     },
     pendingUpload () {
