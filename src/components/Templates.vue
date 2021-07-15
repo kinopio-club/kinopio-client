1e8e89ce26f1b68bf783f36f8dfde9df3c1273e4
Pirijan <hi@pirijan.com>
2021-07-15 14:26:13 -0400

    remove template category picker

--- src/components/Templates.vue
+++ src/components/Templates.vue
@@ -1,25 +1,17 @@
 <template lang="pug">
-.templates(v-if="visible" :open="visible" @click.left.stop @click.left="closeDialogs")
-  section.categories(v-if="!hideOptions")
-    .button-wrap.category-wrap
-      button(@click.left.stop="toggleTemplateCategoryPickerIsVisible" :class="{active: templateCategoryPickerIsVisible}")
-        .badge.info.inline-badge(:class="categoryClassName(filterCategory)") {{filterCategory.name}}
-      TemplateCategoryPicker(:visible="templateCategoryPickerIsVisible" :selectedCategoryId="filteredCategoryId" @closeDialog="closeDialogs" @selectCategory="updateFilteredCategory")
-      Contact(:visible="contactIsVisible")
+.templates(v-if="visible" :open="visible" @click.left.stop)
   section.results-section(ref="results" :style="{'max-height': resultsSectionHeight + 'px'}")
     SpaceList(:spaces="spacesFiltered" :showCategory="true" @selectSpace="changeSpace")
 </template>
 
 <script>
 import templates from '@/data/templates.js'
-import TemplateCategoryPicker from '@/components/dialogs/TemplateCategoryPicker.vue'
 import SpaceList from '@/components/SpaceList.vue'
 import utils from '@/utils.js'
 
 export default {
   name: 'Templates',
   components: {
-    TemplateCategoryPicker,
     SpaceList
   },
   props: {
@@ -35,9 +27,7 @@ export default {
   },
   data () {
     return {
-      filteredCategoryId: 0,
       filteredSpaces: [],
-      templateCategoryPickerIsVisible: false,
       resultsSectionHeight: null
     }
   },
@@ -47,19 +37,12 @@ export default {
     },
     spaces () {
       let spaces = templates.spaces()
-      // add category meta to space
-      spaces = spaces.map(space => {
+      return spaces.map(space => {
         const category = this.categories.find(category => category.id === space.categoryId)
         space.category = category.name
         space.fullName = `${space.category} – ${space.name}`
         return space
       })
-      // filter by categories
-      if (this.filteredCategoryId === 0) {
-        return spaces
-      } else {
-        return spaces.filter(space => space.categoryId === this.filteredCategoryId)
-      }
     },
     spacesFiltered () {
       if (this.filter) {
@@ -67,28 +50,11 @@ export default {
       } else {
         return this.spaces
       }
-    },
-    filterCategory () {
-      return this.categories.find(category => category.id === this.filteredCategoryId)
     }
   },
   methods: {
-    categoryClassName (filterCategory) {
-      return filterCategory.name.toLowerCase()
-    },
     changeSpace (space) {
       this.$store.dispatch('currentSpace/changeSpace', { space, isRemote: true })
-    },
-    closeDialogs () {
-      this.templateCategoryPickerIsVisible = false
-    },
-    updateFilteredCategory (category) {
-      this.filteredCategoryId = category.id
-    },
-    toggleTemplateCategoryPickerIsVisible () {
-      const isVisible = this.templateCategoryPickerIsVisible
-      this.closeDialogs()
-      this.templateCategoryPickerIsVisible = !isVisible
     },
     updateResultsSectionHeight () {
       if (!this.visible) { return }
@@ -100,7 +66,6 @@ export default {
   },
   watch: {
     visible (visible) {
-      this.templateCategoryPickerIsVisible = false
       if (visible) {
         this.updateResultsSectionHeight()
       }
@@ -123,9 +88,6 @@ export default {
     > button
       .badge
         margin 0
-  .contact
-    top calc(100% - 8px)
-    bottom initial
   // template category colors
   .inline-badge
     &.learning
