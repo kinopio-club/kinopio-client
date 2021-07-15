8ddb4e73f90fc9a58e9a230aaac9b11433d85de6
Pirijan <hi@pirijan.com>
2021-07-15 10:52:32 -0400

    add journal template

--- src/data/templates.js
+++ src/data/templates.js
@@ -43,6 +43,11 @@ export default {
         name: 'Research'
       },
       {
+        id: 'aKbPUalz4zHLqGwpDncDI',
+        categoryId: 2,
+        name: 'Journal'
+      },
+      {
         id: 'YD_eqjNH7DQpgDKjEDSzu',
         categoryId: 2,
         name: 'Making Hard Decisions'
