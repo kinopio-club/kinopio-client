e2ec011923c0d0b5969e47f04f51e0392c86174f
Pirijan <hi@pirijan.com>
2021-09-09 16:02:52 -0400

    stub favorites filters

---
+++ src/components/dialogs/FavoritesFilters.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+dialog.narrow.favorites-filters(v-if="visible" :open="visible" @click.left.stop ref="dialog")
+  section
+    p
+      img.icon.heart(src="@/assets/heart.svg")
+      span Spaces Filter
+  section
+    .segmented-buttons
+      button(@click="showAllSpaces" :class="{active: all}") All
+      button(@click="showJournalsOnly" :class="{active: journals}")
+        span Journals
+      button(@click="showSpacesOnly" :class="{active: spaces}") Spaces
+
+</template>
+
+<script>
+
+export default {
+  name: 'FavoritesFilters',
+  props: {
+    visible: Boolean,
+    favoritesFilter: String
+  },
+  data () {
+    return {
+      // moonPhase: {}
+    }
+  },
+  computed: {
+    all () {
+      return Boolean(!this.favoritesFilter)
+    },
+    journals () {
+      return this.favoritesFilter === 'journals'
+    },
+    spaces () {
+      return this.favoritesFilter === 'spaces'
+    }
+  },
+  methods: {
+    showJournalsOnly () {
+      this.$emit('updateFavoritesFilter', 'journals')
+    },
+    showSpacesOnly () {
+      this.$emit('updateFavoritesFilter', 'spaces')
+    },
+    showAllSpaces () {
+      this.$emit('updateFavoritesFilter', null)
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
