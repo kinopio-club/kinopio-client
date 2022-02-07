3007c1d821be16d0baa81851685c94deb0a27115
Pirijan <hi@pirijan.com>
2022-02-07 10:13:45 -0500

    noop

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -607,7 +607,7 @@ export default {
     },
     cardUrlPreviewIsVisible () {
       const cardHasUrlPreviewInfo = Boolean(this.card.urlPreviewTitle || this.card.urlPreviewDescription || this.card.urlPreviewImage)
-      // experiment: remove urlPreviewErrorUrl checking to eliminate false positives. Observe if there's a downside irl and if this attribute should be removed entirely?
+      // TEMP experiment: remove card.urlPreviewErrorUrl checking to eliminate false positives. Observe if there's a downside irl and if this attribute should be removed entirely?
       // const isErrorUrl = this.card.urlPreviewErrorUrl && (this.card.urlPreviewUrl === this.card.urlPreviewErrorUrl)
       return Boolean(this.card.urlPreviewIsVisible && this.card.urlPreviewUrl && cardHasUrlPreviewInfo) // && !isErrorUrl
     },
