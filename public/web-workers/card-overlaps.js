a0d5f73e8b472ccb687069411420c86040611dd3
pirijan <hi@pirijan.com>
2022-02-10 14:57:58 -0500

    use full overlap, instead of partial

--- public/web-workers/card-overlaps.js
+++ public/web-workers/card-overlaps.js
@@ -44,7 +44,7 @@ self.onmessage = function (event) {
 const isCardsOverlappingGroup = (card, groups) => {
   const index = groups.findIndex(groupCards => {
     return groupCards.find(groupCard => {
-      const isOverlap = isCardsOverlapping(groupCard, card) || isCardsOverlapping(card, groupCard)
+      const isOverlap = isCardInsideCard(groupCard, card) || isCardInsideCard(card, groupCard)
       return isOverlap
     })
   })
@@ -52,8 +52,8 @@ const isCardsOverlappingGroup = (card, groups) => {
   return { index }
 }
 
-// [ card1 [-]-card2-]
-const isCardsOverlapping = (card1, card2) => {
+// [ group [-card2-]]
+const isCardInsideCard = (card1, card2) => {
   const xStart = isBetween({
     value: card1.x,
     min: card2.x,
@@ -74,11 +74,36 @@ const isCardsOverlapping = (card1, card2) => {
     min: card2.y,
     max: card2.y + card2.height
   })
-  const x = xStart || xEnd
-  const y = yStart || yEnd
-  const isOverlap = x && y
-  return isOverlap
+  return xStart && xEnd && yStart && yEnd
 }
+
+// [ card1 [-]-card2-]
+// const isCardsOverlapping = (card1, card2) => {
+//   const xStart = isBetween({
+//     value: card1.x,
+//     min: card2.x,
+//     max: card2.x + card2.width
+//   })
+//   const xEnd = isBetween({
+//     value: card1.x + card1.width,
+//     min: card2.x,
+//     max: card2.x + card2.width
+//   })
+//   const yStart = isBetween({
+//     value: card1.y,
+//     min: card2.y,
+//     max: card2.y + card2.height
+//   })
+//   const yEnd = isBetween({
+//     value: card1.y + card1.height,
+//     min: card2.y,
+//     max: card2.y + card2.height
+//   })
+//   const x = xStart || xEnd
+//   const y = yStart || yEnd
+//   const isOverlap = x && y
+//   return isOverlap
+// }
 
 // based on utils.isBetween
 const isBetween = ({ value, min, max }) => {
