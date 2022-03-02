d86e72d520c3a50d22bfb9651f75b87780046cab
Pirijan <hi@pirijan.com>
2022-03-02 14:47:43 -0500

    noop: update 'open' privacy language to be less ambiguous
    https://ptb.discord.com/channels/857305113936134204/857724063903252530/948611212785098802

--- src/data/privacy.js
+++ src/data/privacy.js
@@ -4,14 +4,15 @@ export default {
     return [
       {
         name: 'open',
-        description: 'Everyone can view and add cards',
+        friendlyName: 'Open to All',
+        description: 'Anyone can view and add cards',
         color: 'success',
         icon: 'open'
       },
       {
         name: 'closed',
         friendlyName: 'Public Read Only',
-        description: 'everyone can view, only you and collaborators can edit',
+        description: 'Anyone can view, only you and collaborators can edit',
         color: 'info',
         icon: 'unlock'
       },
