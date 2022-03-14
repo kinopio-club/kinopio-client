0c16e067304f18c9954320840554cc664e3b1ffd
Pirijan <hi@pirijan.com>
2022-03-14 16:43:19 -0400

    noop comments

--- src/router.js
+++ src/router.js
@@ -9,6 +9,9 @@ const router = createRouter({
     {
       path: '/add',
       name: 'add',
+      // route level code-splitting
+      // this generates a separate chunk (add.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
       component: () => import('./views/Add.vue'),
       beforeEnter: (to, from, next) => {
         store.commit('isAddPage', true)
