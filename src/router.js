008ac1c0562e6c772391779844743edf86eb3129
Pirijan <hi@pirijan.com>
2022-03-14 16:51:04 -0400

    noop

--- src/router.js
+++ src/router.js
@@ -10,7 +10,7 @@ const router = createRouter({
       path: '/add',
       name: 'add',
       // route level code-splitting
-      // this generates a separate chunk (add.[hash].js) for this route
+      // this generates a separate chunk (Add.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('./views/Add.vue'),
       beforeEnter: (to, from, next) => {
