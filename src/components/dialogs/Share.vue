4a5241d77a3d7c01eef76aa238fa0caf2505674f
Pirijan <hi@pirijan.com>
2022-04-12 12:59:14 -0400

    hide pdf buttons for milestone shipping

--- src/components/dialogs/Share.vue
+++ src/components/dialogs/Share.vue
@@ -32,10 +32,10 @@ dialog.narrow.share(v-if="visible" :open="visible" @click.left.stop="closeDialog
             span Embed
           Embed(:visible="embedIsVisible")
         //- PDF
-        .button-wrap
-          button(@click.left.stop="togglePdfIsVisible" :class="{ active: pdfIsVisible }")
-            span PDF
-          Pdf(:visible="pdfIsVisible")
+        //- .button-wrap
+        //-   button(@click.left.stop="togglePdfIsVisible" :class="{ active: pdfIsVisible }")
+        //-     span PDF
+        //-   Pdf(:visible="pdfIsVisible")
 
       .badge.success.success-message(v-if="urlIsCopied") Url Copied
     p.share-private(v-if="spaceIsPrivate")
@@ -47,12 +47,12 @@ dialog.narrow.share(v-if="visible" :open="visible" @click.left.stop="closeDialog
       span.badge.success.last-child
         img.icon.open(src="@/assets/open.svg")
         span {{privacyName(0)}}
-    p
-      //- PDF
-      .button-wrap
-        button(@click.left.stop="togglePdfIsVisible" :class="{ active: pdfIsVisible }")
-          span PDF
-        Pdf(:visible="pdfIsVisible")
+    //- PDF
+    //- p
+    //-   .button-wrap
+    //-     button(@click.left.stop="togglePdfIsVisible" :class="{ active: pdfIsVisible }")
+    //-       span PDF
+    //-     Pdf(:visible="pdfIsVisible")
 
   section(v-if="spaceHasUrl && isSpaceMember")
     .button-wrap
