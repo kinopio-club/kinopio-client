4e4b17a57c73d10289bf541e0b030cc2118e733c
Pirijan <hi@pirijan.com>
2022-07-26 12:00:21 -0400

    zoom: farther out = smaller scrollarea px

--- src/components/ScrollAtEdgesHandler.vue
+++ src/components/ScrollAtEdgesHandler.vue
@@ -62,7 +62,7 @@ export default {
   methods: {
     initInteractions (event) {
       const position = utils.cursorPositionInViewport(event)
-      const zoom = this.spaceCounterZoomDecimal
+      const zoom = this.spaceZoomDecimal
       startCursor = position
       endCursor = position
       scrollAreaHeight = (this.viewportHeight / 8) * zoom
