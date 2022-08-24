9bbc388eecb4e023170ecc4e37643533d97b7ca2
Pirijan <hi@pirijan.com>
2022-08-24 09:07:47 -0400

    noop: componentized AddToIndex

---
+++ src/components/AddToInbox.vue
@@ -0,0 +1,336 @@
+<template lang="pug">
+section.add-to-inbox(v-if="visible")
+  .info
+    .badge.info
+      img.icon.inbox-icon(src="@/assets/inbox.svg")
+      span Add to Inbox
+    //- error: card limit
+    a(:href="kinopioDomain" v-if="cardsCreatedIsOverLimit")
+      .badge.danger.button-badge
+        span Upgrade for more →
+    //- error: connection
+    .badge.danger(v-if="error.unknownServerError || error.maxLength") (シ_ _)シ Server error
+  //- textarea
+  .row
+    User(:user="currentUser" :isClickable="false" :hideYouLabel="true")
+    .textarea-wrap
+      textarea.name(
+        ref="name"
+        rows="1"
+        placeholder="Type text here, or paste a URL"
+        v-model="name"
+        :maxlength="maxCardLength"
+        @keydown.enter.exact.prevent="createCard"
+        @focusin="updateKeyboardShortcutTipIsVisible(true)"
+        @focusout="updateKeyboardShortcutTipIsVisible(false)"
+
+        @keyup.alt.enter.exact.stop
+        @keyup.ctrl.enter.exact.stop
+        @keydown.alt.enter.exact.stop="insertLineBreak"
+        @keydown.ctrl.enter.exact.stop="insertLineBreak"
+      )
+  //- button
+  .row
+    .button-wrap
+      a(:href="spaceUrlPath")
+        button
+          img.icon.inbox-icon(src="@/assets/inbox.svg")
+          span Inbox
+    .button-wrap
+      button(@click.stop="createCard" :class="{active: loading.createCard, disabled: error.maxLength}")
+        img.icon.add-icon(src="@/assets/add.svg")
+        span Add
+        Loader(:visible="loading.createCard")
+      .badge.label-badge.info-badge(v-if="keyboardShortcutTipIsVisible")
+        span Enter
+  .row(v-if="success")
+    .badge.success
+      span {{kaomoji}}, add another one?
+</template>
+
+<script>
+import inboxSpace from '@/data/inbox.json'
+import Loader from '@/components/Loader.vue'
+import User from '@/components/User.vue'
+import utils from '@/utils.js'
+
+import { nanoid } from 'nanoid'
+import sample from 'lodash-es/sample'
+
+export default {
+  name: 'AddToInbox',
+  components: {
+    Loader,
+    User
+  },
+  props: {
+    visible: Boolean
+  },
+  mounted () {
+    window.addEventListener('message', this.insertUrl)
+    this.$nextTick(() => {
+      this.focusAndSelectName()
+    })
+  },
+  beforeUnmount () {
+    window.removeEventListener('message', this.insertUrl)
+  },
+  data () {
+    return {
+      loading: {
+        createCard: false
+      },
+      error: {
+        maxLength: false,
+        unknownServerError: false
+      },
+      success: false,
+      newName: '',
+      keyboardShortcutTipIsVisible: false,
+      spaceUrlPath: 'inbox',
+      kaomoji: ''
+    }
+  },
+  computed: {
+    kinopioDomain () { return utils.kinopioDomain() },
+    cardsCreatedIsOverLimit () { return this.$store.getters['currentUser/cardsCreatedIsOverLimit'] },
+    maxCardLength () { return utils.maxCardLength() },
+    currentUser () { return this.$store.state.currentUser },
+    name: {
+      get () {
+        return this.newName
+      },
+      set (newName) {
+        this.newName = newName
+        this.textareaSizes()
+        this.clearErrorsAndSuccess()
+        this.updateMaxLengthError()
+      }
+    }
+
+  },
+  methods: {
+    updateKaomoji () {
+      const kaomojis = ['( ^_^)／', '( ﾟ▽ﾟ)/', '(^-^*)/', '＼(°o°；）', '( ･ω･)ﾉ', '( ・_・)ノ', '＼( ･_･)']
+      this.kaomoji = sample(kaomojis)
+    },
+    insertUrl (event) {
+      const url = event.data
+      this.newName = url + this.newName
+      this.$nextTick(() => {
+        this.textareaSizes()
+        this.focusAndSelectName()
+        this.updateMaxLengthError()
+      })
+    },
+    textareaSizes () {
+      const textarea = this.$refs.name
+      if (!textarea) { return }
+      let modifier = 0
+      if (this.canEditCard) {
+        modifier = 1
+      }
+      textarea.style.height = textarea.scrollHeight + modifier + 'px'
+    },
+
+    // url preview (from Card.vue)
+
+    async urlPreview (url) {
+      try {
+        let response = await this.$store.dispatch('api/urlPreview', url)
+        if (!response) { return }
+        let { data, host } = response
+        console.log('🚗 link preview', host, data)
+        const { links, meta } = data
+        return { links, meta }
+      } catch (error) {
+        console.warn('🚑 urlPreview', error, url)
+      }
+    },
+    previewImage ({ thumbnail }) {
+      const minWidth = 200
+      if (!thumbnail) { return '' }
+      let image = thumbnail.find(item => {
+        let shouldSkipImage = false
+        if (item.media) {
+          if (item.media.width < minWidth) {
+            shouldSkipImage = true
+          }
+        }
+        return item.href && !shouldSkipImage
+      })
+      if (!image) { return '' }
+      return image.href || ''
+    },
+    previewFavicon ({ icon }) {
+      if (!icon) { return '' }
+      let image = icon.find(item => item.href)
+      return image.href || ''
+    },
+
+    // card
+
+    focusName () {
+      const element = this.$refs.name
+      if (!element) { return }
+      element.focus()
+    },
+    focusAndSelectName () {
+      const element = this.$refs.name
+      if (!element) { return }
+      const length = element.value.length
+      this.focusName()
+      if (length) {
+        element.setSelectionRange(0, length)
+      }
+    },
+    emptyInboxSpace () {
+      const user = this.$store.state.currentUser
+      let space = utils.clone(inboxSpace)
+      space.id = nanoid()
+      space.userId = user.id
+      space.users = [user]
+      space.cards = space.cards.map(card => {
+        card.id = nanoid()
+        card.spaceId = space.id
+        return card
+      })
+      return space
+    },
+    async createCard () {
+      this.clearErrorsAndSuccess()
+      if (this.cardsCreatedIsOverLimit) { return }
+      if (this.error.maxLength) { return }
+      if (this.loading.createCard) { return }
+      if (!this.newName) { return }
+      this.loading.createCard = true
+      // url preview data
+      const url = utils.urlFromString(this.newName)
+      let urlPreview = {}
+      if (url) {
+        let { links, meta } = await this.urlPreview(url)
+        urlPreview = {
+          title: utils.truncated(meta.title || meta.site),
+          description: utils.truncated(meta.description, 280),
+          image: this.previewImage(links),
+          favicon: this.previewFavicon(links)
+        }
+      }
+      // create card
+      let card = {
+        id: nanoid(),
+        name: this.newName,
+        z: 1,
+        urlPreviewUrl: url,
+        urlPreviewTitle: urlPreview.title,
+        urlPreviewDescription: urlPreview.description,
+        urlPreviewImage: urlPreview.image,
+        urlPreviewFavicon: urlPreview.favicon
+      }
+      // inbox space
+      try {
+        let space = await this.$store.dispatch('currentUser/inboxSpace')
+        if (!space) {
+          space = this.emptyInboxSpace()
+          await this.$store.dispatch('api/createSpace', space)
+        }
+        // save card to inbox
+        const user = this.$store.state.currentUser
+        card.spaceId = space.id
+        card.userId = user.id
+        this.spaceUrlPath = space.id
+        console.log('🛫 create card', card)
+        this.$store.dispatch('api/addToQueue', { name: 'createCard', body: card, spaceId: space.id })
+        this.updateKaomoji()
+        this.success = true
+        this.newName = ''
+      } catch (error) {
+        console.error('🚑 createCard', error)
+        this.error.unknownServerError = true
+      }
+      this.loading.createCard = false
+      this.focusAndSelectName()
+    },
+    clearErrorsAndSuccess () {
+      this.error.unknownServerError = false
+      this.error.signInCredentials = false
+      this.success = false
+    },
+    updateMaxLengthError () {
+      if (this.newName.length >= this.maxCardLength - 1) {
+        this.error.maxLength = true
+      } else {
+        this.error.maxLength = false
+      }
+    },
+    insertLineBreak (event) {
+      const position = this.$refs.name.selectionEnd
+      const name = this.newName
+      const newName = name.substring(0, position) + '\n' + name.substring(position)
+      setTimeout(() => {
+        this.setSelectionRange(position + 1, position + 1)
+      })
+      this.insertedLineBreak = true
+      this.newName = newName
+      this.$nextTick(() => {
+        this.textareaSizes()
+      })
+    },
+
+    // handlers
+
+    updateKeyboardShortcutTipIsVisible (value) {
+      this.keyboardShortcutTipIsVisible = value
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+section.add-to-inbox
+  .info-badge
+    position static
+    display inline-block
+    min-height 16px
+    padding 0px 3px
+    vertical-align 0
+    margin-left 4px
+  .success
+    .row
+      margin-bottom 4px
+      &:last-child
+        margin-bottom 0
+
+  .info
+    margin 0
+    margin-bottom 10px
+    .badge
+      display inline-block
+      margin 0
+    > .badge
+      margin-right 6px
+
+  .sign-in
+    background var(--secondary-background)
+    form
+      margin-top 10px
+    input
+      margin-bottom 10px
+    button
+      margin 0
+  .inbox-icon,
+  .add-icon
+    vertical-align 0
+  a
+    color var(--primary)
+    text-decoration none
+  .textarea-wrap
+    margin-top 3px
+    margin-left 6px
+    textarea
+      margin 0
+
+  .button-wrap + .button-wrap
+    margin-left 6px
+
+</style>
