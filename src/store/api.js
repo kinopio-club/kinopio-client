660658799bc934041a31e22d8d0cca9594a87529
Pirijan <hi@pirijan.com>
2022-10-10 12:51:03 -0400

    noop remove iframely self-hosted service

--- src/store/api.js
+++ src/store/api.js
@@ -808,13 +808,8 @@ const self = {
     },
     urlPreview: async (context, url) => {
       try {
-        // let host = 'https://iframely.kinopio.club/iframely'
         const apiKey = '0788beaa34f65adc0fe7ac'
-        // const fallbackList = ['youtube', 'twitter', 'facebook', 'instagram', 'wikipedia', 'amazon', 'github']
-        // const shouldUseIFramely = fallbackList.find(item => url.includes(item))
-        // if (shouldUseIFramely) {
         const host = 'https://iframe.ly/api/iframely'
-        // }
         const response = await fetch(`${host}/?url=${encodeURIComponent(url)}&api_key=${apiKey}`)
         if (response.status !== 200) {
           throw new Error(response.status)
