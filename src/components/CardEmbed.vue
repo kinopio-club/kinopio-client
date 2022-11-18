f1b723fe7546e7c5a132b67e4623007732f344b2
Pirijan <hi@pirijan.com>
2022-11-18 17:02:46 -0500

    resize youtube embed resizes height (using 16:9 aspect ratio)

--- src/components/CardEmbed.vue
+++ src/components/CardEmbed.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.card-embed(v-if="visible")
+.card-embed(v-if="visible" :style="{'height': heightFromWidth}")
   iframe(allowfullscreen :src="url")
 </template>
 
@@ -8,15 +8,27 @@ export default {
   name: 'CardEmbed',
   props: {
     visible: Boolean,
-    url: String
+    url: String,
+    card: Object
+  },
+  computed: {
+    heightFromWidth () {
+      const width = this.card.width
+      const aspectRatio = 9 / 16
+      const height = Math.round(width * aspectRatio)
+      return height + 'px'
+    }
   }
 }
 </script>
 
 <style lang="stylus">
 .card-embed
+  width 100%
   iframe
     border none
     border-radius 3px
     max-width 100%
+    width 100%
+    height 100%
 </style>
