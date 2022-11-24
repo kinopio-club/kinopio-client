50987803bdf61f0baf8828a9d1789813fb9e0d56
Pirijan <hi@pirijan.com>
2022-11-24 15:18:11 -0500

    fix sticky cards, add utils.cursorPositionInSpace

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1063,11 +1063,7 @@ export default {
       const halfHeight = height / 2
       let centerX = this.x + halfWidth
       let centerY = this.y + halfHeight
-      let position = utils.cursorPositionInPage(event)
-      position = {
-        x: position.x * this.spaceCounterZoomDecimal,
-        y: position.y * this.spaceCounterZoomDecimal
-      }
+      let position = utils.cursorPositionInSpace({ event })
       // position from card center
       const xFromCenter = position.x - centerX
       const yFromCenter = position.y - centerY
@@ -1170,7 +1166,7 @@ export default {
       }
       event.stopPropagation()
       if (!this.canEditCard) {
-        const position = utils.cursorPositionInPage(event)
+        const position = utils.cursorPositionInSpace({ event })
         this.$store.commit('addNotificationWithPosition', { message: 'Card is Read Only', position, type: 'info', layer: 'space', icon: 'cancel' })
         return
       }
