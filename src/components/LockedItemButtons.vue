3621a5e970ab697690f69803a147a9eae27a5624
Pirijan <hi@pirijan.com>
2022-12-09 16:16:28 -0500

    noop dry

--- src/components/LockedItemButtons.vue
+++ src/components/LockedItemButtons.vue
@@ -12,6 +12,8 @@ template(v-for="card in lockedCards")
 import BoxUnlockButton from '@/components/BoxUnlockButton.vue'
 import CardUnlockButton from '@/components/CardUnlockButton.vue'
 
+import { mapState, mapGetters } from 'vuex'
+
 export default {
   name: 'LockedItemButtons',
   components: {
@@ -29,6 +31,12 @@ export default {
     visible: Boolean
   },
   computed: {
+    ...mapState([
+    ]),
+    ...mapGetters([
+      'currentScrollPosition',
+      'spaceZoomDecimal'
+    ]),
     lockedBoxes () { return this.$store.getters['currentBoxes/isLocked'] },
     lockedCards () { return this.$store.getters['currentCards/isLocked'] }
   },
@@ -42,13 +50,13 @@ export default {
     cardButtonPosition (card) {
       const element = document.querySelector(`article[data-card-id="${card.id}"] .lock-button-wrap`)
       if (!element) { return }
-      let rect = element.getBoundingClientRect()
-      const scroll = this.$store.getters.currentScrollPosition()
-      const zoom = this.$store.getters.spaceZoomDecimal
+      const currentScrollPosition = this.currentScrollPosition()
+      const zoom = this.spaceZoomDecimal
       const offset = {
-        x: scroll.x * zoom,
-        y: scroll.y * zoom
+        x: currentScrollPosition.x * zoom,
+        y: currentScrollPosition.y * zoom
       }
+      let rect = element.getBoundingClientRect()
       rect = {
         x: rect.x + offset.x,
         y: rect.y + offset.y
