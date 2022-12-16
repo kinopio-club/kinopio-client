2993cb0660d8e64bb10b9bb77251e48bebc0e371
Pirijan <hi@pirijan.com>
2022-12-16 11:36:42 -0500

    create paused gif during interaction

--- src/components/ImageOrVideo.vue
+++ src/components/ImageOrVideo.vue
@@ -4,12 +4,11 @@ video(v-if="Boolean(video)" autoplay loop muted playsinline :key="video" :class=
   source(:src="video")
 //- Image
 img.image(v-if="pendingUploadDataUrl" :src="pendingUploadDataUrl" :class="{selected: isSelectedOrDragging}" @load="updateDimensions")
-img.image(v-else-if="Boolean(image)" :src="image" :class="{selected: isSelectedOrDragging}" @load="updateDimensions")
+img.image(v-else-if="imageIsVisible" :src="image" :class="{selected: isSelectedOrDragging}" @load="updateDimensions" ref="image")
+canvas.image(v-else-if="pausedGifIsVisible" ref="pausedGif" :style="pausedGifStyles")
 </template>
 
 <script>
-// import utils from '@/utils.js'
-
 import { mapState, mapGetters } from 'vuex'
 
 export default {
@@ -26,32 +25,75 @@ export default {
   created () {
     this.$store.subscribe((mutation, state) => {
       if (mutation.type === 'triggerOptimizePerformanceDuringScrollOrZoom') {
-        this.checkIfShouldPauseVideo()
+        this.pause()
       }
     })
   },
-  mounted () {
-  },
-  beforeUnmount () {
-  },
   data () {
     return {
+      imageRect: {}
     }
   },
   computed: {
     ...mapState([
     ]),
     ...mapGetters([
-      'isInteractingScrollOrZoom'
-    ])
+      'isInteractingScrollOrZoom',
+      'spaceCounterZoomDecimal'
+    ]),
+    imageIsGif () {
+      if (!this.image) { return }
+      return this.image.includes('.gif')
+    },
+    imageIsVisible () {
+      if (this.imageIsGif) {
+        return !this.isInteractingScrollOrZoom
+      } else {
+        return this.image
+      }
+    },
+    pausedGifIsVisible () {
+      return this.isInteractingScrollOrZoom && this.imageIsGif
+    },
+    pausedGifStyles () {
+      return {
+        width: this.imageRect.width + 'px',
+        height: this.imageRect.height + 'px'
+      }
+    }
   },
   methods: {
     updateDimensions () {
       this.$store.dispatch('currentCards/updateDimensions', { cards: [this.card] })
     },
-    checkIfShouldPauseVideo () {
+    pause () {
+      this.pauseGif()
+      this.pauseVideo()
+    },
+    pauseGif () {
+      if (!this.isInteractingScrollOrZoom) { return }
+      if (!this.imageIsGif) { return }
+      const element = this.$refs.image
+      if (!element) { return }
+      const rect = element.getBoundingClientRect()
+      this.imageRect = {
+        width: rect.width * this.spaceCounterZoomDecimal,
+        height: rect.height * this.spaceCounterZoomDecimal
+      }
+      this.$nextTick(() => {
+        let canvas = this.$refs.pausedGif
+        canvas.width = this.imageRect.width * window.devicePixelRatio
+        canvas.height = this.imageRect.height * window.devicePixelRatio
+        const context = canvas.getContext('2d')
+        let image = new Image()
+        image.src = this.image
+        context.drawImage(image, 0, 0)
+      })
+    },
+    pauseVideo () {
       if (!this.video) { return }
       const element = this.$refs.video
+      if (!element) { return }
       if (this.isInteractingScrollOrZoom) {
         element.pause()
       } else {
@@ -61,7 +103,7 @@ export default {
   },
   watch: {
     isVisibleInViewport (value) {
-      this.checkIfShouldPauseVideo()
+      this.pause()
     }
   }
 }
