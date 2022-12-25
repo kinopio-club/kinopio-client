442b86906d1ab1fba190685274a20f399afa9a45
pirijan <hi@pirijan.com>
2022-12-25 14:46:02 -0500

    stub inbox cards

--- src/components/dialogs/AddToInbox.vue
+++ src/components/dialogs/AddToInbox.vue
@@ -1,10 +1,16 @@
 <template lang="pug">
 dialog.add-to-inbox(v-if="visible" :open="visible" @touchstart.stop.prevent @touchend.stop.prevent @click.left.stop ref="dialog")
   AddToInbox(:visible="true")
+  //- section if isloading
+  section(v-if="isCards && !currentSpaceIsInbox")
+    p blank slate, no cards
+    p list your recent inbox ccards, instructions to add. click a card to move it to this space
 </template>
 
 <script>
 import AddToInbox from '@/components/AddToInbox.vue'
+
+import { mapState, mapGetters } from 'vuex'
 
 export default {
   name: 'AddToInboxDialog',
@@ -14,10 +20,31 @@ export default {
   props: {
     visible: Boolean
   },
+  data () {
+    return {
+      cards: []
+    }
+  },
   computed: {
-    // inboxUrl () { return this.successSpaceId || 'inbox' },
+    ...mapState([
+      'currentSpace'
+    ]),
+    ...mapGetters([
+    ]),
+    isCards () {
+      return Boolean(this.cards.length)
+    },
+    currentSpaceIsInbox () {
+      return this.currentSpace.name === 'Inbox'
+    }
   },
   methods: {
+    moveCardToSpace () {
+      // get center vp + scroll
+      // api patch: update id w new card x,y, and spaceid
+      // commit the card to state: currentCards/create
+      // animate//highlight the card
+    }
   }
 }
 </script>
