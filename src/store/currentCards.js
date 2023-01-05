fb5c481a5057565a3c4795bb10abc709cfbebce0
Pirijan <hi@pirijan.com>
2023-01-05 17:11:02 -0500

    experimental temp fix, dont broadcast on update card dimensions

--- src/store/currentCards.js
+++ src/store/currentCards.js
@@ -426,8 +426,8 @@ const currentCards = {
             height: Math.ceil(card.height)
           }
           context.dispatch('api/addToQueue', { name: 'updateCard', body }, { root: true })
-          console.error('updateDimensions')
-          context.dispatch('broadcast/update', { updates: body, type: 'updateCard', handler: 'currentCards/update' }, { root: true })
+          // console.error('updateDimensions')
+          // context.dispatch('broadcast/update', { updates: body, type: 'updateCard', handler: 'currentCards/update' }, { root: true })
           context.commit('update', body)
           context.dispatch('currentConnections/updatePaths', { cardId: card.id, shouldUpdateApi: true }, { root: true })
           updateTallestCardHeight(card.height)
