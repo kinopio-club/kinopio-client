3d2fb6f17eb76ceb87fcf53be98198a3dacabb34
Pirijan <hi@pirijan.com>
2023-01-09 12:15:03 -0500

    add pricing dialog

---
+++ src/components/dialogs/Pricing.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+dialog.narrow.pricing(v-if="visible" :open="visible" @click.left.stop ref="dialog")
+  section
+    p Kinopio is free for 100 cards, afterwards it's $6/month or $60/year
+    table
+      tbody
+        tr.table-header
+          td Free
+          td Upgraded
+        tr
+          td 100 Cards
+          td Unlimited Cards
+
+  section
+    p Hi, my name is Pirijan and I'm the creator of Kinopio.
+    p I believe in building ethical, economically-sustainable,
+      span {{' '}}
+      a(href="https://pketh.org/organic-software.html") organic software
+      span {{' '}}
+      span that's designed by artists, built by craftspeople, and funded by the people who enjoy it.
+</template>
+
+<script>
+export default {
+  name: 'Pricing',
+  props: {
+    visible: Boolean
+  }
+}
+</script>
+
+<style lang="stylus">
+.pricing
+  top calc(100% - 8px)
+  left initial
+  right 8px
+  max-height calc(100vh - 25px)
+  p
+    user-select text
+</style>
