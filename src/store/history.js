32291683475878133d7b0756e3b07151cefb8244
pirijan <hi@pirijan.com>
2023-02-01 15:10:09 -0500

    undo/redo card name change resets card dimensions

--- src/store/history.js
+++ src/store/history.js
@@ -220,7 +220,7 @@ const self = {
             card = item.prev
             context.dispatch('currentCards/update', card, { root: true })
             nextTick(() => {
-              context.dispatch('currentCards/updateDimensions', { cardId: card.id }, { root: true })
+              context.dispatch('currentCards/resetDimensions', { cardId: card.id }, { root: true })
               context.dispatch('currentConnections/updatePaths', { cardId: card.id }, { root: true })
             })
             break
@@ -289,7 +289,7 @@ const self = {
             card = item.new
             context.dispatch('currentCards/update', card, { root: true })
             nextTick(() => {
-              context.dispatch('currentCards/updateDimensions', { cardId: card.id }, { root: true })
+              context.dispatch('currentCards/resetDimensions', { cardId: card.id }, { root: true })
               context.dispatch('currentConnections/updatePaths', { cardId: card.id }, { root: true })
             })
             break
