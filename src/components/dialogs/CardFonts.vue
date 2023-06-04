d6eb1656e6a0766a35a27635691424cca0e0aa57
Pirijan <hi@pirijan.com>
2023-06-04 10:21:07 -0400

    add current h1,h2,h3 preview

--- src/components/dialogs/CardFonts.vue
+++ src/components/dialogs/CardFonts.vue
@@ -1,48 +1,81 @@
 <script setup>
-// import utils from '@/utils.js'
+import utils from '@/utils.js'
+import NameSegment from '@/components/NameSegment.vue'
 
 import { reactive, computed, onMounted, defineProps, defineEmits, watch, ref, nextTick } from 'vue'
-// https://vuex.vuejs.org/guide/composition-api.html#accessing-state-and-getters
 import { useStore } from 'vuex'
 const store = useStore()
 
-onMounted(() => {
-  console.log(`🍆 the dialog is now mounted.`, store.state.currentSpace)
-})
-
 const props = defineProps({
-  visible: Boolean
+  visible: Boolean,
+  card: Object
 })
-const emit = defineEmits(['updateCount'])
-
+// const emit = defineEmits(['updateCount'])
 watch(() => props.visible, (value, prevValue) => {
   if (value) {
-    console.log('💁‍♀️', value)
+    updateSegments()
   }
 })
 
+// preview name segments
+
 const state = reactive({
-  count: 0
+  segments: []
+})
+const updateSegments = () => {
+  const markdownSegments = utils.markdownSegments(props.card.name)
+  let segments = []
+  markdownSegments.forEach(segment => {
+    const { type, content } = segment
+    if (!content.trim()) { return }
+    const types = ['h1', 'h2', 'h3']
+    if (types.includes(type)) {
+      segment.isText = true
+      segment.markdown = [segment]
+      segments.push(segment)
+    }
+    state.segments = segments
+  })
+}
+
+// styles
+
+const backgroundColorIsDark = () => {
+  const color = props.card.backgroundColor
+  if (color) {
+    return utils.colorIsDark(color)
+  } else {
+    return store.state.currentUser.theme === 'dark'
+  }
+}
+const styles = computed(() => {
+  let styles = {
+    backgroundColor: props.card.backgroundColor
+  }
+  if (backgroundColorIsDark()) {
+    styles.color = utils.cssVariable('primary-on-dark-background')
+  }
+  return styles
 })
 
-const themeName = computed(() => store.state.currentUser.theme)
-const incrementBy = () => {
-  state.count = state.count + 1
-  emit('updateCount', state.count)
-  // store.dispatch('themes/isSystem', false)
-}
 </script>
 
 <template lang="pug">
-dialog.narrow.dialog-name(v-if="visible" :open="visible" @click.left.stop ref="dialog")
+dialog.narrow.card-fonts(v-if="visible" :open="visible" @click.left.stop ref="dialog")
+  section.preview(:style="styles")
+    template(v-for="segment in state.segments")
+      NameSegment(:segment="segment" :parentCardId="props.card.id")
   section
-    p blank dialog, please duplicate
-  section
-    button(@click="incrementBy")
-      span Count is: {{ state.count }}
-    p Current theme is: {{ themeName }}
+    p add fonts list
+
 </template>
 
 <style lang="stylus">
-// .dialog-name
+.card-fonts
+  .name-segment
+    display block
+  section.preview
+    overflow hidden
+    max-height 200px
+    background-color var(--secondary-background)
 </style>
