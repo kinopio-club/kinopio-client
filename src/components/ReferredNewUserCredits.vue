7c1c4e81fe8126a93656f892e5805711e01c92b6
Pirijan <hi@pirijan.com>
2023-07-18 11:08:46 -0400

    hide ReferredNewUserCredits on ios app

--- src/components/ReferredNewUserCredits.vue
+++ src/components/ReferredNewUserCredits.vue
@@ -9,12 +9,14 @@ const referralCreditAmount = computed(() => consts.referralCreditAmount)
 const referredByUserId = computed(() => store.state.currentUser.referredByUserId)
 const currentUserIsSignedIn = computed(() => store.getters['currentUser/isSignedIn'])
 const referrerName = computed(() => store.state.currentUser.referrerName)
+const isSecureAppContextIOS = computed(() => consts.isSecureAppContextIOS)
 
 </script>
 
 <template lang="pug">
-p.badge.success(v-if="referrerName && !currentUserIsSignedIn") welcome {{referrerName}}, once you sign up your account will be upgraded to free
-p.badge.success(v-else-if="referredByUserId && !currentUserIsSignedIn") Signing up will earn you ${{referralCreditAmount}} in referral credits
+template(v-if="!isSecureAppContextIOS")
+  p.badge.success(v-if="referrerName && !currentUserIsSignedIn") welcome {{referrerName}}, once you sign up your account will be upgraded to free
+  p.badge.success(v-else-if="referredByUserId && !currentUserIsSignedIn") Signing up will earn you ${{referralCreditAmount}} in referral credits
 </template>
 
 <style lang="stylus">
