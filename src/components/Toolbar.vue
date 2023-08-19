63e39925c8f7378c72ba874aba84fb5571f589ad
pirijan <hi@pirijan.com>
2023-08-19 18:30:49 -0400

    noop refactor: toolbar to composition

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,50 +1,63 @@
+<script setup>
+import { reactive, computed, onMounted, defineProps, defineEmits, watch, ref, nextTick } from 'vue'
+import { useStore } from 'vuex'
+const store = useStore()
+
+onMounted(() => {
+  store.subscribe((mutation, state) => {
+    if (mutation.type === 'currentUserToolbar') {
+      store.dispatch('closeAllDialogs')
+      store.dispatch('clearMultipleSelected')
+    }
+  })
+})
+
+const props = defineProps({
+  visible: Boolean
+})
+
+const visible = computed(() => props.visible && canEditSpace.value)
+const canEditSpace = computed(() => store.getters['currentUser/canEditSpace']())
+
+// toolbar state
+
+const currentUserToolbar = computed(() => store.state.currentUserToolbar)
+const currentUserToolbarIsBox = computed(() => {
+  if (store.state.currentUserIsResizingBox) { return }
+  return store.state.currentUserToolbar === 'box'
+})
+const toggleToolbar = (value) => {
+  const initialValue = 'card'
+  const shouldToggleOffBox = value === 'box' && currentUserToolbarIsBox.value
+  if (shouldToggleOffBox) {
+    store.commit('currentUserToolbar', initialValue)
+  } else {
+    store.commit('currentUserToolbar', value)
+  }
+}
+
+// line
+
+const addLine = () => {
+  store.dispatch('closeAllDialogs')
+  store.dispatch('clearMultipleSelected')
+  console.log('♥️♥️♥️♥️')
+}
+
+</script>
+
 <template lang="pug">
 nav.toolbar(v-if="visible")
   .segmented-buttons.vertical
     //- Box
-    button(v-if="canEditSpace" :class="{ active: currentUserToolbarIsBox }" @click="toggleToolbar('box')" title="Add Box (B)")
+    button(:class="{ active: currentUserToolbarIsBox }" @click="toggleToolbar('box')" title="Add Box (B)")
       img.icon.box-icon(src="@/assets/box.svg")
       .label-badge.toolbar-badge-wrap.jiggle(v-if="currentUserToolbarIsBox")
         span Add Box (B)
     //- Line
-    button(title="Add Line (L)")
+    button(@click="addLine" title="Add Line (L)")
       img.icon(src="@/assets/line.svg")
 </template>
-
-<script>
-export default {
-  name: 'Toolbar',
-  props: {
-    visible: Boolean
-  },
-  computed: {
-    canEditSpace () { return this.$store.getters['currentUser/canEditSpace']() },
-    currentUserToolbar () { return this.$store.state.currentUserToolbar },
-    // currentUserToolbarIsCard () { return this.currentUserToolbar === 'card' },
-    currentUserToolbarIsBox () {
-      if (this.$store.state.currentUserIsResizingBox) { return }
-      return this.currentUserToolbar === 'box'
-    }
-  },
-  methods: {
-    toggleToolbar (value) {
-      const initialValue = 'card'
-      const shouldToggleOffBox = value === 'box' && this.currentUserToolbarIsBox
-      if (shouldToggleOffBox) {
-        this.$store.commit('currentUserToolbar', initialValue)
-      } else {
-        this.$store.commit('currentUserToolbar', value)
-      }
-    }
-  },
-  watch: {
-    currentUserToolbar (value) {
-      this.$store.dispatch('closeAllDialogs')
-      this.$store.dispatch('clearMultipleSelected')
-    }
-  }
-}
-</script>
 
 <style lang="stylus">
 .toolbar
@@ -61,5 +74,4 @@ export default {
     span
       width 100%
       color var(--primary)
-
 </style>
