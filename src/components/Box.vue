f60860a42b07e76030fc7d793dc9f64b1467ea68
Pirijan <hi@pirijan.com>
2023-10-25 19:55:25 -0400

    stub save box info size

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -86,6 +86,12 @@ export default {
   props: {
     box: Object
   },
+  mounted () {
+    const element = document.querySelector(`.box-info[data-box-id="${this.box.id}"]`)
+    const DOMRect = element.getBoundingClientRect()
+    console.log('🍇box', this.box.id, this.box.infoWidth, DOMRect.width)
+    // operation updateBox  w width
+  },
   data () {
     return {
       isHover: false,
