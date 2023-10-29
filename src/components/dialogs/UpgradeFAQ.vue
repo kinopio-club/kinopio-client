8db3cc76eb29d9447d5c14c7c12b293e2173dd27
pirijan <hi@pirijan.com>
2023-10-28 21:54:36 -0400

    add closeOtherDetails

--- src/components/dialogs/UpgradeFAQ.vue
+++ src/components/dialogs/UpgradeFAQ.vue
@@ -10,6 +10,18 @@ const dialog = ref(null)
 const props = defineProps({
   visible: Boolean
 })
+
+const closeOtherDetails = (event) => {
+  const summaries = document.querySelectorAll('summary')
+  const target = event.target
+  summaries.forEach(summary => {
+    if (summary !== target) {
+      const details = summary.closest('details')
+      details.removeAttribute('open')
+    }
+  })
+}
+
 </script>
 
 <template lang="pug">
@@ -18,20 +30,19 @@ dialog.narrow.upgrade-faq(v-if="visible" :open="visible" @click.left.stop ref="d
     p Upgrade FAQ
   section.results-section
     details
-      summary What happens if I cancel my subscription?
+      summary(@click="closeOtherDetails") What happens if I cancel my subscription?
       section.subsection
         p You'll always be able to view the cards and spaces that you create. But you won't be able to create new cards beyond the free limit.
 
     details
-      summary Is it easy to cancel?
+      summary(@click="closeOtherDetails") Is it easy to cancel?
       section.subsection
         p Yes. You can cancel anytime through User → Settings → Billing and Credits
 
     details
-      summary How long is the life plan?
+      summary(@click="closeOtherDetails") How long is the life plan?
       section.subsection
-        p The life plan is a perpetual licence for as long as Kinopio operates.
-        p While Kinopio requires a webserver, it was designed to be inexpensive to host beyond the foreseeable future.
+        p The life plan is a perpetual licence for as long as Kinopio operates. Which will be for many years, likely decades.
 </template>
 
 <style lang="stylus">
