1ef190f66fdf85231fa8b989ed248339a3b2d9e5
Pirijan <hi@pirijan.com>
2023-11-01 10:27:40 -0400

    prefer image urls in name > urlpreview img

--- src/components/Screenshot.vue
+++ src/components/Screenshot.vue
@@ -150,15 +150,15 @@ const drawCards = async () => {
   }
 }
 const cardImageUrl = (card) => {
-  // url preview image
-  const imageUrlIsUrlPreview = card.urlPreviewImage && card.urlPreviewIsVisible && !card.shouldHideUrlPreviewImage
-  if (imageUrlIsUrlPreview) {
-    return card.urlPreviewImage
-  }
   // image url in card name
   const urls = utils.urlsFromString(card.name)
   if (urls) {
     return urls.find(url => utils.urlIsImage(url))
+  }
+  // url preview image
+  const imageUrlIsUrlPreview = card.urlPreviewImage && card.urlPreviewIsVisible && !card.shouldHideUrlPreviewImage
+  if (imageUrlIsUrlPreview) {
+    return card.urlPreviewImage
   }
 }
 const drawCardImage = async (card) => {
