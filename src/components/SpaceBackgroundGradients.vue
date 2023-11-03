92bb02ccfc88454b49db7647c03c35fc243cb36f
Pirijan <hi@pirijan.com>
2023-11-03 11:58:40 -0400

    randomly generate background gradients

--- src/components/SpaceBackgroundGradients.vue
+++ src/components/SpaceBackgroundGradients.vue
@@ -1,24 +1,164 @@
 <script setup>
 import { reactive, computed, onMounted, defineProps, defineEmits, watch, ref, nextTick } from 'vue'
 import { useStore } from 'vuex'
+import random from 'lodash-es/random'
+import randomColor from 'randomcolor'
+import { colord, extend } from 'colord'
+
 const store = useStore()
 
+onMounted(() => {
+  generate()
+})
 const props = defineProps({
   visible: Boolean
 })
 
-// const themeName = computed(() => store.state.currentUser.theme)
+const state = reactive({
+  layer1: {
+    positionX: 0,
+    positionY: 0,
+    color1: '',
+    color2: ''
+  },
+  layer2: {
+    positionX: 0,
+    positionY: 0,
+    color1: '',
+    color2: ''
+  },
+  layer3: {
+    positionX: 0,
+    positionY: 0,
+    color1: '',
+    color2: ''
+  },
+  layer4: {
+    positionX: 0,
+    positionY: 0,
+    color1: '',
+    color2: ''
+  },
+  layer5: {
+    positionX: 0,
+    positionY: 0,
+    color1: '',
+    color2: ''
+  },
+  layer6: {
+    positionX: 0,
+    positionY: 0,
+    color1: '',
+    color2: ''
+  },
+  layer7: {
+    color: ''
+  }
+})
+
+// generate new
+
+const randomRGBA = (alpha) => {
+  const hex = randomColor({ hue: 'random', luminosity: 'random' })
+  // const alpha = random(1, true)
+  const rgba = colord(hex).alpha(alpha).toRgbString()
+  return rgba
+}
+const generate = () => {
+  const gradientLayers = ['layer1', 'layer2', 'layer3', 'layer4', 'layer5', 'layer6']
+  const backgroundLayer = 'layer7'
+  gradientLayers.forEach(layer => generateLayer(layer))
+  console.log('🐸 background gradient layers', state)
+  state[backgroundLayer].color = randomRGBA(1)
+}
+const generateLayer = (layer) => {
+  state[layer].positionX = random(140, true)
+  state[layer].positionY = random(140, true)
+  state[layer].color1 = randomRGBA(1)
+  state[layer].color2 = randomRGBA(0)
+}
+
+// layers
+
+const stylesLayer1 = computed(() => {
+  const layer = 'layer1'
+  const positionX = state[layer].positionX
+  const positionY = state[layer].positionY
+  const color1 = state[layer].color1
+  const color2 = state[layer].color2
+  return {
+    background: `radial-gradient(at ${positionX}% ${positionY}%, ${color1} 0%, ${color2} 80%)`
+  }
+})
+const stylesLayer2 = computed(() => {
+  const layer = 'layer2'
+  const positionX = state[layer].positionX
+  const positionY = state[layer].positionY
+  const color1 = state[layer].color1
+  const color2 = state[layer].color2
+  return {
+    background: `radial-gradient(at ${positionX}% ${positionY}%, ${color1} 0%, ${color2} 80%)`
+  }
+})
+const stylesLayer3 = computed(() => {
+  const layer = 'layer3'
+  const positionX = state[layer].positionX
+  const positionY = state[layer].positionY
+  const color1 = state[layer].color1
+  const color2 = state[layer].color2
+  return {
+    background: `radial-gradient(at ${positionX}% ${positionY}%, ${color1} 0%, ${color2} 80%)`
+  }
+})
+
+const stylesLayer4 = computed(() => {
+  const layer = 'layer4'
+  const positionX = state[layer].positionX
+  const positionY = state[layer].positionY
+  const color1 = state[layer].color1
+  const color2 = state[layer].color2
+  return {
+    background: `radial-gradient(at ${positionX}% ${positionY}%, ${color1} 0%, ${color2} 80%)`
+  }
+})
+const stylesLayer5 = computed(() => {
+  const layer = 'layer5'
+  const positionX = state[layer].positionX
+  const positionY = state[layer].positionY
+  const color1 = state[layer].color1
+  const color2 = state[layer].color2
+  return {
+    background: `radial-gradient(at ${positionX}% ${positionY}%, ${color1} 0%, ${color2} 80%)`
+  }
+})
+const stylesLayer6 = computed(() => {
+  const layer = 'layer6'
+  const positionX = state[layer].positionX
+  const positionY = state[layer].positionY
+  const color1 = state[layer].color1
+  const color2 = state[layer].color2
+  return {
+    background: `radial-gradient(at ${positionX}% ${positionY}%, ${color1} 0%, ${color2} 80%)`
+  }
+})
+const stylesLayer7 = computed(() => {
+  const layer = 'layer7'
+  return {
+    background: state[layer].color
+  }
+})
+
 </script>
 
 <template lang="pug">
 .space-background-gradients(v-if="visible")
-  .gradients-layer.layer-1(style='background: radial-gradient(at 35.211765% 134.309804%, rgb(92, 37, 245) 0%, rgba(255, 255, 255, 0) 80%); ')
-  .gradients-layer.layer-2(style='background: radial-gradient(at 64.035294% 80.505882%, rgb(155, 79, 147) 0%, rgba(255, 255, 255, 0) 80%); ')
-  .gradients-layer.layer-3(style='background: radial-gradient(at 100.545098% 117.290196%, rgb(182, 185, 214) 0%, rgba(255, 255, 255, 0) 80%); ')
-  .gradients-layer.layer-4(style='background: radial-gradient(at 48.662745% 31.643137%, rgb(58, 120, 58) 0%, rgba(255, 255, 255, 0) 80%); ')
-  .gradients-layer.layer-5(style='background: radial-gradient(at 70.623529% 39.329412%, rgb(15, 243, 72) 0%, rgba(255, 255, 255, 0) 80%); ')
-  .gradients-layer.layer-6(style='background: radial-gradient(at 116.741176% 62.388235%, rgb(224, 202, 114) 0%, rgba(255, 255, 255, 0) 80%); ')
-  .gradients-layer.layer-7(style='background: rgb(116, 38, 247);')
+  .gradients-layer.layer-1(:style="stylesLayer1")
+  .gradients-layer.layer-2(:style="stylesLayer2")
+  .gradients-layer.layer-3(:style="stylesLayer3")
+  .gradients-layer.layer-4(:style="stylesLayer4")
+  .gradients-layer.layer-5(:style="stylesLayer5")
+  .gradients-layer.layer-6(:style="stylesLayer6")
+  .gradients-layer.layer-7(:style="stylesLayer7")
 </template>
 
 <style lang="stylus">
@@ -33,12 +173,10 @@ const props = defineProps({
   background var(--primary-background)
 
   .gradients-layer
-    // 7 layers
     position absolute
     height 100%
     width 100%
-    transition opacity 8000ms
-    // background: radial-gradient(at 35.211765% 134.309804%, rgb(92, 37, 245) 0%, rgba(255, 255, 255, 0) 80%);
+    transition opacity 4000ms
 
   .layer-1
     opacity 0
@@ -59,8 +197,7 @@ const props = defineProps({
     opacity 1
     z-index 95
   .layer-7
-    // non radial gradient
-    transition background 4000ms
+    transition background 2000ms
     opacity 1
     z-index 5
 </style>
