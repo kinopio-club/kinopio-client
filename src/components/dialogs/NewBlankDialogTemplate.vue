bfb897cbf65b92ac366175d43d7ad5ef36e3dd23
Pirijan <hi@pirijan.com>
2023-11-07 10:51:12 -0500

    noop add onUnmounted to blank templates

--- src/components/dialogs/NewBlankDialogTemplate.vue
+++ src/components/dialogs/NewBlankDialogTemplate.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive, computed, onMounted, defineProps, defineEmits, watch, ref, nextTick } from 'vue'
+import { reactive, computed, onMounted, onUnmounted, defineProps, defineEmits, watch, ref, nextTick } from 'vue'
 import { useStore } from 'vuex'
 
 import utils from '@/utils.js'
