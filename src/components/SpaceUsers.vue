e44894bcd5bc69b596a4cbde28c34e50efa08740
Pirijan <hi@pirijan.com>
2023-11-17 11:03:38 -0500

    spaceusers -> composition

--- src/components/SpaceUsers.vue
+++ src/components/SpaceUsers.vue
@@ -1,3 +1,42 @@
+<script setup>
+import { reactive, computed, onMounted, onUnmounted, defineProps, defineEmits, watch, ref, nextTick, defineAsyncComponent } from 'vue'
+import { useStore } from 'vuex'
+
+import utils from '@/utils.js'
+
+import uniqBy from 'lodash-es/uniqBy'
+const User = defineAsyncComponent({
+  loader: () => import('@/components/User.vue')
+})
+const store = useStore()
+
+const props = defineProps({
+  userDetailsIsInline: Boolean
+})
+
+const isEmbedMode = computed(() => store.state.isEmbedMode)
+const isAddPage = computed(() => store.state.isAddPage)
+const currentUser = computed(() => store.state.currentUser)
+const currentSpace = computed(() => store.state.currentSpace)
+const currentUserIsSpaceMember = computed(() => store.getters['currentUser/isSpaceMember']())
+
+const members = computed(() => currentSpace.value.users)
+const users = computed(() => {
+  let users = utils.clone(currentSpace.value.users)
+  return users.filter(user => user.id !== currentUser.value.id)
+})
+const collaborators = computed(() => {
+  let collaborators = currentSpace.value.collaborators
+  return collaborators.filter(user => user.id !== currentUser.value.id)
+})
+const spectators = computed(() => {
+  let spectators = currentSpace.value.spectators
+  spectators = spectators.filter(user => user.id !== currentUser.value.id)
+  spectators = uniqBy(spectators, 'id')
+  return spectators
+})
+</script>
+
 <template lang="pug">
 //- Add Page
 .space-users(v-if="isAddPage")
@@ -16,50 +55,7 @@
     User(v-for="user in collaborators" :user="user" :isClickable="true" :detailsOnRight="true" :key="user.id" :shouldCloseAllDialogs="true" tabindex="0")
     User(v-for="user in users" :user="user" :isClickable="true" :detailsOnRight="true" :key="user.id" :shouldCloseAllDialogs="true" tabindex="0")
     User(v-if="currentUserIsSpaceMember" :user="currentUser" :isClickable="true" :detailsOnRight="true" :key="currentUser.id" :shouldCloseAllDialogs="true" tabindex="0")
-
 </template>
-
-<script>
-import utils from '@/utils.js'
-
-import { defineAsyncComponent } from 'vue'
-
-import uniqBy from 'lodash-es/uniqBy'
-const User = defineAsyncComponent({
-  loader: () => import('@/components/User.vue')
-})
-
-export default {
-  name: 'TagList',
-  components: {
-    User
-  },
-  computed: {
-    isEmbedMode () { return this.$store.state.isEmbedMode },
-    isAddPage () { return this.$store.state.isAddPage },
-    currentUser () { return this.$store.state.currentUser },
-    currentSpace () { return this.$store.state.currentSpace },
-    currentUserIsSpaceMember () { return this.$store.getters['currentUser/isSpaceMember']() },
-    members () {
-      return this.currentSpace.users
-    },
-    users () {
-      let users = utils.clone(this.currentSpace.users)
-      return users.filter(user => user.id !== this.currentUser.id)
-    },
-    collaborators () {
-      let collaborators = this.currentSpace.collaborators
-      return collaborators.filter(user => user.id !== this.currentUser.id)
-    },
-    spectators () {
-      let spectators = this.currentSpace.spectators
-      spectators = spectators.filter(user => user.id !== this.currentUser.id)
-      spectators = uniqBy(spectators, 'id')
-      return spectators
-    }
-  }
-}
-</script>
 
 <style lang="stylus">
 .space-users
@@ -71,5 +67,4 @@ export default {
     flex-wrap wrap
     justify-content flex-end
     align-content flex-start
-
 </style>
