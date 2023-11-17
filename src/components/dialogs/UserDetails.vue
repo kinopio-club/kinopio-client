f6f045e6c972fb634e58f88fdcd3e1209afca3e8
Pirijan <hi@pirijan.com>
2023-11-17 09:49:22 -0500

    userdetails -> to composition

--- src/components/dialogs/UserDetails.vue
+++ src/components/dialogs/UserDetails.vue
@@ -1,14 +1,240 @@
-<template lang="pug">
-dialog.narrow.user-details(v-if="visible" @keyup.stop :open="visible" @click.left.stop="closeDialogs" @keydown.stop :style="styles" ref="dialog")
+<script setup>
+import { reactive, computed, onMounted, onUnmounted, defineProps, defineEmits, watch, ref, nextTick, defineAsyncComponent } from 'vue'
+import { useStore } from 'vuex'
 
-  //- Not Current User
+import ColorPicker from '@/components/dialogs/ColorPicker.vue'
+import SpacePicker from '@/components/dialogs/SpacePicker.vue'
+import Loader from '@/components/Loader.vue'
+import UserBadges from '@/components/UserBadges.vue'
+import UserLabelInline from '@/components/UserLabelInline.vue'
+import cache from '@/cache.js'
+import utils from '@/utils.js'
+import postMessage from '@/postMessage.js'
+const User = defineAsyncComponent({
+  loader: () => import('@/components/User.vue')
+})
+const store = useStore()
+
+const dialogElement = ref(null)
+const descriptionElement = ref(null)
+
+onMounted(() => {
+  store.subscribe((mutation, state) => {
+    if (mutation.type === 'triggerScrollUserDetailsIntoView' && visible.value) {
+      scrollUserDetailsIntoView()
+    }
+  })
+})
+
+const state = reactive({
+  colorPickerIsVisible: false,
+  loadingUserspaces: false,
+  spacePickerIsVisible: false,
+  userSpaces: [],
+  error: {
+    unknownServerError: false
+  }
+})
+
+const visible = computed(() => store.state.userDetailsIsVisible)
+watch(() => visible.value, (value, prevValue) => {
+  closeDialogs()
+  clearUserSpaces()
+  if (value) {
+    updateTextareaSize()
+  }
+})
+const userDetailsPosition = computed(() => store.state.userDetailsPosition)
+watch(() => userDetailsPosition.value, (value, prevValue) => {
+  closeDialogs()
+})
+const styles = computed(() => {
+  let { x, y, shouldIgnoreZoom, transformOriginIsTopRight } = userDetailsPosition.value
+  let zoom = spaceCounterZoomDecimal.value
+  if (shouldIgnoreZoom) {
+    zoom = 1
+  }
+  if (store.state.isTouchDevice) {
+    zoom = utils.pinchCounterZoomDecimal()
+    if (zoom > 1) {
+      x = x * zoom
+      y = y * zoom
+    }
+  }
+  const styles = {
+    transform: `scale(${zoom})`,
+    left: x + 'px',
+    top: y + 'px'
+  }
+  if (transformOriginIsTopRight) {
+    styles.transformOrigin = 'top right'
+  }
+  return styles
+})
+const updateTextareaSize = async () => {
+  await nextTick()
+  let textarea = descriptionElement.value
+  if (!textarea) { return }
+  textarea.style.height = textarea.scrollHeight + 1 + 'px'
+}
+const spaceCounterZoomDecimal = computed(() => store.getters.spaceCounterZoomDecimal)
+const isLoadingFavorites = computed(() => store.state.isLoadingFavorites)
+const userSettingsIsVisible = computed(() => store.state.userSettingsIsVisible)
+const closeDialogs = () => {
+  state.colorPickerIsVisible = false
+  state.spacePickerIsVisible = false
+}
+
+// update state
+
+const toggleUserSettingsIsVisible = () => {
+  const value = !store.state.userSettingsIsVisible
+  store.dispatch('closeAllDialogs')
+  store.commit('userSettingsIsVisible', value)
+}
+const toggleColorPicker = () => {
+  const isVisible = state.colorPickerIsVisible
+  closeDialogs()
+  state.colorPickerIsVisible = !isVisible
+}
+const triggerSignUpOrInIsVisible = () => {
+  store.dispatch('closeAllDialogs')
+  store.commit('triggerSignUpOrInIsVisible')
+}
+const changeSpace = (space) => {
+  store.dispatch('currentSpace/changeSpace', space)
+}
+const scrollUserDetailsIntoView = async () => {
+  await nextTick()
+  const element = dialogElement.value
+  utils.scrollIntoView({ element })
+}
+
+// current user
+
+const currentUserIsSpaceMember = computed(() => store.getters['currentUser/isSpaceMember']())
+const currentUserIsSignedIn = computed(() => store.getters['currentUser/isSignedIn'])
+const signOut = () => {
+  postMessage.send({ name: 'onLogout' })
+  cache.removeAll()
+  // clear history wipe state from vue-router
+  window.history.replaceState({}, 'Kinopio', '/')
+  location.reload()
+}
+
+// user info
+
+const user = computed(() => store.state.userDetailsUser)
+const userColor = computed(() => user.value.color)
+const userIsMember = computed(() => Boolean(store.getters['currentSpace/memberById'](user.value.id)))
+const userIsUpgraded = computed(() => user.value.isUpgraded)
+const isCurrentUser = computed(() => store.getters['currentUser/isCurrentUser'](user.value))
+const userIsSignedIn = computed(() => {
+  if (user.value.isSignedIn === false) {
+    return false
+  }
+  return true
+})
+const userName = computed({
+  get () {
+    return user.value.name
+  },
+  set (newValue) {
+    updateUser({ name: newValue })
+  }
+})
+const userDescription = computed({
+  get () {
+    return user.value.description
+  },
+  set (newValue) {
+    updateUser({ description: newValue })
+    updateTextareaSize()
+  }
+})
+const userWebsite = computed({
+  get () {
+    return user.value.website
+  },
+  set (newValue) {
+    updateUser({ website: newValue })
+  }
+})
+const websiteUrl = computed(() => {
+  if (!userWebsite.value) { return }
+  const urls = utils.urlsFromString(userWebsite.value)
+  if (!urls) { return }
+  return urls[0]
+})
+const isFavoriteUser = computed(() => {
+  const favoriteUsers = store.state.currentUser.favoriteUsers
+  const isFavoriteUser = Boolean(favoriteUsers.find(favoriteUser => {
+    return favoriteUser.id === user.value.id
+  }))
+  return isFavoriteUser
+})
+const isCollaborator = computed(() => {
+  const currentSpace = store.state.currentSpace
+  const collaborators = currentSpace.collaborators || []
+  return Boolean(collaborators.find(collaborator => {
+    return collaborator.id === user.value.id
+  }))
+})
+const getUserSpaces = async () => {
+  state.error.unknownServerError = false
+  if (state.loadingUserspaces) { return }
+  if (state.spacePickerIsVisible) {
+    closeDialogs()
+    return
+  }
+  state.loadingUserspaces = true
+  state.spacePickerIsVisible = true
+  try {
+    const publicUser = await store.dispatch('api/getPublicUser', user.value)
+    state.userSpaces = publicUser.spaces
+  } catch (error) {
+    state.error.unknownServerError = true
+    clearUserSpaces()
+  }
+  state.loadingUserspaces = false
+}
+const clearUserSpaces = () => {
+  state.loadingUserspaces = false
+  state.userSpaces = []
+}
+
+// update user
+
+const updateUser = (update) => {
+  store.dispatch('currentUser/update', update)
+}
+const toggleIsFavoriteUser = () => {
+  if (isFavoriteUser.value) {
+    store.dispatch('currentUser/removeFavorite', { type: 'user', item: user.value })
+  } else {
+    store.dispatch('currentUser/addFavorite', { type: 'user', item: user.value })
+  }
+}
+const updateUserColor = (newValue) => {
+  updateUser({ color: newValue })
+}
+const removeCollaborator = () => {
+  store.dispatch('currentSpace/removeCollaboratorFromSpace', user.value)
+  store.dispatch('closeAllDialogs')
+}
+</script>
+
+<template lang="pug">
+dialog.narrow.user-details(v-if="visible" @keyup.stop :open="visible" @click.left.stop="closeDialogs" @keydown.stop :style="styles" ref="dialogElement")
+
+  //- Other User
   section(v-if="!isCurrentUser")
     .user-info
       .row
         User(:user="user" :isClickable="false" :detailsOnRight="false" :key="user.id")
         p.name.user-details-name {{user.name}}
       .row(v-if="user.description")
-        textarea(ref="description" :value="user.description" disabled)
+        textarea(ref="descriptionElement" :value="user.description" disabled)
       .row.website(v-if="user.website")
         p(v-if="!websiteUrl") {{user.website}}
         a(:href="websiteUrl" v-if="websiteUrl")
@@ -20,9 +246,9 @@ dialog.narrow.user-details(v-if="visible" @keyup.stop :open="visible" @click.lef
     section.current-user
       .row
         .button-wrap
-          button.change-color(@click.left.stop="toggleColorPicker" :class="{active: colorPickerIsVisible}")
+          button.change-color(@click.left.stop="toggleColorPicker" :class="{active: state.colorPickerIsVisible}")
             .current-color(:style="{ background: userColor }")
-          ColorPicker(:currentColor="userColor" :visible="colorPickerIsVisible" @selectedColor="updateUserColor")
+          ColorPicker(:currentColor="userColor" :visible="state.colorPickerIsVisible" @selectedColor="updateUserColor")
         input.name.user-details-name(placeholder="What's your name?" v-model="userName" name="Name" maxlength=100)
       .row
         textarea(ref="description" placeholder="Tell us about yourself" v-model="userDescription" name="Description" maxlength=220 rows="1")
@@ -32,8 +258,7 @@ dialog.narrow.user-details(v-if="visible" @keyup.stop :open="visible" @click.lef
           button.inline-button
             img.icon.visit.arrow-icon(src="@/assets/visit.svg")
       UserBadges(:user="user")
-
-    section(v-if="!isAddPage")
+    section
       .row
         .button-wrap
           button(@click.left.stop="toggleUserSettingsIsVisible" :class="{active: userSettingsIsVisible}")
@@ -48,18 +273,18 @@ dialog.narrow.user-details(v-if="visible" @keyup.stop :open="visible" @click.lef
   //- Other User
   section(v-if="!isCurrentUser && userIsSignedIn && user.id")
     .button-wrap
-      button(@click.left.stop="getUserSpaces" :class="{active: loadingUserspaces || spacePickerIsVisible}")
+      button(@click.left.stop="getUserSpaces" :class="{active: state.loadingUserspaces || state.spacePickerIsVisible}")
         User(:user="user" :isClickable="false" :detailsOnRight="false" :key="user.id")
         span Spaces
-        Loader(:visible="loadingUserspaces")
-      SpacePicker(:visible="spacePickerIsVisible" :loading="loadingUserspaces" :user="user" :userSpaces="userSpaces" @selectSpace="changeSpace")
+        Loader(:visible="state.loadingUserspaces")
+      SpacePicker(:visible="state.spacePickerIsVisible" :loading="state.loadingUserspaces" :user="user" :userSpaces="state.userSpaces" @selectSpace="changeSpace")
     .button-wrap
       button(:class="{active: isFavoriteUser}" @click.left.prevent="toggleIsFavoriteUser" @keydown.stop.enter="toggleIsFavoriteUser")
         img.icon(v-if="isFavoriteUser" src="@/assets/heart.svg")
         img.icon(v-else src="@/assets/heart-empty.svg")
         span Follow
         Loader(:visible="isLoadingFavorites")
-    .badge.danger.error-message(v-if="error.unknownServerError") (シ_ _)シ Something went wrong, Please try again or contact support
+    .badge.danger.error-message(v-if="state.error.unknownServerError") (シ_ _)シ Something went wrong, Please try again or contact support
 
   //- Collaborator
   section(v-if="isCollaborator && currentUserIsSpaceMember")
@@ -72,235 +297,6 @@ dialog.narrow.user-details(v-if="visible" @keyup.stop :open="visible" @click.lef
         img.icon.cancel(src="@/assets/add.svg")
         span Remove From Space
 </template>
-
-<script>
-import ColorPicker from '@/components/dialogs/ColorPicker.vue'
-import SpacePicker from '@/components/dialogs/SpacePicker.vue'
-import Loader from '@/components/Loader.vue'
-import UserBadges from '@/components/UserBadges.vue'
-import UserLabelInline from '@/components/UserLabelInline.vue'
-import cache from '@/cache.js'
-import utils from '@/utils.js'
-import postMessage from '@/postMessage.js'
-import { defineAsyncComponent } from 'vue'
-const User = defineAsyncComponent({
-  loader: () => import('@/components/User.vue')
-})
-
-export default {
-  name: 'UserDetails',
-  components: {
-    ColorPicker,
-    User,
-    Loader,
-    UserBadges,
-    SpacePicker,
-    UserLabelInline
-  },
-  created () {
-    this.$store.subscribe((mutation, state) => {
-      if (mutation.type === 'triggerScrollUserDetailsIntoView' && this.visible) {
-        this.scrollUserDetailsIntoView()
-      }
-    })
-  },
-  data () {
-    return {
-      colorPickerIsVisible: false,
-      loadingUserspaces: false,
-      spacePickerIsVisible: false,
-      userSpaces: [],
-      error: {
-        unknownServerError: false
-      }
-    }
-  },
-  computed: {
-    visible () { return this.$store.state.userDetailsIsVisible },
-    user () { return this.$store.state.userDetailsUser },
-    userDetailsPosition () { return this.$store.state.userDetailsPosition },
-    spaceCounterZoomDecimal () { return this.$store.getters.spaceCounterZoomDecimal },
-    styles () {
-      let { x, y, shouldIgnoreZoom, transformOriginIsTopRight } = this.userDetailsPosition
-      let zoom = this.spaceCounterZoomDecimal
-      if (shouldIgnoreZoom) {
-        zoom = 1
-      }
-      if (this.$store.state.isTouchDevice) {
-        zoom = utils.pinchCounterZoomDecimal()
-        if (zoom > 1) {
-          x = x * zoom
-          y = y * zoom
-        }
-      }
-      const styles = {
-        transform: `scale(${zoom})`,
-        left: x + 'px',
-        top: y + 'px'
-      }
-      if (transformOriginIsTopRight) {
-        styles.transformOrigin = 'top right'
-      }
-      return styles
-    },
-    userColor () { return this.user.color },
-    userIsMember () { return Boolean(this.$store.getters['currentSpace/memberById'](this.user.id)) },
-    userIsUpgraded () { return this.user.isUpgraded },
-    isCurrentUser () { return this.$store.getters['currentUser/isCurrentUser'](this.user) },
-    currentUserIsSignedIn () { return this.$store.getters['currentUser/isSignedIn'] },
-    isLoadingFavorites () { return this.$store.state.isLoadingFavorites },
-    isAddPage () { return this.$store.state.isAddPage },
-    userIsSignedIn () {
-      if (this.user.isSignedIn === false) {
-        return false
-      }
-      return true
-    },
-    userName: {
-      get () {
-        return this.user.name
-      },
-      set (newValue) {
-        this.updateUser({ name: newValue })
-      }
-    },
-    userDescription: {
-      get () {
-        return this.user.description
-      },
-      set (newValue) {
-        this.updateUser({ description: newValue })
-        this.updateTextareaSize()
-      }
-    },
-    userWebsite: {
-      get () {
-        return this.user.website
-      },
-      set (newValue) {
-        this.updateUser({ website: newValue })
-      }
-    },
-    websiteUrl () {
-      if (!this.userWebsite) { return }
-      const urls = utils.urlsFromString(this.userWebsite)
-      if (!urls) { return }
-      return urls[0]
-    },
-    isFavoriteUser () {
-      const favoriteUsers = this.$store.state.currentUser.favoriteUsers
-      const isFavoriteUser = Boolean(favoriteUsers.find(user => {
-        return user.id === this.user.id
-      }))
-      return isFavoriteUser
-    },
-    isCollaborator () {
-      const currentSpace = this.$store.state.currentSpace
-      const collaborators = currentSpace.collaborators || []
-      return Boolean(collaborators.find(collaborator => {
-        return collaborator.id === this.user.id
-      }))
-    },
-    currentUserIsSpaceMember () { return this.$store.getters['currentUser/isSpaceMember']() },
-    userSettingsIsVisible () { return this.$store.state.userSettingsIsVisible }
-  },
-  methods: {
-    toggleIsFavoriteUser () {
-      if (this.isFavoriteUser) {
-        this.$store.dispatch('currentUser/removeFavorite', { type: 'user', item: this.user })
-      } else {
-        this.$store.dispatch('currentUser/addFavorite', { type: 'user', item: this.user })
-      }
-    },
-    toggleUserSettingsIsVisible () {
-      const value = !this.$store.state.userSettingsIsVisible
-      this.$store.dispatch('closeAllDialogs')
-      this.$store.commit('userSettingsIsVisible', value)
-    },
-    toggleColorPicker () {
-      const isVisible = this.colorPickerIsVisible
-      this.closeDialogs()
-      this.colorPickerIsVisible = !isVisible
-    },
-    closeDialogs () {
-      this.colorPickerIsVisible = false
-      this.spacePickerIsVisible = false
-    },
-    updateUserColor (newValue) {
-      this.updateUser({ color: newValue })
-    },
-    updateUser (update) {
-      this.$store.dispatch('currentUser/update', update)
-    },
-    updateTextareaSize () {
-      this.$nextTick(() => {
-        let textarea = this.$refs.description
-        if (!textarea) { return }
-        textarea.style.height = textarea.scrollHeight + 1 + 'px'
-      })
-    },
-    signOut () {
-      postMessage.send({ name: 'onLogout' })
-      cache.removeAll()
-      // clear history wipe state from vue-router
-      window.history.replaceState({}, 'Kinopio', '/')
-      location.reload()
-    },
-    triggerSignUpOrInIsVisible () {
-      this.$store.dispatch('closeAllDialogs')
-      this.$store.commit('triggerSignUpOrInIsVisible')
-    },
-    async getUserSpaces () {
-      this.error.unknownServerError = false
-      if (this.loadingUserspaces) { return }
-      if (this.spacePickerIsVisible) {
-        this.closeDialogs()
-        return
-      }
-      this.loadingUserspaces = true
-      this.spacePickerIsVisible = true
-      try {
-        const publicUser = await this.$store.dispatch('api/getPublicUser', this.user)
-        this.userSpaces = publicUser.spaces
-      } catch (error) {
-        this.error.unknownServerError = true
-        this.clearUserSpaces()
-      }
-      this.loadingUserspaces = false
-    },
-    clearUserSpaces () {
-      this.loadingUserspaces = false
-      this.userSpaces = []
-    },
-    changeSpace (space) {
-      this.$store.dispatch('currentSpace/changeSpace', space)
-    },
-    async removeCollaborator () {
-      const user = this.user
-      this.$store.dispatch('currentSpace/removeCollaboratorFromSpace', user)
-      this.$store.dispatch('closeAllDialogs')
-    },
-    scrollUserDetailsIntoView () {
-      this.$nextTick(() => {
-        const element = this.$refs.dialog
-        utils.scrollIntoView({ element })
-      })
-    }
-  },
-  watch: {
-    visible (visible) {
-      this.closeDialogs()
-      this.clearUserSpaces()
-      if (visible) {
-        this.updateTextareaSize()
-      }
-    },
-    userDetailsPosition (position) {
-      this.closeDialogs()
-    }
-  }
-}
-</script>
 
 <style lang="stylus">
 .user-details
