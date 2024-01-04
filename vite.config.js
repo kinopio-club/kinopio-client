1d112c98346b28c150c36d4308dbef2f6d3d6163
Pirijan <hi@pirijan.com>
2024-01-04 14:36:49 -0500

    add are.na offline caching (for community bks)

--- vite.config.js
+++ vite.config.js
@@ -45,6 +45,20 @@ export default defineConfig({
                 statuses: [0, 200]
               }
             }
+          },
+          {
+            urlPattern: /^https:\/\/images\.are\.na\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'are-na-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
           }
         ]
       }
