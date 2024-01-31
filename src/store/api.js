7fb8281fc04ee381a27477f7de772d5464361ed0
Pirijan <hi@pirijan.com>
2024-01-31 11:04:39 -0500

    noop route name change

--- src/store/api.js
+++ src/store/api.js
@@ -605,7 +605,7 @@ const self = {
       if (!shouldRequest({ apiKey })) { return }
       try {
         const options = await context.dispatch('requestOptions', { body, method: 'POST', space: context.rootState.currentSpace })
-        const response = await fetch(`${host}/space/invite/${body.spaceId}`, options)
+        const response = await fetch(`${host}/space/email-invites/${body.spaceId}`, options)
         return normalizeResponse(response)
       } catch (error) {
         context.dispatch('handleServerError', { name: 'sendSpaceInviteEmails', error })
