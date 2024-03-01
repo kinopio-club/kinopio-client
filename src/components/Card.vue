kinopio-club/kinopio-client

commit cf1448aa22b8c628ee6fe98039f5eda6687df5f3

card display error state if imageOrVideo fails to load

src/components/Card.vue
@@ -57,7 +57,7 @@ article.card-wrap#card(
     Frames(:card="card")
 
     template(v-if="!isComment")
-      ImageOrVideo(:isSelectedOrDragging="isSelectedOrDragging" :pendingUploadDataUrl="pendingUploadDataUrl" :image="formats.image" :video="formats.video" @updateCardDimensions="updateCardDimensions")
+      ImageOrVideo(:isSelectedOrDragging="isSelectedOrDragging" :pendingUploadDataUrl="pendingUploadDataUrl" :image="formats.image" :video="formats.video" @updateCardDimensions="updateCardDimensions" @imageLoadError="toggleIsDisplayError(true)" @imageLoadSuccess="toggleIsDisplayError(false)")
 
     TiltResize(:card="card" :visible="tiltResizeIsVisible")
 
@@ -336,7 +336,8 @@ export default {
       isAnimationUnsticking: false,
       stickyStretchResistance: 6,
       defaultColor: '#e3e3e3',
-      pathIsUpdated: false
+      pathIsUpdated: false,
+      isDisplayError: false
     }
   },
   computed: {
@@ -641,7 +642,8 @@ export default {
         'is-dark': this.backgroundColorIsDark,
         's-width': this.width < m,
         'm-width': utils.isBetween({ value: this.width, min: m, max: l }),
-        'l-width': this.width > l
+        'l-width': this.width > l,
+        'display-error': this.isDisplayError
       }
       return classes
     },
@@ -1189,6 +1191,9 @@ export default {
         this.$store.dispatch('currentConnections/updatePaths', { cardId: this.card.id, shouldUpdateApi: false })
         this.pathIsUpdated = true
       })
+    },
+    toggleIsDisplayError (value) {
+      this.isDisplayError = value
     },
 
     // migration added june 2023
@@ -2207,6 +2212,9 @@ article.card-wrap
     touch-action manipulation
     .name
       color var(--primary-on-light-background)
+    &.display-error
+      background var(--danger-background) !important
+      min-height 32px
     &:hover,
     &.hover
       box-shadow var(--hover-shadow)

src/components/ImageOrVideo.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
 //- Video
-video(v-if="Boolean(video)" autoplay loop muted playsinline :key="video" :class="{selected: isSelectedOrDragging}" @canplay="updateDimensions" ref="video")
+video(v-if="Boolean(video)" autoplay loop muted playsinline :key="video" :class="{selected: isSelectedOrDragging}" @canplay="handleSuccess" ref="video")
   source(:src="video")
 //- Image
-img.image(v-if="pendingUploadDataUrl" :src="pendingUploadDataUrl" :class="{selected: isSelectedOrDragging}" @load="updateDimensions")
-img.image(v-else-if="Boolean(image)" :src="image" :class="{selected: isSelectedOrDragging}" @load="updateDimensions")
+img.image(v-if="pendingUploadDataUrl" :src="pendingUploadDataUrl" :class="{selected: isSelectedOrDragging}" @load="handleSuccess")
+img.image(v-else-if="Boolean(image)" :src="image" :class="{selected: isSelectedOrDragging}" @load="handleSuccess" @error="handleError")
 </template>
 
 <script>
@@ -20,7 +20,7 @@ export default {
     image: String,
     video: String
   },
-  emits: ['updateCardDimensions'],
+  emits: ['updateCardDimensions', 'imageLoadSuccess', 'imageLoadError'],
   computed: {
     isInteractingWithItem () { return this.$store.getters.isInteractingWithItem }
   },
@@ -37,6 +37,14 @@ export default {
       if (!this.video) { return }
       const element = this.$refs.video
       element.play()
+    },
+    handleSuccess (event) {
+      this.$emit('imageLoadSuccess')
+      this.updateDimensions()
+    },
+    handleError (event) {
+      this.$emit('imageLoadError')
+      this.updateDimensions()
     }
   },
   watch: {

commit 695abe74be9c75a237e3b161a46b78afb8c15ac2

fix card isVisible bug introduced in cf1448aa22b8c628ee6fe98039f5eda6687df5f3 https://club.kinopio.club/t/large-and-complex-space-only-loads-partially/1261

src/components/Card.vue
@@ -229,6 +229,7 @@ import TiltResize from '@/components/TiltResize.vue'
 import dayjs from 'dayjs'
 import { mapState, mapGetters } from 'vuex'
 import qs from '@aguezz/qs-parse'
+import debounce from 'lodash-es/debounce'
 
 let isMultiTouch
 let initialTouchEvent = {}
@@ -304,6 +305,8 @@ export default {
     this.checkIfShouldUpdatePreviewHtml()
     const defaultCardMaxWidth = this.$store.getters['currentCards/defaultCardMaxWidth'] + 'px'
     utils.setCssVariable('card-width', defaultCardMaxWidth)
+    this.updateIsVisibleInViewport()
+    window.addEventListener('scroll', this.updateIsVisibleInViewport)
   },
   data () {
     return {
@@ -336,7 +339,8 @@ export default {
       isAnimationUnsticking: false,
       stickyStretchResistance: 6,
       defaultColor: '#e3e3e3',
-      pathIsUpdated: false
+      pathIsUpdated: false,
+      isVisibleInViewport: true
     }
   },
   computed: {
@@ -1140,8 +1144,10 @@ export default {
       if (!this.userDetailsIsVisible) { return }
       const user = this.createdByUser
       return user.id === this.userDetailsUser.id
-    },
-    isVisibleInViewport () {
+    }
+  },
+  methods: {
+    updateIsVisibleInViewport: debounce(async function () {
       let isVisible
       if (this.disableViewportOptimizations) { isVisible = true }
       if (this.shouldJiggle) { isVisible = true }
@@ -1168,13 +1174,15 @@ export default {
       const scrollIsBelowTop = scroll > y
       const middleIsVisible = scrollIsAboveBottom && scrollIsBelowTop
       isVisible = isVisible || (isTopVisible || isBottomVisible || middleIsVisible)
+      this.isVisibleInViewport = isVisible
       if (isVisible) {
-        this.correctPaths()
+        this.$nextTick(() => {
+          this.updateCardDimensions()
+          this.correctPaths()
+        })
       }
-      return isVisible
-    }
-  },
-  methods: {
+    }, 50, { leading: true }),
+
     updateCardDimensions () {
       let card = { id: this.card.id }
       card = utils.updateCardDimensions(card)
@@ -2176,13 +2184,6 @@ export default {
   watch: {
     linkToPreview (value) {
       this.updateUrlData()
-    },
-    isVisibleInViewport (value) {
-      if (!value) { return }
-      this.$nextTick(() => {
-        this.updateCardDimensions()
-        this.correctPaths()
-      })
     }
   }
 }