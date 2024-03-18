cfaf9eb97da2cf80755c56daf951d8ecf942479d
Pirijan <hi@pirijan.com>
2024-03-18 14:17:18 -0400

    noop Cards.vue -> composition

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,32 +1,27 @@
+<script setup>
+import { reactive, computed, onMounted, onUnmounted, defineProps, defineEmits, watch, ref, nextTick } from 'vue'
+import { useStore } from 'vuex'
+
+import Card from '@/components/Card.vue'
+const store = useStore()
+
+onMounted(() => {
+  store.subscribe((mutation, state) => {
+    if (mutation.type === 'triggerUpdateUrlPreviewComplete') {
+      store.dispatch('currentCards/checkIfShouldUpdateNewTweetCards')
+    }
+  })
+})
+
+const unlockedCards = computed(() => store.getters['currentCards/isNotLocked'])
+</script>
+
 <template lang="pug">
 .cards
   //- locked cards rendered in ItemsLocked
   template(v-for="card in unlockedCards")
     Card(:card="card")
 </template>
-
-<script>
-import Card from '@/components/Card.vue'
-
-export default {
-  name: 'Cards',
-  components: {
-    Card
-  },
-  created () {
-    this.$store.subscribe((mutation, state) => {
-      if (mutation.type === 'triggerUpdateUrlPreviewComplete') {
-        this.$store.dispatch('currentCards/checkIfShouldUpdateNewTweetCards')
-      }
-    })
-  },
-  computed: {
-    unlockedCards () { return this.$store.getters['currentCards/isNotLocked'] }
-  },
-  methods: {
-  }
-}
-</script>
 
 <style lang="stylus">
 </style>
