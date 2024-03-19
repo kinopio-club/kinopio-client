baebc35c9e6c9e9e64b83fb69953dea523d33a13
Pirijan <hi@pirijan.com>
2024-03-18 22:29:19 -0400

    aftermove fix increment

--- src/store/currentCards.js
+++ src/store/currentCards.js
@@ -604,6 +604,7 @@ const currentCards = {
         card.y = card.y + prevMoveDelta.y
         return card
       })
+      context.dispatch('incrementSelectedZs')
       context.commit('move', { cards, spaceId })
       cards = cards.filter(card => card)
       context.dispatch('api/addToQueue', {
