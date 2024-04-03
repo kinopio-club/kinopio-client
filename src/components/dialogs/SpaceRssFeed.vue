1420cb29d4a76490dd4c9e6e87002913e47c844c
Pirijan <hi@pirijan.com>
2024-04-02 20:52:09 -0400

    spacerssfeed dialog -> composition

--- src/components/dialogs/SpaceRssFeed.vue
+++ src/components/dialogs/SpaceRssFeed.vue
@@ -1,8 +1,48 @@
+<script setup>
+import { reactive, computed, onMounted, onBeforeUnmount, onUnmounted, defineProps, defineEmits, watch, ref, nextTick } from 'vue'
+import { useStore } from 'vuex'
+
+import utils from '@/utils.js'
+import consts from '@/consts.js'
+const store = useStore()
+
+const props = defineProps({
+  visible: Boolean
+})
+watch(() => props.visible, (value, prevValue) => {
+  store.commit('clearNotificationsWithPosition')
+  if (value) {
+    updateUrl()
+  }
+})
+
+const state = reactive({
+  urlIsCopied: false,
+  url: ''
+})
+
+const spaceIsPrivate = computed(() => store.state.currentSpace.privacy === 'private')
+const copyUrl = async (event) => {
+  store.commit('clearNotificationsWithPosition')
+  const position = utils.cursorPositionInPage(event)
+  try {
+    await navigator.clipboard.writeText(state.url)
+    store.commit('addNotificationWithPosition', { message: 'Copied', position, type: 'success', layer: 'app', icon: 'checkmark' })
+  } catch (error) {
+    console.warn('🚑 copyText', error)
+    store.commit('addNotificationWithPosition', { message: 'Copy Error', position, type: 'danger', layer: 'app', icon: 'cancel' })
+  }
+}
+const updateUrl = () => {
+  const spaceId = store.state.currentSpace.id
+  state.url = `${consts.apiHost()}/space/${spaceId}/feed.json`
+}
+</script>
+
 <template lang="pug">
 dialog.narrow.space-rss-feed(v-if="visible" :open="visible" @click.left.stop)
   section
     p Space RSS Feed
-
   section
     .row
       p Subscribe to cards recently created or updated
@@ -10,62 +50,12 @@ dialog.narrow.space-rss-feed(v-if="visible" :open="visible" @click.left.stop)
       p.badge.danger(v-if="spaceIsPrivate")
         img.icon(src="@/assets/lock.svg")
         span RSS Feeds are only available on public spaces
-
     template(v-if="!spaceIsPrivate")
       .row
         button(@click.left="copyUrl")
           img.icon.copy(src="@/assets/copy.svg")
           span Copy Feed URL
-
 </template>
-
-<script>
-import utils from '@/utils.js'
-import consts from '@/consts.js'
-
-export default {
-  name: 'RssFeed',
-  props: {
-    visible: Boolean
-  },
-  data () {
-    return {
-      urlIsCopied: false,
-      url: ''
-    }
-  },
-  computed: {
-    spaceIsPrivate () {
-      return this.$store.state.currentSpace.privacy === 'private'
-    }
-  },
-  methods: {
-    async copyUrl (event) {
-      this.$store.commit('clearNotificationsWithPosition')
-      const position = utils.cursorPositionInPage(event)
-      try {
-        await navigator.clipboard.writeText(this.url)
-        this.$store.commit('addNotificationWithPosition', { message: 'Copied', position, type: 'success', layer: 'app', icon: 'checkmark' })
-      } catch (error) {
-        console.warn('🚑 copyText', error)
-        this.$store.commit('addNotificationWithPosition', { message: 'Copy Error', position, type: 'danger', layer: 'app', icon: 'cancel' })
-      }
-    },
-    updateUrl () {
-      const spaceId = this.$store.state.currentSpace.id
-      this.url = `${consts.apiHost()}/space/${spaceId}/feed.json`
-    }
-  },
-  watch: {
-    visible (visible) {
-      this.$store.commit('clearNotificationsWithPosition')
-      if (visible) {
-        this.updateUrl()
-      }
-    }
-  }
-}
-</script>
 
 <style lang="stylus">
 dialog.space-rss-feed
