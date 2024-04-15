c6e77961ca1a97d92471679064fe37f447c2a1ce
Pirijan <hi@pirijan.com>
2024-04-15 13:38:55 -0400

    noop stats -> composition

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,20 +1,75 @@
-<template lang="pug">
+<script setup>
+import { reactive, computed, onMounted, onBeforeUnmount, onUnmounted, defineProps, defineEmits, watch, ref, nextTick } from 'vue'
+import { useStore } from 'vuex'
 
+import Loader from '@/components/Loader.vue'
+import utils from '@/utils.js'
+
+import dayjs from 'dayjs'
+const store = useStore()
+
+const props = defineProps({
+  visible: Boolean
+})
+
+const currentSpace = computed(() => store.state.currentSpace)
+const isLoadingSpace = computed(() => store.state.isLoadingSpace)
+
+// visits
+
+const visits = computed(() => store.state.currentSpace.visits + 1)
+
+// dates
+
+const showAbsoluteDate = computed(() => store.state.currentUser.filterShowAbsoluteDates)
+const date = (date) => {
+  if (showAbsoluteDate.value) {
+    return dayjs(date).format('YYYY-MM-DD')
+  } else {
+    date = utils.shortRelativeTime(date)
+    if (date === 'now') {
+      return date
+    } else {
+      return date + ' ago'
+    }
+  }
+}
+const toggleFilterShowAbsoluteDates = () => {
+  const value = !store.state.currentUser.filterShowAbsoluteDates
+  store.dispatch('currentUser/toggleFilterShowAbsoluteDates', value)
+}
+
+// items
+
+const tags = computed(() => store.getters['currentSpace/spaceTags'])
+const cards = computed(() => store.getters['currentCards/all'])
+const connections = computed(() => store.getters['currentConnections/all'])
+const boxes = computed(() => store.getters['currentBoxes/all'])
+
+// word count
+
+const wordCount = computed(() => {
+  let words = ''
+  cards.value.forEach(card => {
+    words = words + ' ' + card.name
+  })
+  const wordCount = words.split(' ').length
+  return wordCount
+})
+</script>
+
+<template lang="pug">
 section.stats(v-if="visible")
   p Stats for {{currentSpace.name}}
-
   p(v-if="isLoadingSpace")
     Loader(:visible="true")
-
   template(v-if="!isLoadingSpace")
-
     table
       tbody
         tr.table-header
           td Visits
         tr
           td {{visits}}
-
     table
       tbody
         tr.table-header
@@ -27,7 +82,6 @@ section.stats(v-if="visible")
           td {{connections.length}}
           td {{boxes.length}}
           td {{tags.length}}
-
     table
       tbody
         tr.table-header
@@ -41,68 +95,13 @@ section.stats(v-if="visible")
           td(v-if="currentSpace.editedAt")
             .badge.button-badge.secondary(@click.stop="toggleFilterShowAbsoluteDates")
               span {{date(currentSpace.editedAt)}}
-
     table
       tbody
         tr.table-header
           td Word Count
         tr
           td {{wordCount}}
-
 </template>
-
-<script>
-import Loader from '@/components/Loader.vue'
-import utils from '@/utils.js'
-
-import dayjs from 'dayjs'
-
-export default {
-  name: 'Stats',
-  components: {
-    Loader
-  },
-  props: {
-    visible: Boolean
-  },
-  computed: {
-    currentSpace () { return this.$store.state.currentSpace },
-    showAbsoluteDate () { return this.$store.state.currentUser.filterShowAbsoluteDates },
-    tags () { return this.$store.getters['currentSpace/spaceTags'] },
-    cards () { return this.$store.getters['currentCards/all'] },
-    connections () { return this.$store.getters['currentConnections/all'] },
-    boxes () { return this.$store.getters['currentBoxes/all'] },
-    visits () { return this.$store.state.currentSpace.visits + 1 },
-    wordCount () {
-      let words = ''
-      this.cards.forEach(card => {
-        words = words + ' ' + card.name
-      })
-      const wordCount = words.split(' ').length
-      return wordCount
-    },
-    isLoadingSpace () { return this.$store.state.isLoadingSpace }
-  },
-  methods: {
-    date (date) {
-      if (this.showAbsoluteDate) {
-        return dayjs(date).format('YYYY-MM-DD')
-      } else {
-        date = utils.shortRelativeTime(date)
-        if (date === 'now') {
-          return date
-        } else {
-          return date + ' ago'
-        }
-      }
-    },
-    toggleFilterShowAbsoluteDates () {
-      const value = !this.$store.state.currentUser.filterShowAbsoluteDates
-      this.$store.dispatch('currentUser/toggleFilterShowAbsoluteDates', value)
-    }
-  }
-}
-</script>
 
 <style lang="stylus">
 // .stats
