11fa85c888fa56a322cac29be2586491bb5b7a64
Pirijan <hi@pirijan.com>
2024-04-19 10:20:20 -0400

    Revert "temp logging"
    This reverts commit 796e2f6bc80a822addd2d6e24e774a7a1604b385.

--- src/components/KeyboardShortcutsHandler.vue
+++ src/components/KeyboardShortcutsHandler.vue
@@ -140,8 +140,6 @@ export default {
       const isCardScope = checkIsCardScope(event)
       const isSpaceScope = checkIsSpaceScope(event)
       const isFromInput = event.target.closest('input') || event.target.closest('textarea')
-      console.log('🐸🐸', key, isMeta, event.shiftKey, event)
-
       // Add Child Card
       if (event.shiftKey && key === 'enter' && (isSpaceScope || isCardScope)) {
         this.addChildCard()
