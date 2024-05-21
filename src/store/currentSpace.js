2340aec450c3155f5b5afba6a3d076992e04be18
Pirijan <hi@pirijan.com>
2024-05-21 15:59:47 -0400

    update paths for images w links on load

--- src/store/currentSpace.js
+++ src/store/currentSpace.js
@@ -313,8 +313,8 @@ const currentSpace = {
           })
           if (!linkedCard) { return }
           nextTick(() => {
-            // context.dispatch('currentConnections/updatePaths', { cardId: linkedCard.id, shouldUpdateApi: canEditSpace }, { root: true })
-            // context.dispatch('currentCards/updateDimensions', { cardId: linkedCard.id }, { root: true })
+            context.dispatch('currentConnections/updatePaths', { cardId: linkedCard.id, shouldUpdateApi: canEditSpace }, { root: true })
+            context.dispatch('currentCards/updateDimensions', { cardId: linkedCard.id }, { root: true })
             context.commit('isLoadingOtherItems', false, { root: true })
           })
         })
