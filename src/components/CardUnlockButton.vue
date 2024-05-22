292dd4f8f6fb7c6ba4272540a1ca976bb00adc4d
Pirijan <hi@pirijan.com>
2024-05-22 18:51:05 -0400

    noop cardunlockbutton -> composition

--- src/components/CardUnlockButton.vue
+++ src/components/CardUnlockButton.vue
@@ -1,3 +1,90 @@
+<script setup>
+import { reactive, computed, onMounted, onBeforeUnmount, onUnmounted, defineProps, defineEmits, watch, ref, nextTick } from 'vue'
+import { useStore } from 'vuex'
+
+import utils from '@/utils.js'
+const store = useStore()
+
+onMounted(() => {
+  store.subscribe((mutation, state) => {
+    const { type, payload } = mutation
+    if (type === 'triggerUpdateTheme') {
+      state.defaultColor = utils.cssVariable('secondary-background')
+    } else if (type === 'triggerUpdateLockedItemButtonPositionCardId' && payload === props.card.id) {
+      updatePosition()
+    }
+  })
+  state.defaultColor = utils.cssVariable('secondary-background')
+})
+
+const props = defineProps({
+  card: Object
+})
+
+const state = reactive({
+  defaultColor: '#e3e3e3',
+  position: null
+})
+
+const positionStyles = computed(() => {
+  const pointIsEmpty = utils.pointIsEmpty(state.position)
+  if (!state.position || pointIsEmpty) {
+    return { display: 'none' }
+  }
+  let position = utils.updatePositionWithSpaceOffset(state.position)
+  const x = (position.x + window.scrollX) * store.getters.spaceCounterZoomDecimal
+  const y = (position.y + window.scrollY) * store.getters.spaceCounterZoomDecimal
+  position = {
+    left: x + 'px',
+    top: y + 'px'
+  }
+  return position
+})
+const backgroundStyles = computed(() => {
+  return { backgroundColor: 'transparent' }
+})
+const canEditCard = computed(() => store.getters['currentUser/canEditCard'](props.card))
+const canEditSpace = computed(() => store.getters['currentUser/canEditSpace']())
+const connectionTypes = computed(() => store.getters['currentConnections/typesByCardId'](props.card.id))
+// theme
+const backgroundColorIsDark = computed(() => {
+  const color = props.card.backgroundColor || state.defaultColor
+  return utils.colorIsDark(color)
+})
+const isThemeDark = computed(() => store.state.currentUser.theme === 'dark')
+const isDarkInLightTheme = computed(() => backgroundColorIsDark.value && !isThemeDark.value)
+const isLightInDarkTheme = computed(() => !backgroundColorIsDark.value && isThemeDark.value)
+
+const updatePosition = () => {
+  let cardElement = document.querySelector(`article[data-card-id="${props.card.id}"]`)
+  if (!cardElement) { return }
+  if (!cardElement.dataset.shouldRender) {
+    return
+  }
+  const element = cardElement.querySelector('.lock-button-wrap')
+  if (!element) { return }
+  const rect = element.getBoundingClientRect()
+  state.position = rect
+}
+const unlockCard = (event) => {
+  if (store.state.currentUserIsDrawingConnection) {
+    return
+  }
+  event.stopPropagation()
+  if (!canEditCard.value || !canEditSpace.value) {
+    const position = utils.cursorPositionInPage(event)
+    store.commit('addNotificationWithPosition', { message: 'Card is Read Only', position, type: 'info', layer: 'space', icon: 'cancel' })
+    return
+  }
+  store.commit('currentUserIsDraggingCard', false)
+  store.dispatch('currentCards/update', {
+    id: props.card.id,
+    isLocked: false
+  })
+}
+
+</script>
+
 <template lang="pug">
 .card-unlock-button.inline-button-wrap(:style="positionStyles" @mouseup.left="unlockCard" @touchend="unlockCard" :data-card-id="card.id")
   button.inline-button(tabindex="-1" :style="backgroundStyles" :class="{'is-light-in-dark-theme': isLightInDarkTheme, 'is-dark-in-light-theme': isDarkInLightTheme}")
@@ -6,99 +93,6 @@
         .color(:style="{ background: type.color}")
     img.icon.lock-icon(src="@/assets/lock.svg")
 </template>
-
-<script>
-import utils from '@/utils.js'
-
-import { mapState, mapGetters } from 'vuex'
-
-export default {
-  name: 'CardUnlockButton',
-  components: {
-  },
-  props: {
-    card: Object
-  },
-  created () {
-    this.$store.subscribe((mutation, state) => {
-      const { type, payload } = mutation
-      if (type === 'triggerUpdateTheme') {
-        this.defaultColor = utils.cssVariable('secondary-background')
-      } else if (type === 'triggerUpdateLockedItemButtonPositionCardId' && payload === this.card.id) {
-        this.updatePosition()
-      }
-    })
-  },
-  mounted () {
-    this.defaultColor = utils.cssVariable('secondary-background')
-  },
-  data () {
-    return {
-      defaultColor: '#e3e3e3',
-      position: null
-    }
-  },
-  computed: {
-    positionStyles () {
-      const pointIsEmpty = utils.pointIsEmpty(this.position)
-      if (!this.position || pointIsEmpty) {
-        return { display: 'none' }
-      }
-      let position = utils.updatePositionWithSpaceOffset(this.position)
-      const x = (position.x + window.scrollX) * this.$store.getters.spaceCounterZoomDecimal
-      const y = (position.y + window.scrollY) * this.$store.getters.spaceCounterZoomDecimal
-      position = {
-        left: x + 'px',
-        top: y + 'px'
-      }
-      return position
-    },
-    backgroundStyles () {
-      return { backgroundColor: 'transparent' }
-    },
-    canEditCard () { return this.$store.getters['currentUser/canEditCard'](this.card) },
-    canEditSpace () { return this.$store.getters['currentUser/canEditSpace']() },
-    connectionTypes () { return this.$store.getters['currentConnections/typesByCardId'](this.card.id) },
-    // theme
-    backgroundColorIsDark () {
-      const color = this.card.backgroundColor || this.defaultColor
-      return utils.colorIsDark(color)
-    },
-    isThemeDark () { return this.$store.state.currentUser.theme === 'dark' },
-    isDarkInLightTheme () { return this.backgroundColorIsDark && !this.isThemeDark },
-    isLightInDarkTheme () { return !this.backgroundColorIsDark && this.isThemeDark }
-  },
-  methods: {
-    updatePosition () {
-      let cardElement = document.querySelector(`article[data-card-id="${this.card.id}"]`)
-      if (!cardElement) { return }
-      if (!cardElement.dataset.shouldRender) {
-        return
-      }
-      const element = cardElement.querySelector('.lock-button-wrap')
-      if (!element) { return }
-      const rect = element.getBoundingClientRect()
-      this.position = rect
-    },
-    unlockCard (event) {
-      if (this.$store.state.currentUserIsDrawingConnection) {
-        return
-      }
-      event.stopPropagation()
-      if (!this.canEditCard || !this.canEditSpace) {
-        const position = utils.cursorPositionInPage(event)
-        this.$store.commit('addNotificationWithPosition', { message: 'Card is Read Only', position, type: 'info', layer: 'space', icon: 'cancel' })
-        return
-      }
-      this.$store.commit('currentUserIsDraggingCard', false)
-      this.$store.dispatch('currentCards/update', {
-        id: this.card.id,
-        isLocked: false
-      })
-    }
-  }
-}
-</script>
 
 <style lang="stylus">
 .card-unlock-button
