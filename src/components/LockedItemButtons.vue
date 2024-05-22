1a0c62d9222cdcf8ae294091b8a6e27824b5dea9
Pirijan <hi@pirijan.com>
2024-05-22 19:23:33 -0400

    noop lockeditembuttons -> composition

--- src/components/LockedItemButtons.vue
+++ src/components/LockedItemButtons.vue
@@ -1,3 +1,27 @@
+<script setup>
+import { reactive, computed, onMounted, onBeforeUnmount, onUnmounted, defineProps, defineEmits, watch, ref, nextTick } from 'vue'
+import { useStore } from 'vuex'
+
+import BoxUnlockButton from '@/components/BoxUnlockButton.vue'
+import CardUnlockButton from '@/components/CardUnlockButton.vue'
+import utils from '@/utils.js'
+const store = useStore()
+
+const props = defineProps({
+  visible: Boolean
+})
+
+const lockedBoxes = computed(() => store.getters['currentBoxes/isLocked'])
+const lockedCards = computed(() => store.getters['currentCards/isLocked'])
+const isThemeDark = computed(() => store.state.currentUser.theme === 'dark')
+const boxButtonPosition = (box) => {
+  const element = document.querySelector(`.box[data-box-id="${box.id}"] .lock-button-wrap`)
+  if (!element) { return }
+  const rect = element.getBoundingClientRect()
+  return rect
+}
+</script>
+
 <template lang="pug">
 //- boxes
 template(v-for="box in lockedBoxes")
@@ -5,38 +29,7 @@ template(v-for="box in lockedBoxes")
 //- cards
 template(v-for="card in lockedCards")
   CardUnlockButton(:card="card")
-
 </template>
-
-<script>
-import BoxUnlockButton from '@/components/BoxUnlockButton.vue'
-import CardUnlockButton from '@/components/CardUnlockButton.vue'
-import utils from '@/utils.js'
-
-export default {
-  name: 'LockedItemButtons',
-  components: {
-    BoxUnlockButton,
-    CardUnlockButton
-  },
-  props: {
-    visible: Boolean
-  },
-  computed: {
-    lockedBoxes () { return this.$store.getters['currentBoxes/isLocked'] },
-    lockedCards () { return this.$store.getters['currentCards/isLocked'] },
-    isThemeDark () { return this.$store.state.currentUser.theme === 'dark' }
-  },
-  methods: {
-    boxButtonPosition (box) {
-      const element = document.querySelector(`.box[data-box-id="${box.id}"] .lock-button-wrap`)
-      if (!element) { return }
-      const rect = element.getBoundingClientRect()
-      return rect
-    }
-  }
-}
-</script>
 
 <style lang="stylus">
 // .component-name
