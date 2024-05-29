005ab425ab491335042c22d28f87190679a93d38
pirijan <hi@pirijan.com>
2024-05-29 19:46:26 -0400

    userbadges => composition

--- src/components/UserBadges.vue
+++ src/components/UserBadges.vue
@@ -1,26 +1,58 @@
+<script setup>
+import { reactive, computed, onMounted, onBeforeUnmount, defineProps, defineEmits, watch, ref, nextTick } from 'vue'
+import { useStore } from 'vuex'
+
+import userBadges from '@/data/userBadges.json'
+const store = useStore()
+
+const props = defineProps({
+  user: Object
+})
+const state = reactive({
+  name: '',
+  description: ''
+})
+
+const isBadges = computed(() => {
+  const badges = ['isSpectator', 'isUpgraded', 'isModerator', 'isDonor', 'isAmbassador']
+  return badges.find(badge => props.user[badge])
+})
+
+const toggleDescription = (name) => {
+  if (state.name === name) {
+    state.name = ''
+    state.description = ''
+    return
+  }
+  const badge = userBadges.find(userBadge => userBadge.name === name)
+  state.name = badge.name
+  state.description = badge.description
+}
+</script>
+
 <template lang="pug">
 .row.user-badges(v-if="isBadges")
   //- Spectator
-  .badge.button-badge.status(v-if="user.isSpectator" @click.stop="toggleDescription('Spectators')" :class="{active: name === 'Spectators'}")
+  .badge.button-badge.status(v-if="user.isSpectator" @click.stop="toggleDescription('Spectators')" :class="{active: state.name === 'Spectators'}")
     span Spectator
   //- Upgraded
-  .badge.button-badge.success.badge-upgraded(v-if="user.isUpgraded" @click.stop="toggleDescription('Upgraded')" :class="{active: name === 'Upgraded'}")
+  .badge.button-badge.success.badge-upgraded(v-if="user.isUpgraded" @click.stop="toggleDescription('Upgraded')" :class="{active: state.name === 'Upgraded'}")
     span Upgraded
   //- Donor
-  .badge.button-badge.success.badge-donor(v-if="user.isDonor" @click.stop="toggleDescription('Donor')" :class="{active: name === 'Donor'}")
+  .badge.button-badge.success.badge-donor(v-if="user.isDonor" @click.stop="toggleDescription('Donor')" :class="{active: state.name === 'Donor'}")
     span Donor
   //- Moderator
-  .badge.button-badge.info.badge-moderator(v-if="user.isModerator" @click.stop="toggleDescription('Moderator')" :class="{active: name === 'Moderator'}")
+  .badge.button-badge.info.badge-moderator(v-if="user.isModerator" @click.stop="toggleDescription('Moderator')" :class="{active: state.name === 'Moderator'}")
     span Moderator
   //- Ambassador
-  .badge.button-badge.success.badge-ambassador(v-if="user.isAmbassador" @click.stop="toggleDescription('Ambassador')" :class="{active: name === 'Ambassador'}")
+  .badge.button-badge.success.badge-ambassador(v-if="user.isAmbassador" @click.stop="toggleDescription('Ambassador')" :class="{active: state.name === 'Ambassador'}")
     //- img.icon.heart(src="@/assets/heart.svg")
     span Ambassador
 
 //- badge description
-.row(v-if="description")
+.row(v-if="state.description")
   section.subsection
-    span(v-html="description")
+    span(v-html="state.description")
 
 //- card count
 .row
@@ -29,42 +61,6 @@
     span {{user.cardsCreatedCount}} Cards Created
 
 </template>
-
-<script>
-
-import userBadges from '@/data/userBadges.json'
-
-export default {
-  name: 'UserBadges',
-  props: {
-    user: Object
-  },
-  data () {
-    return {
-      name: '',
-      description: ''
-    }
-  },
-  computed: {
-    isBadges () {
-      const badges = ['isSpectator', 'isUpgraded', 'isModerator', 'isDonor', 'isAmbassador']
-      return badges.find(badge => this.user[badge])
-    }
-  },
-  methods: {
-    toggleDescription (name) {
-      if (this.name === name) {
-        this.name = ''
-        this.description = ''
-        return
-      }
-      const badge = userBadges.find(userBadge => userBadge.name === name)
-      this.name = badge.name
-      this.description = badge.description
-    }
-  }
-}
-</script>
 
 <style lang="stylus">
 .user-badges
