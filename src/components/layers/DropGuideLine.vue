9173626ffc7bd57bc456e659db80259f6582c5ff
pirijan <hi@pirijan.com>
2024-06-04 10:13:34 -0400

    dropguideline -> composition noop

--- src/components/layers/DropGuideLine.vue
+++ src/components/layers/DropGuideLine.vue
@@ -1,11 +1,9 @@
-<template lang="pug">
-aside
-  canvas#drop-guide-line.drop-guide-line
-  canvas#remote-drop-guide-line.remote-drop-guide-line
-</template>
+<script setup>
+import { reactive, computed, onMounted, onBeforeUnmount, defineProps, defineEmits, watch, ref, nextTick } from 'vue'
+import { useStore } from 'vuex'
 
-<script>
 import { nanoid } from 'nanoid'
+const store = useStore()
 
 let canvas, context, remoteCanvas, remoteContext, paintingGuidesTimer, remotePaintingGuidesTimer
 
@@ -17,226 +15,223 @@ let controlPointOddY = lineMaxHeight / 2
 const centerLineY = lineMaxHeight / 2
 let isReverse = false
 
-export default {
-  name: 'DropGuideLine',
-  props: {
-    currentCursor: Object,
-    currentCursorInSpace: Object,
-    uploadIsDraggedOver: Boolean
-  },
-  created () {
-    this.$store.subscribe((mutation, state) => {
-      if (mutation.type === 'triggerUpdateRemoteDropGuideLine') {
-        let update = mutation.payload
-        update.startPoint = this.updateRemotePosition(update.startPoint)
-        update.curve = this.createCurve(update.startPoint)
-        this.addRemoteCurve(update)
-        this.remotePainting()
-      }
-      if (mutation.type === 'triggerUpdateStopRemoteUserDropGuideLine') {
-        this.removeRemoteFramesByUser(mutation.payload.userId)
-        remoteContext.clearRect(0, 0, remoteCanvas.width, remoteCanvas.height)
-      }
-    })
-  },
-
-  mounted () {
-    canvas = document.getElementById('drop-guide-line')
-    remoteCanvas = document.getElementById('remote-drop-guide-line')
-    context = canvas.getContext('2d')
-    remoteContext = remoteCanvas.getContext('2d')
-    this.updateCanvasSize()
-    window.addEventListener('load', this.updateCanvasSize)
-    window.addEventListener('resize', this.updateCanvasSize)
-  },
-  beforeUnmount () {
-    window.removeEventListener('load', this.updateCanvasSize)
-    window.removeEventListener('resize', this.updateCanvasSize)
-  },
-  computed: {
-    viewportWidth () { return this.$store.state.viewportWidth },
-    viewportHeight () { return this.$store.state.viewportHeight },
-    currentUserColor () { return this.$store.state.currentUser.color }
-  },
-  methods: {
-
-    // curves
-
-    updateRemotePosition (position) {
-      const zoom = this.$store.getters.spaceZoomDecimal
-      const space = document.getElementById('space')
-      const rect = space.getBoundingClientRect()
-      position = {
-        x: (position.x * zoom) + rect.x,
-        y: (position.y * zoom) + rect.y
-      }
-      return position
-    },
-    createCurve (startPoint) {
-      const numberOfControlPoints = 4
-      const lineSegmentLength = lineWidth / numberOfControlPoints
-      const lineSegmentIncrement = (lineSegmentLength / 2)
-      if (controlPointEvenY <= lineMaxHeight && controlPointOddY >= 0 && !isReverse) {
-        if (controlPointEvenY <= 0) {
-          controlPointEvenY = 0
-          isReverse = true
-        }
-        controlPointEvenY--
-        controlPointOddY++
-      } else {
-        controlPointEvenY++
-        controlPointOddY--
-        if (controlPointEvenY >= lineMaxHeight) {
-          controlPointEvenY = lineMaxHeight
-          isReverse = false
-        }
-      }
-      // 1
-      const controlPointX1 = startPoint.x + lineSegmentIncrement
-      const endPointX1 = startPoint.x + lineSegmentLength
-      // 2
-      const controlPointX2 = endPointX1 + lineSegmentIncrement
-      const endPointX2 = startPoint.x + (lineSegmentLength * 2)
-      // 3
-      const controlPointX3 = endPointX2 + lineSegmentIncrement
-      const endPointX3 = startPoint.x + (lineSegmentLength * 3)
-      // 4
-      const controlPointX4 = endPointX3 + lineSegmentIncrement
-      const endPointX4 = startPoint.x + (lineSegmentLength * 4)
-      // 5
-      const endPointY = startPoint.y + centerLineY
-      return { startPoint, controlPointEvenY, controlPointOddY, controlPointX1, endPointX1, controlPointX2, endPointX2, controlPointX3, endPointX3, controlPointX4, endPointX4, endPointY }
-    },
-    paintCurve (context, curve) {
-      const { startPoint, controlPointEvenY, controlPointOddY, controlPointX1, endPointX1, controlPointX2, endPointX2, controlPointX3, endPointX3, controlPointX4, endPointX4, endPointY } = curve
-      context.beginPath()
-      context.moveTo(startPoint.x, startPoint.y)
-      context.quadraticCurveTo(controlPointX1, controlPointOddY + startPoint.y, endPointX1, endPointY)
-      context.quadraticCurveTo(controlPointX2, controlPointEvenY + startPoint.y, endPointX2, endPointY)
-      context.quadraticCurveTo(controlPointX3, controlPointOddY + startPoint.y, endPointX3, endPointY)
-      context.quadraticCurveTo(controlPointX4, controlPointEvenY + startPoint.y, endPointX4, endPointY)
-      context.stroke()
-    },
-
-    // Remote Painting
-
-    addRemoteCurve (curve) {
-      remoteDropGuideLines.push(curve)
-    },
-    remotePainting () {
-      if (!remotePaintingGuidesTimer) {
-        remotePaintingGuidesTimer = window.requestAnimationFrame(this.remotePaintGuidesFrame)
-      }
-    },
-    removeRemoteFrame (frameId) {
-      remoteDropGuideLines = remoteDropGuideLines.filter(curve => {
-        return curve.frameId !== frameId
-      })
-    },
-    removeRemoteFramesByUser (userId) {
-      remoteDropGuideLines = remoteDropGuideLines.filter(curve => {
-        return curve.userId !== userId
-      })
-    },
-    remotePaintGuidesFrame () {
-      remoteContext.clearRect(0, 0, remoteCanvas.width, remoteCanvas.height)
-      remoteDropGuideLines.forEach(guideLine => {
-        this.removeRemoteFrame(guideLine.frameId)
-        remoteContext.lineWidth = 4
-        remoteContext.lineCap = 'round'
-        remoteContext.strokeStyle = guideLine.color
-        this.paintCurve(remoteContext, guideLine.curve)
-      })
-      if (remoteDropGuideLines.length > 0) {
-        window.requestAnimationFrame(this.remotePaintGuidesFrame)
-      } else {
-        setTimeout(() => {
-          window.cancelAnimationFrame(remotePaintingGuidesTimer)
-          remotePaintingGuidesTimer = undefined
-        }, 0)
-      }
-    },
-
-    // Painting
-
-    paintGuides () {
-      context.clearRect(0, 0, canvas.width, canvas.height)
-      context.strokeStyle = this.currentUserColor
-      context.lineWidth = 4
-      context.lineCap = 'round'
-      // paint curve
-      let startPoint = {
-        x: this.currentCursor.x,
-        y: this.currentCursor.y
-      }
-      let curve = this.createCurve(startPoint)
-      this.paintCurve(context, curve)
-      // broadcast curve
-      const scroll = { x: window.scrollX, y: window.scrollY }
-      startPoint = {
-        x: this.currentCursor.x + scroll.x,
-        y: this.currentCursor.y + scroll.y
-      }
-      this.broadcastCursorAndCurve({ startPoint, color: this.currentUserColor })
-      if (paintingGuidesTimer) {
-        window.requestAnimationFrame(this.paintGuides)
-      } else {
-        this.stopPaintingGuides()
-      }
-    },
-    startPaintingGuides () {
-      if (!paintingGuidesTimer) {
-        paintingGuidesTimer = window.requestAnimationFrame(this.paintGuides)
-      }
-    },
-    stopPaintingGuides () {
-      window.cancelAnimationFrame(paintingGuidesTimer)
-      paintingGuidesTimer = undefined
-      context.clearRect(0, 0, canvas.width, canvas.height)
-      this.broadcastStopPaintingGuide()
-    },
-    updateCanvasSize () {
-      canvas.width = this.viewportWidth * window.devicePixelRatio
-      canvas.height = this.viewportHeight * window.devicePixelRatio
-      canvas.style.width = this.viewportWidth + 'px'
-      canvas.style.height = this.viewportHeight + 'px'
-      context.scale(window.devicePixelRatio, window.devicePixelRatio)
-      remoteCanvas.width = this.viewportWidth * window.devicePixelRatio
-      remoteCanvas.height = this.viewportHeight * window.devicePixelRatio
-      remoteCanvas.style.width = this.viewportWidth + 'px'
-      remoteCanvas.style.height = this.viewportHeight + 'px'
-      remoteContext.scale(window.devicePixelRatio, window.devicePixelRatio)
-    },
-    broadcastCursorAndCurve ({ startPoint, color }) {
-      const canEditSpace = this.$store.getters['currentUser/canEditSpace']()
-      if (!canEditSpace) { return }
-      let updates = {}
-      updates.x = this.currentCursorInSpace.x
-      updates.y = this.currentCursorInSpace.y
-      updates.color = color
-      updates.userId = this.$store.state.currentUser.id
-      this.$store.commit('broadcast/update', { updates, type: 'updateRemoteUserCursor', handler: 'triggerUpdateRemoteUserCursor' })
-      updates.startPoint = startPoint
-      updates.color = color
-      updates.frameId = nanoid()
-      this.$store.commit('broadcast/update', { updates, type: 'updateRemoteUserDropGuideLine', handler: 'triggerUpdateRemoteDropGuideLine' })
-    },
-    broadcastStopPaintingGuide () {
-      const updates = { userId: this.$store.state.currentUser.id }
-      this.$store.commit('broadcast/update', { updates, type: 'updateStopRemoteUserDropGuideLine', handler: 'triggerUpdateStopRemoteUserDropGuideLine' })
+onMounted(() => {
+  store.subscribe((mutation, state) => {
+    if (mutation.type === 'triggerUpdateRemoteDropGuideLine') {
+      let update = mutation.payload
+      update.startPoint = updateRemotePosition(update.startPoint)
+      update.curve = createCurve(update.startPoint)
+      addRemoteCurve(update)
+      remotePainting()
     }
-  },
-  watch: {
-    uploadIsDraggedOver (value) {
-      if (value) {
-        this.startPaintingGuides()
-      } else {
-        this.stopPaintingGuides()
-      }
+    if (mutation.type === 'triggerUpdateStopRemoteUserDropGuideLine') {
+      removeRemoteFramesByUser(mutation.payload.userId)
+      remoteContext.clearRect(0, 0, remoteCanvas.width, remoteCanvas.height)
+    }
+  })
+  canvas = document.getElementById('drop-guide-line')
+  remoteCanvas = document.getElementById('remote-drop-guide-line')
+  context = canvas.getContext('2d')
+  remoteContext = remoteCanvas.getContext('2d')
+  updateCanvasSize()
+  window.addEventListener('load', updateCanvasSize)
+  window.addEventListener('resize', updateCanvasSize)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('load', updateCanvasSize)
+  window.removeEventListener('resize', updateCanvasSize)
+})
+
+const props = defineProps({
+  currentCursor: Object,
+  currentCursorInSpace: Object,
+  uploadIsDraggedOver: Boolean
+})
+watch(() => props.uploadIsDraggedOver, (value, prevValue) => {
+  if (value) {
+    startPaintingGuides()
+  } else {
+    stopPaintingGuides()
+  }
+})
+
+const viewportWidth = computed(() => store.state.viewportWidth)
+const viewportHeight = computed(() => store.state.viewportHeight)
+const currentUserColor = computed(() => store.state.currentUser.color)
+
+const updateRemotePosition = (position) => {
+  const zoom = store.getters.spaceZoomDecimal
+  const space = document.getElementById('space')
+  const rect = space.getBoundingClientRect()
+  position = {
+    x: (position.x * zoom) + rect.x,
+    y: (position.y * zoom) + rect.y
+  }
+  return position
+}
+const createCurve = (startPoint) => {
+  const numberOfControlPoints = 4
+  const lineSegmentLength = lineWidth / numberOfControlPoints
+  const lineSegmentIncrement = (lineSegmentLength / 2)
+  if (controlPointEvenY <= lineMaxHeight && controlPointOddY >= 0 && !isReverse) {
+    if (controlPointEvenY <= 0) {
+      controlPointEvenY = 0
+      isReverse = true
+    }
+    controlPointEvenY--
+    controlPointOddY++
+  } else {
+    controlPointEvenY++
+    controlPointOddY--
+    if (controlPointEvenY >= lineMaxHeight) {
+      controlPointEvenY = lineMaxHeight
+      isReverse = false
     }
   }
+  // 1
+  const controlPointX1 = startPoint.x + lineSegmentIncrement
+  const endPointX1 = startPoint.x + lineSegmentLength
+  // 2
+  const controlPointX2 = endPointX1 + lineSegmentIncrement
+  const endPointX2 = startPoint.x + (lineSegmentLength * 2)
+  // 3
+  const controlPointX3 = endPointX2 + lineSegmentIncrement
+  const endPointX3 = startPoint.x + (lineSegmentLength * 3)
+  // 4
+  const controlPointX4 = endPointX3 + lineSegmentIncrement
+  const endPointX4 = startPoint.x + (lineSegmentLength * 4)
+  // 5
+  const endPointY = startPoint.y + centerLineY
+  return { startPoint, controlPointEvenY, controlPointOddY, controlPointX1, endPointX1, controlPointX2, endPointX2, controlPointX3, endPointX3, controlPointX4, endPointX4, endPointY }
 }
+const paintCurve = (context, curve) => {
+  const { startPoint, controlPointEvenY, controlPointOddY, controlPointX1, endPointX1, controlPointX2, endPointX2, controlPointX3, endPointX3, controlPointX4, endPointX4, endPointY } = curve
+  context.beginPath()
+  context.moveTo(startPoint.x, startPoint.y)
+  context.quadraticCurveTo(controlPointX1, controlPointOddY + startPoint.y, endPointX1, endPointY)
+  context.quadraticCurveTo(controlPointX2, controlPointEvenY + startPoint.y, endPointX2, endPointY)
+  context.quadraticCurveTo(controlPointX3, controlPointOddY + startPoint.y, endPointX3, endPointY)
+  context.quadraticCurveTo(controlPointX4, controlPointEvenY + startPoint.y, endPointX4, endPointY)
+  context.stroke()
+}
+
+// Remote Painting
+
+const addRemoteCurve = (curve) => {
+  remoteDropGuideLines.push(curve)
+}
+const remotePainting = () => {
+  if (!remotePaintingGuidesTimer) {
+    remotePaintingGuidesTimer = window.requestAnimationFrame(remotePaintGuidesFrame)
+  }
+}
+const removeRemoteFrame = (frameId) => {
+  remoteDropGuideLines = remoteDropGuideLines.filter(curve => {
+    return curve.frameId !== frameId
+  })
+}
+const removeRemoteFramesByUser = (userId) => {
+  remoteDropGuideLines = remoteDropGuideLines.filter(curve => {
+    return curve.userId !== userId
+  })
+}
+const remotePaintGuidesFrame = () => {
+  remoteContext.clearRect(0, 0, remoteCanvas.width, remoteCanvas.height)
+  remoteDropGuideLines.forEach(guideLine => {
+    removeRemoteFrame(guideLine.frameId)
+    remoteContext.lineWidth = 4
+    remoteContext.lineCap = 'round'
+    remoteContext.strokeStyle = guideLine.color
+    paintCurve(remoteContext, guideLine.curve)
+  })
+  if (remoteDropGuideLines.length > 0) {
+    window.requestAnimationFrame(remotePaintGuidesFrame)
+  } else {
+    setTimeout(() => {
+      window.cancelAnimationFrame(remotePaintingGuidesTimer)
+      remotePaintingGuidesTimer = undefined
+    }, 0)
+  }
+}
+
+// Painting
+
+const paintGuides = () => {
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  context.strokeStyle = currentUserColor.value
+  context.lineWidth = 4
+  context.lineCap = 'round'
+  // paint curve
+  let startPoint = {
+    x: props.currentCursor.x,
+    y: props.currentCursor.y
+  }
+  let curve = createCurve(startPoint)
+  paintCurve(context, curve)
+  // broadcast curve
+  const scroll = { x: window.scrollX, y: window.scrollY }
+  startPoint = {
+    x: props.currentCursor.x + scroll.x,
+    y: props.currentCursor.y + scroll.y
+  }
+  broadcastCursorAndCurve({ startPoint, color: currentUserColor.value })
+  if (paintingGuidesTimer) {
+    window.requestAnimationFrame(paintGuides)
+  } else {
+    stopPaintingGuides()
+  }
+}
+const startPaintingGuides = () => {
+  if (!paintingGuidesTimer) {
+    paintingGuidesTimer = window.requestAnimationFrame(paintGuides)
+  }
+}
+const stopPaintingGuides = () => {
+  window.cancelAnimationFrame(paintingGuidesTimer)
+  paintingGuidesTimer = undefined
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  broadcastStopPaintingGuide()
+}
+const updateCanvasSize = () => {
+  canvas.width = viewportWidth.value * window.devicePixelRatio
+  canvas.height = viewportHeight.value * window.devicePixelRatio
+  canvas.style.width = viewportWidth.value + 'px'
+  canvas.style.height = viewportHeight.value + 'px'
+  context.scale(window.devicePixelRatio, window.devicePixelRatio)
+  remoteCanvas.width = viewportWidth.value * window.devicePixelRatio
+  remoteCanvas.height = viewportHeight.value * window.devicePixelRatio
+  remoteCanvas.style.width = viewportWidth.value + 'px'
+  remoteCanvas.style.height = viewportHeight.value + 'px'
+  remoteContext.scale(window.devicePixelRatio, window.devicePixelRatio)
+}
+const broadcastCursorAndCurve = ({ startPoint, color }) => {
+  const canEditSpace = store.getters['currentUser/canEditSpace']()
+  if (!canEditSpace) { return }
+  let updates = {}
+  updates.x = props.currentCursorInSpace.x
+  updates.y = props.currentCursorInSpace.y
+  updates.color = color
+  updates.userId = store.state.currentUser.id
+  store.commit('broadcast/update', { updates, type: 'updateRemoteUserCursor', handler: 'triggerUpdateRemoteUserCursor' })
+  updates.startPoint = startPoint
+  updates.color = color
+  updates.frameId = nanoid()
+  store.commit('broadcast/update', { updates, type: 'updateRemoteUserDropGuideLine', handler: 'triggerUpdateRemoteDropGuideLine' })
+}
+const broadcastStopPaintingGuide = () => {
+  const updates = { userId: store.state.currentUser.id }
+  store.commit('broadcast/update', { updates, type: 'updateStopRemoteUserDropGuideLine', handler: 'triggerUpdateStopRemoteUserDropGuideLine' })
+}
+
 </script>
+
+<template lang="pug">
+aside
+  canvas#drop-guide-line.drop-guide-line
+  canvas#remote-drop-guide-line.remote-drop-guide-line
+</template>
+
 <style lang="stylus" scoped>
 canvas
   position fixed
