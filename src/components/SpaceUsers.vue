5cc20e96aa8170305282b1cdaafd4583093e1482
pirijan <hi@pirijan.com>
2024-06-05 13:53:10 -0400

    noop header

--- src/components/SpaceUsers.vue
+++ src/components/SpaceUsers.vue
@@ -46,11 +46,14 @@ const spectators = computed(() => {
 .space-users(v-else-if="isEmbedMode")
   .users
     User(v-for="user in members" :user="user" :isClickable="true" :detailsOnRight="true" :key="user.id" :shouldCloseAllDialogs="true" tabindex="0" :userDetailsIsInline="userDetailsIsInline")
-//- Normal Space
+
+//- Space
 .space-users(v-else)
-  .users.spectators
+  //- spectators
+  .users.spectators(v-if="spectators.length")
     User(v-for="user in spectators" :user="user" :isClickable="true" :detailsOnRight="true" :key="user.id" :shouldCloseAllDialogs="true" tabindex="0" :userDetailsIsInline="userDetailsIsInline")
     User(v-if="!currentUserIsSpaceMember" :user="currentUser" :isClickable="true" :detailsOnRight="true" :key="currentUser.id" :shouldCloseAllDialogs="true" tabindex="0" :userDetailsIsInline="userDetailsIsInline")
+  //- collaborators, members, you
   .users
     User(v-for="user in collaborators" :user="user" :isClickable="true" :detailsOnRight="true" :key="user.id" :shouldCloseAllDialogs="true" tabindex="0" :userDetailsIsInline="userDetailsIsInline")
     User(v-for="user in users" :user="user" :isClickable="true" :detailsOnRight="true" :key="user.id" :shouldCloseAllDialogs="true" tabindex="0" :userDetailsIsInline="userDetailsIsInline")
