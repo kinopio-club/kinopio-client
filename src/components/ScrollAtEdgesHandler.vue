ad4577ab26922e4dea900d1be6b919003f857f8e
pirijan <hi@pirijan.com>
2024-06-06 12:37:12 -0400

    scrollatedges -> composition

--- src/components/ScrollAtEdgesHandler.vue
+++ src/components/ScrollAtEdgesHandler.vue
@@ -1,266 +1,261 @@
-<template lang='pug'>
-</template>
+<script setup>
+import { reactive, computed, onMounted, onBeforeUnmount, defineProps, defineEmits, watch, ref, nextTick } from 'vue'
+import { useStore } from 'vuex'
 
-<script>
 import utils from '@/utils.js'
+
+const store = useStore()
 
 const scrollArea = 50
 let startCursor, prevCursor, prevCursorPage, endCursor, scrollTimer, maxHeight, maxWidth, currentEvent
 let movementDirection = {}
 
-export default {
-  name: 'ScrollAtEdgesHandler',
-  created () {
-    this.$store.subscribe((mutation, state) => {
-      if (mutation.type === 'currentUserIsPaintingLocked' && mutation.payload) {
-        this.stopScrollTimer()
-      }
-      if (mutation.type === 'triggeredTouchCardDragPosition') {
-        const position = this.$store.state.triggeredTouchCardDragPosition
-        const event = {
-          clientX: position.x,
-          clientY: position.y
-        }
-        this.stopScrollTimer()
-        this.initInteractions(event)
-      }
-    })
-  },
-  mounted () {
-    window.addEventListener('mousedown', this.initInteractions)
-    window.addEventListener('touchstart', this.initInteractions)
-    // bind events to window to receive events when mouse is outside window
-    window.addEventListener('mousemove', this.interact)
-    window.addEventListener('touchmove', this.interact)
-    window.addEventListener('mouseup', this.stopInteractions)
-    window.addEventListener('touchend', this.stopInteractions)
-  },
-  beforeUnmount () {
-    window.removeEventListener('mousedown', this.initInteractions)
-    window.removeEventListener('touchstart', this.initInteractions)
-    window.removeEventListener('mousemove', this.interact)
-    window.removeEventListener('touchmove', this.interact)
-    window.removeEventListener('mouseup', this.stopInteractions)
-    window.removeEventListener('touchend', this.stopInteractions)
-  },
-  computed: {
-    viewportHeight () { return this.$store.state.viewportHeight },
-    viewportWidth () { return this.$store.state.viewportWidth },
-    pageHeight () { return this.$store.state.pageHeight },
-    pageWidth () { return this.$store.state.pageWidth },
-    currentUserIsPainting () { return this.$store.state.currentUserIsPainting },
-    isDraggingCard () { return this.$store.state.currentUserIsDraggingCard },
-    isDrawingConnection () { return this.$store.state.currentUserIsDrawingConnection },
-    isResizingCard () { return this.$store.state.currentUserIsResizingCard },
-    spaceCounterZoomDecimal () { return this.$store.getters.spaceCounterZoomDecimal },
-    spaceZoomDecimal () { return this.$store.getters.spaceZoomDecimal },
-    shouldPreventResize () { return this.currentUserIsPainting || this.isDrawingConnection || this.isResizingCard }
-  },
-  methods: {
-    initInteractions (event) {
-      currentEvent = event
-      const position = utils.cursorPositionInViewport(event)
-      const zoom = this.spaceZoomDecimal
-      startCursor = position
-      endCursor = position
-      maxHeight = Math.max(6500, this.$store.state.viewportHeight) * zoom
-      maxWidth = Math.max(6500, this.$store.state.viewportWidth) * zoom
-      if (this.$store.getters.shouldScrollAtEdges(event)) {
-        this.updateMovementDirection()
-      }
-      if (this.$store.getters.shouldScrollAtEdges(event) && !scrollTimer) {
-        scrollTimer = window.requestAnimationFrame(this.scrollFrame)
-      }
-    },
-    interact (event) {
-      currentEvent = event
-      if (this.$store.getters.shouldScrollAtEdges(event)) {
-        this.updateMovementDirection()
-      }
-      prevCursor = utils.cursorPositionInViewport(event)
-      prevCursorPage = utils.cursorPositionInPage(event)
-    },
-    scrollFrame () {
-      let delta, speed
-      const viewportHeight = this.viewportHeight
-      const viewportWidth = this.viewportWidth
-      const cursor = this.cursor()
-      const cursorIsTopSide = cursor.y <= scrollArea
-      const cursorIsBottomSide = cursor.y >= (viewportHeight - scrollArea)
-      const cursorIsLeftSide = cursor.x <= scrollArea
-      const cursorIsRightSide = cursor.x >= (viewportWidth - scrollArea)
-      const shouldScrollUp = Boolean(cursorIsTopSide && window.scrollY)
-      // Y movement
-      if (movementDirection.y === 'up' && shouldScrollUp) {
-        speed = this.speed(cursor, 'up')
-        delta = {
-          x: 0,
-          y: -speed
-        }
-        this.scrollBy(delta)
-      } else if (movementDirection.y === 'down' && cursorIsBottomSide && this.shouldScrollDown()) {
-        speed = this.speed(cursor, 'down')
-        delta = {
-          x: 0,
-          y: speed
-        }
-        this.increasePageHeight(delta)
-        this.scrollBy(delta)
-      }
-      // X movement
-      if (movementDirection.x === 'left' && cursorIsLeftSide && window.scrollX) {
-        speed = this.speed(cursor, 'left')
-        delta = {
-          x: -speed,
-          y: 0
-        }
-        this.scrollBy(delta)
-      } else if (movementDirection.x === 'right' && cursorIsRightSide && this.shouldScrollRight()) {
-        speed = this.speed(cursor, 'right')
-        delta = {
-          x: speed,
-          y: 0
-        }
-        this.increasePageWidth(delta)
-        this.scrollBy(delta)
-      }
-      if (scrollTimer) {
-        window.requestAnimationFrame(this.scrollFrame)
-      }
-    },
-    cursor () {
-      if (utils.objectHasKeys(prevCursor)) {
-        return prevCursor
-      } else {
-        return startCursor
-      }
-    },
-    updateMovementDirection () {
-      const cursor = this.cursor()
-      const xMove = endCursor.x - cursor.x
-      const yMove = endCursor.y - cursor.y
-      if (Math.sign(yMove) === 1) {
-        movementDirection.y = 'up'
-      } else if (Math.sign(yMove) === -1) {
-        movementDirection.y = 'down'
-      }
-      if (Math.sign(xMove) === 1) {
-        movementDirection.x = 'left'
-      } else if (Math.sign(xMove) === -1) {
-        movementDirection.x = 'right'
-      }
-    },
-    increasePageWidth (delta) {
-      if (this.shouldPreventResize) { return }
-      const cursorIsRightSideOfPage = (this.pageWidth - prevCursorPage.x) < scrollArea
-      if (cursorIsRightSideOfPage) {
-        const pageWidth = this.pageWidth
-        const width = pageWidth + delta.x
-        this.$store.commit('pageWidth', width)
-      }
-    },
-    increasePageHeight (delta) {
-      if (this.shouldPreventResize) { return }
-      const cursorIsBottomSideOfPage = (this.pageHeight - prevCursorPage.y) < scrollArea
-      if (cursorIsBottomSideOfPage) {
-        const pageHeight = this.pageHeight
-        const height = pageHeight + delta.y
-        this.$store.commit('pageHeight', height)
-      }
-    },
-    shouldScrollRight () {
-      this.updatePageSizes()
-      const scrolledTooFarRight = (window.scrollX + this.viewportWidth) > maxWidth
-      return !scrolledTooFarRight
-    },
-    shouldScrollDown () {
-      this.updatePageSizes()
-      const scrolledTooFarDown = (window.scrollY + this.viewportHeight) > maxHeight
-      return !scrolledTooFarDown
-    },
-    scrollBy (delta) {
-      if (utils.isAndroid()) { return }
-      let zoom = this.spaceZoomDecimal
-      if (zoom === 1) {
-        const viewport = utils.visualViewport()
-        zoom = viewport.scale
-      }
-      const currentUserIsBoxSelecting = this.$store.state.currentUserIsBoxSelecting
-      const isDraggingCard = this.$store.state.currentUserIsDraggingCard
-      const isDraggingBox = this.$store.state.currentUserIsDraggingBox
-      const isDraggingItem = isDraggingCard || isDraggingBox
-      delta = {
-        left: Math.round(delta.x * zoom),
-        top: Math.round(delta.y * zoom)
-      }
-      if (isDraggingItem) {
-        const slowMultiplier = 0.9
-        const itemDelta = {
-          x: delta.left * slowMultiplier,
-          y: delta.top * slowMultiplier
-        }
-        this.$store.dispatch('history/pause')
-        if (isDraggingCard || isDraggingBox) {
-          this.$store.dispatch('currentCards/move', { endCursor, prevCursor, delta: itemDelta })
-          this.$store.dispatch('currentBoxes/move', { endCursor, prevCursor, delta: itemDelta })
-        }
-      }
-      if (this.isDrawingConnection) {
-        this.$store.commit('triggerDrawConnectionFrame', currentEvent)
-      }
-      if (this.currentUserIsPainting && !currentUserIsBoxSelecting) {
-        this.$store.commit('triggerPaintFramePosition', currentEvent)
-      }
-      window.scrollBy(delta)
-    },
-    speed (cursor, direction) {
-      const minSpeed = 10
-      const maxSpeed = 20
-      const maxSpeedOutsideWindow = 50
-      const viewportHeight = this.viewportHeight
-      const viewportWidth = this.viewportWidth
-      // viewportSize based on direction
-      const directionIsY = direction === 'up' || direction === 'down'
-      const directionIsX = direction === 'left' || direction === 'right'
-      let viewportSize
-      if (directionIsX) {
-        cursor = cursor.x
-        viewportSize = viewportWidth
-      } else if (directionIsY) {
-        cursor = cursor.y
-        viewportSize = viewportHeight
-      }
-      // calc percent over scrollArea
-      let amount
-      if (direction === 'up' || direction === 'left') {
-        amount = Math.abs(cursor - scrollArea)
-      }
-      if (direction === 'down' || direction === 'right') {
-        amount = Math.abs(cursor - (viewportSize - scrollArea))
-      }
-      let percent = utils.roundFloat(amount / scrollArea)
-      // speed
-      let speed = percent * scrollArea
-      speed = Math.max(speed, minSpeed)
-      if (percent > 1) {
-        speed = Math.min(speed, maxSpeedOutsideWindow)
-      } else {
-        speed = Math.min(speed, maxSpeed)
-      }
-      return speed
-    },
-    updatePageSizes () {
-      this.$store.dispatch('updatePageSizes')
-    },
-    stopScrollTimer () {
-      window.cancelAnimationFrame(scrollTimer)
-      scrollTimer = undefined
-      prevCursor = undefined
-      movementDirection = {}
-    },
-    stopInteractions () {
-      this.stopScrollTimer()
+onMounted(() => {
+  store.subscribe((mutation, state) => {
+    if (mutation.type === 'currentUserIsPaintingLocked' && mutation.payload) {
+      stopScrollTimer()
     }
+    if (mutation.type === 'triggeredTouchCardDragPosition') {
+      const position = store.state.triggeredTouchCardDragPosition
+      const event = {
+        clientX: position.x,
+        clientY: position.y
+      }
+      stopScrollTimer()
+      initInteractions(event)
+    }
+  })
+  window.addEventListener('mousedown', initInteractions)
+  window.addEventListener('touchstart', initInteractions)
+  // bind events to window to receive events when mouse is outside window
+  window.addEventListener('mousemove', interact)
+  window.addEventListener('touchmove', interact)
+  window.addEventListener('mouseup', stopInteractions)
+  window.addEventListener('touchend', stopInteractions)
+})
+onBeforeUnmount(() => {
+  window.removeEventListener('mousedown', initInteractions)
+  window.removeEventListener('touchstart', initInteractions)
+  window.removeEventListener('mousemove', interact)
+  window.removeEventListener('touchmove', interact)
+  window.removeEventListener('mouseup', stopInteractions)
+  window.removeEventListener('touchend', stopInteractions)
+})
+
+const viewportHeight = computed(() => store.state.viewportHeight)
+const viewportWidth = computed(() => store.state.viewportWidth)
+const pageHeight = computed(() => store.state.pageHeight)
+const pageWidth = computed(() => store.state.pageWidth)
+const currentUserIsPainting = computed(() => store.state.currentUserIsPainting)
+const isDraggingCard = computed(() => store.state.currentUserIsDraggingCard)
+const isDrawingConnection = computed(() => store.state.currentUserIsDrawingConnection)
+const isResizingCard = computed(() => store.state.currentUserIsResizingCard)
+const spaceCounterZoomDecimal = computed(() => store.getters.spaceCounterZoomDecimal)
+const spaceZoomDecimal = computed(() => store.getters.spaceZoomDecimal)
+const shouldPreventResize = computed(() => currentUserIsPainting.value || isDrawingConnection.value || isResizingCard.value)
+
+const initInteractions = (event) => {
+  currentEvent = event
+  const position = utils.cursorPositionInViewport(event)
+  const zoom = spaceZoomDecimal.value
+  startCursor = position
+  endCursor = position
+  maxHeight = Math.max(6500, store.state.viewportHeight) * zoom
+  maxWidth = Math.max(6500, store.state.viewportWidth) * zoom
+  if (store.getters.shouldScrollAtEdges(event)) {
+    updateMovementDirection()
+  }
+  if (store.getters.shouldScrollAtEdges(event) && !scrollTimer) {
+    scrollTimer = window.requestAnimationFrame(scrollFrame)
   }
 }
+const interact = (event) => {
+  currentEvent = event
+  if (store.getters.shouldScrollAtEdges(event)) {
+    updateMovementDirection()
+  }
+  prevCursor = utils.cursorPositionInViewport(event)
+  prevCursorPage = utils.cursorPositionInPage(event)
+}
+
+const scrollFrame = () => {
+  let delta, currentSpeed
+  const currentCursor = cursor()
+  const cursorIsTopSide = currentCursor.y <= scrollArea
+  const cursorIsBottomSide = currentCursor.y >= (viewportHeight.value - scrollArea)
+  const cursorIsLeftSide = currentCursor.x <= scrollArea
+  const cursorIsRightSide = currentCursor.x >= (viewportWidth.value - scrollArea)
+  const shouldScrollUp = Boolean(cursorIsTopSide && window.scrollY)
+  // Y movement
+  if (movementDirection.y === 'up' && shouldScrollUp) {
+    currentSpeed = speed(currentCursor, 'up')
+    delta = {
+      x: 0,
+      y: -currentSpeed
+    }
+    scrollBy(delta)
+  } else if (movementDirection.y === 'down' && cursorIsBottomSide && shouldScrollDown()) {
+    currentSpeed = speed(currentCursor, 'down')
+    delta = {
+      x: 0,
+      y: currentSpeed
+    }
+    increasePageHeight(delta)
+    scrollBy(delta)
+  }
+  // X movement
+  if (movementDirection.x === 'left' && cursorIsLeftSide && window.scrollX) {
+    currentSpeed = speed(currentCursor, 'left')
+    delta = {
+      x: -currentSpeed,
+      y: 0
+    }
+    scrollBy(delta)
+  } else if (movementDirection.x === 'right' && cursorIsRightSide && shouldScrollRight()) {
+    currentSpeed = speed(currentCursor, 'right')
+    delta = {
+      x: currentSpeed,
+      y: 0
+    }
+    increasePageWidth(delta)
+    scrollBy(delta)
+  }
+  if (scrollTimer) {
+    window.requestAnimationFrame(scrollFrame)
+  }
+}
+
+const cursor = () => {
+  if (utils.objectHasKeys(prevCursor)) {
+    return prevCursor
+  } else {
+    return startCursor
+  }
+}
+const updateMovementDirection = () => {
+  const frameCursor = cursor()
+  const xMove = endCursor.x - frameCursor.x
+  const yMove = endCursor.y - frameCursor.y
+  if (Math.sign(yMove) === 1) {
+    movementDirection.y = 'up'
+  } else if (Math.sign(yMove) === -1) {
+    movementDirection.y = 'down'
+  }
+  if (Math.sign(xMove) === 1) {
+    movementDirection.x = 'left'
+  } else if (Math.sign(xMove) === -1) {
+    movementDirection.x = 'right'
+  }
+}
+const increasePageWidth = (delta) => {
+  if (shouldPreventResize.value) { return }
+  const cursorIsRightSideOfPage = (pageWidth.value - prevCursorPage.x) < scrollArea
+  if (cursorIsRightSideOfPage) {
+    const width = pageWidth.value + delta.x
+    store.commit('pageWidth', width)
+  }
+}
+const increasePageHeight = (delta) => {
+  if (shouldPreventResize.value) { return }
+  const cursorIsBottomSideOfPage = (pageHeight.value - prevCursorPage.y) < scrollArea
+  if (cursorIsBottomSideOfPage) {
+    const height = pageHeight.value + delta.y
+    store.commit('pageHeight', height)
+  }
+}
+const shouldScrollRight = () => {
+  updatePageSizes()
+  const scrolledTooFarRight = (window.scrollX + viewportWidth.value) > maxWidth
+  return !scrolledTooFarRight
+}
+const shouldScrollDown = () => {
+  updatePageSizes()
+  const scrolledTooFarDown = (window.scrollY + viewportHeight.value) > maxHeight
+  return !scrolledTooFarDown
+}
+const scrollBy = (delta) => {
+  if (utils.isAndroid()) { return }
+  let zoom = spaceZoomDecimal.value
+  if (zoom === 1) {
+    const viewport = utils.visualViewport()
+    zoom = viewport.scale
+  }
+  const currentUserIsBoxSelecting = store.state.currentUserIsBoxSelecting
+  const isDraggingCard = store.state.currentUserIsDraggingCard
+  const isDraggingBox = store.state.currentUserIsDraggingBox
+  const isDraggingItem = isDraggingCard || isDraggingBox
+  delta = {
+    left: Math.round(delta.x * zoom),
+    top: Math.round(delta.y * zoom)
+  }
+  if (isDraggingItem) {
+    const slowMultiplier = 0.9
+    const itemDelta = {
+      x: delta.left * slowMultiplier,
+      y: delta.top * slowMultiplier
+    }
+    store.dispatch('history/pause')
+    if (isDraggingCard || isDraggingBox) {
+      store.dispatch('currentCards/move', { endCursor, prevCursor, delta: itemDelta })
+      store.dispatch('currentBoxes/move', { endCursor, prevCursor, delta: itemDelta })
+    }
+  }
+  if (isDrawingConnection.value) {
+    store.commit('triggerDrawConnectionFrame', currentEvent)
+  }
+  if (currentUserIsPainting.value && !currentUserIsBoxSelecting) {
+    store.commit('triggerPaintFramePosition', currentEvent)
+  }
+  window.scrollBy(delta)
+}
+const speed = (cursor, direction) => {
+  const minSpeed = 10
+  const maxSpeed = 20
+  const maxSpeedOutsideWindow = 50
+  // viewportSize based on direction
+  const directionIsY = direction === 'up' || direction === 'down'
+  const directionIsX = direction === 'left' || direction === 'right'
+  let viewportSize
+  if (directionIsX) {
+    cursor = cursor.x
+    viewportSize = viewportWidth.value
+  } else if (directionIsY) {
+    cursor = cursor.y
+    viewportSize = viewportHeight.value
+  }
+  // calc percent over scrollArea
+  let amount
+  if (direction === 'up' || direction === 'left') {
+    amount = Math.abs(cursor - scrollArea)
+  }
+  if (direction === 'down' || direction === 'right') {
+    amount = Math.abs(cursor - (viewportSize - scrollArea))
+  }
+  let percent = utils.roundFloat(amount / scrollArea)
+  // speed
+  let speed = percent * scrollArea
+  speed = Math.max(speed, minSpeed)
+  if (percent > 1) {
+    speed = Math.min(speed, maxSpeedOutsideWindow)
+  } else {
+    speed = Math.min(speed, maxSpeed)
+  }
+  return speed
+}
+const updatePageSizes = () => {
+  store.dispatch('updatePageSizes')
+}
+const stopScrollTimer = () => {
+  window.cancelAnimationFrame(scrollTimer)
+  scrollTimer = undefined
+  prevCursor = undefined
+  movementDirection = {}
+}
+const stopInteractions = () => {
+  stopScrollTimer()
+}
+
 </script>
+
+<template lang="pug">
+</template>
