495bc0ac3ba80062e3174cbb3e03443780660b7d
pirijan <hi@pirijan.com>
2024-06-13 10:58:56 -0400

    stub sonar

---
+++ src/components/layers/Sonar.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { reactive, computed, onMounted, onBeforeUnmount, defineProps, defineEmits, watch, ref, nextTick } from 'vue'
+import { useStore } from 'vuex'
+const store = useStore()
+
+// import utils from '@/utils.js'
+
+// adapted from https://codepen.io/pillowmermaid/details/xrwVPQ
+
+onMounted(() => {
+  store.subscribe(mutation => {
+    // trigger sonar pos
+    // if (mutation.type === 'triggerUpdateOtherCard') {
+    //   mutation.payload
+    // }
+  })
+})
+
+// const state = reactive({
+//   count: 0
+// })
+
+const viewportHeight = computed(() => store.state.viewportHeight)
+const viewportWidth = computed(() => store.state.viewportWidth)
+
+</script>
+
+<template lang="pug">
+aside
+  canvas#sonar(
+    :width="viewportWidth"
+    :height="viewportHeight"
+  )
+</template>
+
+<style lang="stylus" scoped>
+canvas
+  background-color pink
+  // pointer-events none
+  position fixed
+  top 0
+</style>
