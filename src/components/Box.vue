5982f536ec2b017996fc3d39d31b8927f988292c
pirijan <hi@pirijan.com>
2024-07-02 10:15:13 -0400

    box perf: use only visible boxes for edge snapping
    re: too many boxes resize perf (visible in viewport)
    I start to notice around ~100 boxes. Made a test space if you want to better gauge when it becomes noticeable. https://kinopio.club/xzUG5mi68kGv5OK4zdJgw

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -143,7 +143,7 @@ const infoClasses = computed(() => {
 // edge snapping
 
 const otherBoxes = computed(() => {
-  const boxes = store.getters['currentBoxes/all']
+  const boxes = store.getters['currentBoxes/isSelectableInViewport']
   return boxes.filter(box => box.id !== props.box.id)
 })
 const snapGuideStyles = computed(() => {
