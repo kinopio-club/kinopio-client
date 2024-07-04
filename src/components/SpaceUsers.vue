8116aef0d65249769cde416ed8f07b44ed777356
pirijan <hi@pirijan.com>
2024-07-03 23:14:51 -0400

    stub updateMembersShouldShowUsersButton

--- src/components/SpaceUsers.vue
+++ src/components/SpaceUsers.vue
@@ -1,17 +1,31 @@
 <script setup>
-import { reactive, computed, onMounted, onUnmounted, defineProps, defineEmits, watch, ref, nextTick, defineAsyncComponent } from 'vue'
+import { reactive, computed, onMounted, onUnmounted, onBeforeUnmount, defineProps, defineEmits, watch, ref, nextTick, defineAsyncComponent } from 'vue'
 import { useStore } from 'vuex'
 
+import User from '@/components/User.vue'
+import SpaceUsersButton from '@/components/SpaceUsersButton.vue'
 import utils from '@/utils.js'
 
 import uniqBy from 'lodash-es/uniqBy'
-import User from '@/components/User.vue'
-import SpaceUsersButton from '@/components/SpaceUsersButton.vue'
 
 const store = useStore()
 
+const spaceUsersElement = ref(null)
+
+const avatarWidth = 28
+
+onMounted(() => {
+  window.addEventListener('resize', updateMembersShouldShowUsersButton)
+})
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', updateMembersShouldShowUsersButton)
+})
+
 const props = defineProps({
   userDetailsIsInline: Boolean
+})
+const state = reactive({
+  embersShouldShowUsersButton: false
 })
 
 const isEmbedMode = computed(() => store.state.isEmbedMode)
@@ -19,6 +33,8 @@ const isAddPage = computed(() => store.state.isAddPage)
 const currentUser = computed(() => store.state.currentUser)
 const currentSpace = computed(() => store.state.currentSpace)
 const currentUserIsSpaceMember = computed(() => store.getters['currentUser/isSpaceMember']())
+
+// members
 
 const members = computed(() => currentSpace.value.users)
 const users = computed(() => {
@@ -29,6 +45,15 @@ const collaborators = computed(() => {
   let collaborators = currentSpace.value.collaborators
   return collaborators.filter(user => user.id !== currentUser.value.id)
 })
+watch(() => users.value, (value, prevValue) => {
+  updateMembersShouldShowUsersButton()
+})
+watch(() => collaborators.value, (value, prevValue) => {
+  updateMembersShouldShowUsersButton()
+})
+
+// non-members
+
 const spectators = computed(() => {
   let spectators = currentSpace.value.spectators
   spectators = spectators.filter(user => user.id !== currentUser.value.id)
@@ -36,14 +61,46 @@ const spectators = computed(() => {
   return spectators
 })
 
+// const membersLength = computed(() => {
+//   const items = members.value.concat(collaborators.value)
+//   return items.length
+// })
+// const spectatorsLength = computed(() => {
+//   const items = spectators.value
+//   let length = items.length
+//   if (!currentUserIsSpaceMember.value) {
+//     length += 1
+//   }
+//   return length
+// })
+
 // spectators / collaborators / users / currentuser
-// avatarWidth = 28
+
+// handle too many users
 
 // numberOfUsers * avatarWidth)/pageWidth
-const shouldShowSpaceUsersButton = computed(() => {
-  const pageWidth = store.state.pageWidth
-  return true
-})
+const updateMembersShouldShowUsersButton = () => {
+  // const viewportWidth = store.state.viewportWidth
+  const viewportWidth = utils.visualViewport().width
+  // const element = spaceUsersElement.value
+  // const usersWidth = element.getBoundingClientRect().width
+  const rightElementWrap = document.querySelector('header nav .right')
+  const rightSideWidth = rightElementWrap.getBoundingClientRect().width
+  const leftElementWrap = document.querySelector('header nav .left')
+  const leftSideWidth = leftElementWrap.getBoundingClientRect().width
+  const availableWidth = viewportWidth - leftSideWidth - rightSideWidth
+  const minAvailableWidth = viewportWidth / 4
+  if (availableWidth < minAvailableWidth) {
+    state.embersShouldShowUsersButton = true
+    console.error(viewportWidth, availableWidth, minAvailableWidth, state.embersShouldShowUsersButton)
+  } else {
+    state.embersShouldShowUsersButton = false
+  }
+}
+// const shouldShowSpaceUsersButton = computed(() => {
+//   const viewportWidth = store.state.viewportWidth
+//   return true
+// })
 </script>
 
 <template lang="pug">
@@ -57,7 +114,7 @@ const shouldShowSpaceUsersButton = computed(() => {
     User(v-for="user in members" :user="user" :isClickable="true" :detailsOnRight="true" :key="user.id" :shouldCloseAllDialogs="true" tabindex="0" :userDetailsIsInline="userDetailsIsInline")
 
 //- Space
-.space-users(v-else)
+.space-users(v-else ref="spaceUsersElement")
   //- spectators
   .users.spectators(v-if="spectators.length || !currentUserIsSpaceMember")
     User(v-for="user in spectators" :user="user" :isClickable="true" :detailsOnRight="true" :key="user.id" :shouldCloseAllDialogs="true" tabindex="0" :userDetailsIsInline="userDetailsIsInline")
