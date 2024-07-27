0ac38e0ec79b19ce925f28e6fdefb3b2f6a82fc4
Pirijan <hi@pirijan.com>
2024-07-27 13:08:30 -0400

    copy tweaks

--- src/components/subsections/UserSettingsCards.vue
+++ src/components/subsections/UserSettingsCards.vue
@@ -121,8 +121,8 @@ const initDefaultColor = () => {
       button(@click="updateLimit(consts.highCharacterLimit)" :class="{ active: limitIsMax }")
         span {{consts.highCharacterLimit}}
     p.badge.secondary
-      span(v-if="limitIsDefault") For ideas and thoughts
-      span(v-if="!limitIsDefault") For long-form writing and documenting
+      span(v-if="limitIsDefault") Best for ideas and thoughts
+      span(v-if="!limitIsDefault") Best for long-form writing and documenting
 
   section
     p Max Card Width
