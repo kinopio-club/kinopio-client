7830b492c43f4674609bc2fd8101ed810679a97a
Pirijan <hi@pirijan.com>
2024-08-02 10:44:46 -0400

    team: stub custom userlist (hybrid of userlist and removed lists)

--- src/components/dialogs/Team.vue
+++ src/components/dialogs/Team.vue
@@ -2,8 +2,10 @@
 import { reactive, computed, onMounted, onBeforeUnmount, defineProps, defineEmits, watch, ref, nextTick } from 'vue'
 import { useStore } from 'vuex'
 
+// import UserList from '@/components/UserList.vue'
+import ResultsFilter from '@/components/ResultsFilter.vue'
+import UserLabelInline from '@/components/UserLabelInline.vue'
 import utils from '@/utils.js'
-import UserList from '@/components/UserList.vue'
 
 const store = useStore()
 
@@ -24,10 +26,14 @@ const props = defineProps({
   team: Object
 })
 const state = reactive({
-  dialogHeight: null
+  dialogHeight: null,
+  filter: '',
+  filteredUsers: [],
+  selectedUser: {}
 })
 
 watch(() => props.visible, (value, prevValue) => {
+  state.selectedUser = {}
   if (value) {
     updateDialogHeight()
   }
@@ -40,17 +46,49 @@ const updateDialogHeight = async () => {
   state.dialogHeight = utils.elementHeight(element)
 }
 
-const isTeamAdmin = computed(() => store.getters['currentUser/isTeamAdmin'](props.team.id))
+const currentUserIsTeamAdmin = computed(() => store.getters['currentUser/isTeamAdmin'](props.team.id))
 // :selectedUser="selectedUser"
 // const selectedUser = computed(() => {
 
-const removeTeamUser = async (user) => {
-}
+// const removeTeamUser = async (user) => {
+// }
 
-const toggleUserDetails = (event, user) => {
-  console.log(event, user)
-  // closeDialogs()
-  // showUserDetails(event, user)
+// const toggleUserDetails = (event, user) => {
+//   console.log(event, user)
+//   // closeDialogs()
+//   // showUserDetails(event, user)
+// }
+
+const updateFilteredUsers = (users) => {
+  state.filteredUsers = users
+}
+const updateFilter = (filter) => {
+  state.filter = filter
+}
+const usersFiltered = computed(() => {
+  let items
+  if (state.filter) {
+    items = state.filteredUsers
+  } else {
+    items = props.team.users
+  }
+  return items
+})
+
+// user
+
+const selectUser = (event, user) => {
+  state.selectedUser = user
+  console.log(state.selectedUser)
+  // emit('selectUser', event, user)
+}
+const userIsSelected = (user) => {
+  console.log('🌳', state.selectedUser.id, user.id, currentUserIsTeamAdmin.value, props.team.id)
+  return state.selectedUser.id === user.id
+}
+const removeUser = (user) => {
+  if (!props.isClickable) { }
+  // emit('removeUser', user)
 }
 
 </script>
@@ -59,13 +97,23 @@ const toggleUserDetails = (event, user) => {
 dialog.narrow.dialog-name(v-if="visible" :open="visible" @click.left.stop ref="dialogElement" :style="{'max-height': state.dialogHeight + 'px'}")
   section
     p {{props.team.name}}
-  UserList(
-    :users="props.team.users"
-    @selectUser="toggleUserDetails"
-    :showRemoveUser="isTeamAdmin"
-    @removeUser="removeTeamUser"
-    :isClickable="true"
-  )
+
+  span
+    ResultsFilter(:items="props.team.users" @updateFilter="updateFilter" @updateFilteredItems="updateFilteredUsers")
+    ul.results-list.user-list
+      template(v-for="user in props.team.users" :key="user.id")
+        li(@click.left.stop="selectUser($event, user)" tabindex="0" v-on:keyup.stop.enter="selectUser($event, user)" :class="{ active: userIsSelected(user) }")
+          UserLabelInline(:user="user")
+          button.remove-user.small-button(v-if="currentUserIsTeamAdmin" @click.left.stop="removeUser(user)" title="Remove from space")
+            img.icon.cancel(src="@/assets/add.svg")
+
+  //- UserList(
+  //-   :users="props.team.users"
+  //-   @selectUser="toggleUserDetails"
+  //-   :showRemoveUser="isTeamAdmin"
+  //-   @removeUser="removeTeamUser"
+  //-   :isClickable="true"
+  //- )
 </template>
 
 <style lang="stylus">
