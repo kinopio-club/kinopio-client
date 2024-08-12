6f95cae71b986392b6587ae0d6a741b03ee320ed
pirijan <hi@pirijan.com>
2024-08-12 08:50:59 -0400

    stub user -> userdetailsteams list

---
+++ src/components/UserDetailsTeams.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { reactive, computed, onMounted, onBeforeUnmount, defineProps, defineEmits, watch, ref, nextTick } from 'vue'
+import { useStore } from 'vuex'
+const store = useStore()
+
+// import utils from '@/utils.js'
+
+onMounted(() => {
+  console.log('🐸🐸', props.user)
+  // console.log(`🐴 the component is now mounted.`, store.state.currentSpace)
+  // store.subscribe(mutation => {
+  //   if (mutation.type === 'triggerUpdateOtherCard') {
+  //     mutation.payload
+  //   }
+  // })
+})
+
+const props = defineProps({
+  user: Object
+})
+
+const state = reactive({
+  teamIsVisible: false
+})
+
+const userTeams = computed(() => store.state.currentUser.teams)
+
+const visible = computed(() => userTeams.value?.length)
+watch(() => visible.value, async (value, prevValue) => {
+  state.teamIsVisible = false
+})
+
+// todo server: public user gets public teammeta
+
+</script>
+
+<template lang="pug">
+section.user-details-teams(v-if="visible")
+  template(v-for="team in userTeams")
+    button {{team.color}} {{ team.name }}
+</template>
+
+<style lang="stylus">
+// .user-details-teams
+</style>
