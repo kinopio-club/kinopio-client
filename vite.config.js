28f0408ca36dc09a1cb84c7b65ccfccd5c577535
Pirijan <hi@pirijan.com>
2024-08-21 19:40:22 -0400

    noop vite supports async

--- vite.config.js
+++ vite.config.js
@@ -7,110 +7,112 @@ import fs from 'fs'
 
 const yearTime = 60 * 60 * 24 * 365 // 365 days
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  plugins: [
-    // .vue support
-    createVuePlugin(),
-    // offline support
-    VitePWA({
-      registerType: 'autoUpdate',
-      strategies: 'generateSW',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,svg,png,gif,woff2,ico,jpg,jpeg,webp}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/cdn\.kinopio\.club\/(?!.*?\.mp(3|4)\b).*$/i, // match all except mp3/mp4
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'cdn-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: yearTime
-              },
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          },
-          {
-            urlPattern: /^https:\/\/bk\.kinopio\.club\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'bk-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: yearTime
-              },
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          },
-          {
-            urlPattern: /^https:\/\/images\.are\.na\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'are-na-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: yearTime
-              },
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          },
-          {
-            urlPattern: /^https:\/\/d2w9rnfcy7mm78\.cloudfront\.net\/.*/i, // are.na cdn
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'are-na-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: yearTime
-              },
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          }
-        ]
+export default defineConfig(async ({ command, mode }) => {
+  return {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
       }
-    }),
-    // sitemap
-    sitemap({
-      hostname: 'https://kinopio.club',
-      routes: async () => {
-        const lastmod = new Date().toISOString()
-        const urls = [
-          { url: '/', lastmod },
-          { url: 'https://help.kinopio.club', lastmod },
-          { url: 'https://blog.kinopio.club', lastmod },
-          { url: 'https://kinopio.club/kinopio-roadmap-6TRE21gchHI7alHLuwzd5', lastmod },
-          { url: 'https://kinopio.club/-kinopio-what-s-new-6lsytK8ZfOtMl2oqG05Rj', lastmod },
-          { url: 'https://club.kinopio.club', lastmod }
-        ]
-        console.log('🌺 Sitemap URLs:', urls)
-        return urls
-      },
-      outDir: 'dist',
-      filename: 'sitemap.xml'
-    })
-  ],
-  server: {
-    port: 8080,
-    fs: {
-      // Allow serving files from one level up to the project root
-      allow: ['..']
     },
-    https: {
-      key: fs.readFileSync('./.cert/key.pem'),
-      cert: fs.readFileSync('./.cert/cert.pem')
+    plugins: [
+      // .vue support
+      createVuePlugin(),
+      // offline support
+      VitePWA({
+        registerType: 'autoUpdate',
+        strategies: 'generateSW',
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,svg,png,gif,woff2,ico,jpg,jpeg,webp}'],
+          runtimeCaching: [
+            {
+              urlPattern: /^https:\/\/cdn\.kinopio\.club\/(?!.*?\.mp(3|4)\b).*$/i, // match all except mp3/mp4
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'cdn-cache',
+                expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: yearTime
+                },
+                cacheableResponse: {
+                  statuses: [0, 200]
+                }
+              }
+            },
+            {
+              urlPattern: /^https:\/\/bk\.kinopio\.club\/.*/i,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'bk-cache',
+                expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: yearTime
+                },
+                cacheableResponse: {
+                  statuses: [0, 200]
+                }
+              }
+            },
+            {
+              urlPattern: /^https:\/\/images\.are\.na\/.*/i,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'are-na-cache',
+                expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: yearTime
+                },
+                cacheableResponse: {
+                  statuses: [0, 200]
+                }
+              }
+            },
+            {
+              urlPattern: /^https:\/\/d2w9rnfcy7mm78\.cloudfront\.net\/.*/i, // are.na cdn
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'are-na-cache',
+                expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: yearTime
+                },
+                cacheableResponse: {
+                  statuses: [0, 200]
+                }
+              }
+            }
+          ]
+        }
+      }),
+      // sitemap
+      sitemap({
+        hostname: 'https://kinopio.club',
+        routes: async () => {
+          const lastmod = new Date().toISOString()
+          const urls = [
+            { url: '/', lastmod },
+            { url: 'https://help.kinopio.club', lastmod },
+            { url: 'https://blog.kinopio.club', lastmod },
+            { url: 'https://kinopio.club/kinopio-roadmap-6TRE21gchHI7alHLuwzd5', lastmod },
+            { url: 'https://kinopio.club/-kinopio-what-s-new-6lsytK8ZfOtMl2oqG05Rj', lastmod },
+            { url: 'https://club.kinopio.club', lastmod }
+          ]
+          console.log('🌺 Sitemap URLs:', urls)
+          return urls
+        },
+        outDir: 'dist',
+        filename: 'sitemap.xml'
+      })
+    ],
+    server: {
+      port: 8080,
+      fs: {
+        // Allow serving files from one level up to the project root
+        allow: ['..']
+      },
+      https: {
+        key: fs.readFileSync('./.cert/key.pem'),
+        cert: fs.readFileSync('./.cert/cert.pem')
+      }
     }
   }
 })
