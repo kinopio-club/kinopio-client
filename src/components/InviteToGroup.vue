18c890f9ce0d5b752a6c20294529062acf94492f
Pirijan <hi@pirijan.com>
2024-10-16 16:28:28 -0400

    noop cleanup

--- src/components/InviteToGroup.vue
+++ src/components/InviteToGroup.vue
@@ -4,9 +4,6 @@ import { useStore } from 'vuex'
 
 // import utils from '@/utils.js'
 import GroupLabel from '@/components/GroupLabel.vue'
-// import User from '@/components/User.vue'
-
-// import randomColor from 'randomcolor'
 
 const store = useStore()
 
@@ -15,44 +12,22 @@ const emit = defineEmits(['closeDialogs'])
 const props = defineProps({
   visible: Boolean
 })
-// const state = reactive({
-//   count: 0
-// })
 
-// const currentUser = computed(() => store.state.currentUser)
-// const randomUser = computed(() => {
-//   const luminosity = store.state.currentUser.theme
-//   const color = randomColor({ luminosity })
-//   return { color }
-// })
 const closeDialogs = () => {
   emit('closeDialogs')
 }
-
 const group = computed(() => store.getters['groups/spaceGroup']())
 
 </script>
 
 <template lang="pug">
-section.invite-to-group(v-if="props.visible" @click.stop="closeDialogs")
+section.invite-to-group(v-if="props.visible" @click.stop="closeDialogs" :data-group-collaboratorKey="group.collaboratorKey")
   .row
-    //- p
     GroupLabel(:group="group")
-    //- .users
-    //-   User(:user="currentUser" :isClickable="false" :key="currentUser.id" :isSmall="true" :hideYouLabel="true")
-    //-   User(:user="randomUser" :isClickable="false" :key="currentUser.id" :isSmall="true" :hideYouLabel="true")
     span Invite to Group
 
-  //- p alsdkfj
 </template>
 
 <style lang="stylus">
-section.invite-to-group
-  // .users
-  //   margin-right 5px
-  //   .user
-  //     vertical-align -3px
-  //     .anon-avatar
-  //       top 6px
-
+// section.invite-to-group
 </style>
