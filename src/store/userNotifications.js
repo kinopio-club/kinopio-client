34e42f919e51b7d68af22086a43292f1262663e1
Pirijan <hi@pirijan.com>
2024-11-12 14:56:28 -0500

    add usernotification: addspacetogroup

--- src/store/userNotifications.js
+++ src/store/userNotifications.js
@@ -80,6 +80,27 @@ export default {
       notifiedCardIds.push(cardId)
     },
 
+    // Group
+
+    addSpaceToGroup: (context, { groupId, addedToGroupByUserId }) => {
+      const userCanEdit = context.rootGetters['currentUser/canEditSpace']()
+      if (!userCanEdit) { return }
+      const group = context.rootGetters['groups/byId'](groupId)
+      // recipients are all other group users
+      const recipients = group.users.filter(user => user.id !== addedToGroupByUserId)
+      let recipientUserIds = recipients.map(recipient => recipient.id)
+      recipientUserIds = uniq(recipientUserIds)
+      if (!recipientUserIds.length) { return }
+      const notification = {
+        type: 'addSpaceToGroup',
+        userId: addedToGroupByUserId,
+        recipientUserIds,
+        spaceId: context.state.id,
+        groupId
+      }
+      context.dispatch('api/addToQueue', { name: 'createUserNotification', body: notification }, { root: true })
+    },
+
     // Ask to Add Space to Explore
 
     addAskToAddToExplore: (context) => {
