d2f7d330f00232e1e57cd1df07b9fe682bf62fdb
Pirijan <hi@pirijan.com>
2024-11-12 15:05:19 -0500

    notificationsettings -> composition

--- src/components/dialogs/NotificationSettings.vue
+++ src/components/dialogs/NotificationSettings.vue
@@ -1,5 +1,86 @@
+<script setup>
+import { reactive, computed, onMounted, onBeforeUnmount, defineProps, defineEmits, watch, ref, nextTick } from 'vue'
+import { useStore } from 'vuex'
+
+import Loader from '@/components/Loader.vue'
+import SpaceList from '@/components/SpaceList.vue'
+import utils from '@/utils.js'
+
+const store = useStore()
+
+const dialogElement = ref(null)
+
+onMounted(() => {
+  window.addEventListener('resize', updateDialogHeight)
+})
+
+const props = defineProps({
+  visible: Boolean
+})
+const state = reactive({
+  dialogHeight: null,
+  unsubscribedSpaces: [],
+  isLoading: true
+})
+
+watch(() => props.visible, (value, prevValue) => {
+  if (value) {
+    updateDialogHeight()
+    updateUnsubscribedSpaces()
+    // updateUnsubscribedGroups()
+  }
+})
+
+const updateDialogHeight = async () => {
+  if (!props.visible) { return }
+  await nextTick()
+  let element = dialogElement.value
+  state.dialogHeight = utils.elementHeight(element)
+}
+
+const isSignedIn = computed(() => store.getters['currentUser/isSignedIn'])
+const currentUserIsSignedIn = computed(() => store.getters['currentUser/isSignedIn'])
+const shouldEmailNotifications = computed(() => store.state.currentUser.shouldEmailNotifications)
+const shouldEmailBulletin = computed(() => store.state.currentUser.shouldEmailBulletin)
+const shouldEmailWeeklyReview = computed(() => store.state.currentUser.shouldEmailWeeklyReview)
+const parentDialog = computed(() => 'notificationSettings')
+
+const toggleShouldEmailNotifications = () => {
+  const value = !shouldEmailNotifications.value
+  store.dispatch('currentUser/shouldEmailNotifications', value)
+}
+const toggleShouldEmailBulletin = () => {
+  const value = !shouldEmailBulletin.value
+  store.dispatch('currentUser/shouldEmailBulletin', value)
+}
+const toggleShouldEmailWeeklyReview = () => {
+  const value = !shouldEmailWeeklyReview.value
+  store.dispatch('currentUser/shouldEmailWeeklyReview', value)
+}
+const triggerSignUpOrInIsVisible = () => {
+  store.dispatch('closeAllDialogs')
+  store.commit('triggerSignUpOrInIsVisible')
+}
+
+// spaces
+
+const updateUnsubscribedSpaces = async () => {
+  state.isLoading = true
+  state.unsubscribedSpaces = await store.dispatch('api/getSpacesNotificationUnsubscribed')
+  state.isLoading = false
+}
+const resubscribeToSpace = (space) => {
+  state.unsubscribedSpaces = state.unsubscribedSpaces.filter(item => item.id !== space.id)
+  store.dispatch('api/spaceNotificationResubscribe', space)
+  store.commit('addNotification', { message: `Resubscribed to notifications from ${space.name}`, type: 'success' })
+}
+const changeSpace = (space) => {
+  store.dispatch('currentSpace/changeSpace', space)
+}
+</script>
+
 <template lang="pug">
-dialog.narrow.notification-settings(v-if="visible" :open="visible" @click.left.stop ref="dialog" :style="{'max-height': dialogHeight + 'px'}")
+dialog.narrow.notification-settings(v-if="props.visible" :open="props.visible" @click.left.stop ref="dialogElement" :style="{'max-height': state.dialogHeight + 'px'}")
   section
     p Notifications
   template(v-if="!currentUserIsSignedIn")
@@ -26,12 +107,12 @@ dialog.narrow.notification-settings(v-if="visible" :open="visible" @click.left.s
         label(:class="{active: shouldEmailNotifications}" @click.left.prevent="toggleShouldEmailNotifications" @keydown.stop.enter="toggleShouldEmailNotifications")
           input(type="checkbox" v-model="shouldEmailNotifications")
           span Email Notifications
-      Loader(:visible="isLoading")
+      Loader(:visible="state.isLoading")
 
-      template(v-if="unsubscribedSpaces.length")
+      template(v-if="state.unsubscribedSpaces.length")
         .row Resubscribe to:
         SpaceList(
-          :spaces="unsubscribedSpaces"
+          :spaces="state.unsubscribedSpaces"
           :showUser="true"
           @selectSpace="changeSpace"
           :showCheckmarkSpace="true"
@@ -39,89 +120,7 @@ dialog.narrow.notification-settings(v-if="visible" :open="visible" @click.left.s
           :disableListOptimizations="true"
           :parentDialog="parentDialog"
         )
-
 </template>
-
-<script>
-import Loader from '@/components/Loader.vue'
-import SpaceList from '@/components/SpaceList.vue'
-import utils from '@/utils.js'
-
-export default {
-  name: 'NotificationSettings',
-  components: {
-    Loader,
-    SpaceList
-  },
-  props: {
-    visible: Boolean
-  },
-  data () {
-    return {
-      dialogHeight: null,
-      unsubscribedSpaces: [],
-      isLoading: true
-    }
-  },
-  created () {
-    window.addEventListener('resize', this.updateDialogHeight)
-  },
-  computed: {
-    isSignedIn () { return this.$store.getters['currentUser/isSignedIn'] },
-    currentUserIsSignedIn () { return this.$store.getters['currentUser/isSignedIn'] },
-    shouldEmailNotifications () { return this.$store.state.currentUser.shouldEmailNotifications },
-    shouldEmailBulletin () { return this.$store.state.currentUser.shouldEmailBulletin },
-    shouldEmailWeeklyReview () { return this.$store.state.currentUser.shouldEmailWeeklyReview },
-    parentDialog () { return 'notificationSettings' }
-  },
-  methods: {
-    toggleShouldEmailNotifications () {
-      const value = !this.shouldEmailNotifications
-      this.$store.dispatch('currentUser/shouldEmailNotifications', value)
-    },
-    toggleShouldEmailBulletin () {
-      const value = !this.shouldEmailBulletin
-      this.$store.dispatch('currentUser/shouldEmailBulletin', value)
-    },
-    toggleShouldEmailWeeklyReview () {
-      const value = !this.shouldEmailWeeklyReview
-      this.$store.dispatch('currentUser/shouldEmailWeeklyReview', value)
-    },
-    triggerSignUpOrInIsVisible () {
-      this.$store.dispatch('closeAllDialogs')
-      this.$store.commit('triggerSignUpOrInIsVisible')
-    },
-    updateDialogHeight () {
-      if (!this.visible) { return }
-      this.$nextTick(() => {
-        let element = this.$refs.dialog
-        this.dialogHeight = utils.elementHeight(element)
-      })
-    },
-    async updateUnsubscribedSpaces () {
-      this.isLoading = true
-      this.unsubscribedSpaces = await this.$store.dispatch('api/getSpacesNotificationUnsubscribed')
-      this.isLoading = false
-    },
-    resubscribeToSpace (space) {
-      this.unsubscribedSpaces = this.unsubscribedSpaces.filter(item => item.id !== space.id)
-      this.$store.dispatch('api/spaceNotificationResubscribe', space)
-      this.$store.commit('addNotification', { message: `Resubscribed to notifications from ${space.name}`, type: 'success' })
-    },
-    changeSpace (space) {
-      this.$store.dispatch('currentSpace/changeSpace', space)
-    }
-  },
-  watch: {
-    visible (visible) {
-      if (visible) {
-        this.updateDialogHeight()
-        this.updateUnsubscribedSpaces()
-      }
-    }
-  }
-}
-</script>
 
 <style lang="stylus">
 .notification-settings
