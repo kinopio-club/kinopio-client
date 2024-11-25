8158d215ed45bc8ce2c2abd447d8d015b9e15f32
Pirijan <hi@pirijan.com>
2024-11-25 14:32:46 -0500

    og:image save as jpeg

--- src/pageMeta.js
+++ src/pageMeta.js
@@ -33,7 +33,7 @@ const fetchGroupPublicMeta = async (groupId) => {
 }
 const spacePreviewImageFromId = (spaceId) => {
   if (!spaceId) { return '' }
-  return `${consts.cdnHost}/${spaceId}/preview-image-${spaceId}.jpg`
+  return `${consts.cdnHost}/${spaceId}/preview-image-${spaceId}.jpeg`
 }
 const windowSpaceTitle = (space) => {
   let title
