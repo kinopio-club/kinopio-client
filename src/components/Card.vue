1a8e816ae2a8afae92e040f92471c4f6eba73584
Pirijan <hi@pirijan.com>
2024-12-28 13:47:55 -0500

    only use safe color if card is selected

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -416,7 +416,7 @@ const cardStyle = computed(() => {
   if (props.card.tilt) {
     styles.transform = `rotate(${props.card.tilt}deg)`
   }
-  if (isImageCard.value) {
+  if (isImageCard.value && isSelectedOrDragging.value) {
     color = safeColor(color)
     styles.background = color
   }
