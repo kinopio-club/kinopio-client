79d28aa317172a9498b649267fb2f34effb0034f
pirijan <hi@pirijan.com>
2025-01-24 16:20:35 -0500

    privacy copy tweaks

--- src/data/privacy.js
+++ src/data/privacy.js
@@ -5,8 +5,8 @@ export default {
       {
         name: 'open',
         friendlyName: 'Open to Comments',
-        description: 'Anyone can view and add comments, only collaborators can edit',
-        descriptionGroup: 'Anyone can view and add comments, only group members and collaborators can edit',
+        description: 'Anyone can view and add comments. Only collaborators can edit.',
+        descriptionGroup: 'Anyone can view and add comments. Only group members and collaborators can edit.',
         color: 'success',
         icon: 'comment'
       },
@@ -14,16 +14,16 @@ export default {
         name: 'closed',
         shortName: 'public',
         friendlyName: 'Public Read Only',
-        description: 'Anyone can view, only collaborators can edit',
-        descriptionGroup: 'Anyone can view, only group members and collaborators can edit',
+        description: 'Anyone can view. Only collaborators can edit.',
+        descriptionGroup: 'Anyone can view. Only group members and collaborators can edit.',
         color: 'info',
         icon: 'unlock'
       },
       {
         name: 'private',
         friendlyName: 'Private',
-        description: 'Only collaborators can view and edit',
-        descriptionGroup: 'Only group members and collaborators can view and edit',
+        description: 'Only collaborators can view and edit.',
+        descriptionGroup: 'Only group members and collaborators can view and edit.',
         color: 'danger',
         icon: 'lock'
       }
