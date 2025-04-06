4768850503adf1cd3521620d4eec940b28cdbbb0
Pirijan <hi@pirijan.com>
2025-04-06 11:11:23 -0400

    share readonly: show space group name

--- src/components/dialogs/Share.vue
+++ src/components/dialogs/Share.vue
@@ -177,7 +177,7 @@ dialog.share.wide(v-if="props.visible" :open="props.visible" @click.left.stop="c
           RssFeeds(:visible="state.rssFeedsIsVisible")
 
   section(v-if="spaceIsRemote")
-    ReadOnlySpaceInfoBadges
+    ReadOnlySpaceInfoBadges(:spaceGroup="spaceGroup")
     PrivacyButton(:privacyPickerIsVisible="state.privacyPickerIsVisible" @togglePrivacyPickerIsVisible="togglePrivacyPickerIsVisible" @closeDialogs="closeDialogs")
 
     //- Copy URL
