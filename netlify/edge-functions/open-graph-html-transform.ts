a7683232895801bde47a92cb6df31dec52869d39
Lucas Fischer <lucas.fischer@me.com>
2025-04-08 17:48:52 +0200

    add Netlify edge function to transform Open Graph meta tags

---
+++ netlify/edge-functions/open-graph-html-transform.ts
@@ -0,0 +1,48 @@
+import { Context } from "@netlify/edge-functions";
+import { HTMLRewriter } from "https://ghuc.cc/worker-tools/html-rewriter/index.ts";
+
+const cdnHost = "https://cdn.kinopio.club";
+
+function getSpaceOgImage(spaceId: string) {
+  return `${cdnHost}/${spaceId}/preview-image-${spaceId}.webp`;
+}
+
+export default async function handler(request: Request, context: Context) {
+  const url = new URL(request.url);
+
+  // Assets
+  if (url.pathname.includes(".")) {
+    return;
+  }
+
+  if (url.pathname === '/') {
+    return;
+  }
+
+  const spaceId = url.pathname;
+
+  const response = await context.next();
+  const rewriter = new HTMLRewriter()
+    .on('meta[property="og:image"]', {
+      element: (element) => {
+        element.setAttribute("content", getSpaceOgImage(spaceId));
+      },
+    })
+    .on('meta[property="og:image:width"]', {
+      element: (element) => {
+        element.setAttribute("content", "1180");
+      },
+    })
+    .on('meta[property="og:image:height"]', {
+      element: (element) => {
+        element.setAttribute("content", "670");
+      },
+    })
+    .on('meta[property="og:image:type"]', {
+      element: (element) => {
+        element.setAttribute("content", "image/jpeg");
+      },
+    });
+
+  return rewriter.transform(response);
+}
