436c4c18dfddb7f041218471b9880acd2fed2e63
pirijan <hi@pirijan.com>
2025-04-14 16:17:45 -0400

    prevent initial outsidecolor from being black

--- src/components/OutsideSpaceBackground.vue
+++ src/components/OutsideSpaceBackground.vue
@@ -109,7 +109,7 @@ const updateBackgroundColor = () => {
     backgroundColor = utils.cssVariable('secondary-active-background')
   }
   // darken
-  let darkness = 0.6
+  let darkness = 0.4
   if (isThemeDark.value) {
     darkness = 0.8
   }
