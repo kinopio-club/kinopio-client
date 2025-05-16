a82af04e652c557b0fae7bc745cad46dc3b168f4
pirijan <hi@pirijan.com>
2025-05-16 19:47:50 -0400

    asktoaddtoexplore -> composition and userstore

--- src/components/AskToAddToExplore.vue
+++ src/components/AskToAddToExplore.vue
@@ -1,10 +1,50 @@
+<script setup>
+import { reactive, computed, onMounted, onBeforeUnmount, watch, ref, nextTick } from 'vue'
+import { useStore } from 'vuex'
+import { useUserStore } from '@/stores/useUserStore'
+
+const userStore = useUserStore()
+const store = useStore()
+
+const emit = defineEmits(['updateDialogHeight'])
+
+const state = reactive({
+  error: {
+    userNeedsToSignUpOrIn: false
+  },
+  isAsked: false
+})
+
+const showInExplore = computed(() => userStore.showInExplore)
+const currentUserIsSignedIn = computed(() => userStore.getUserIsSignedIn)
+const isSpaceMember = computed(() => userStore.getUserIsSpaceMember())
+const isVisible = computed(() => {
+  return !isSpaceMember.value && !showInExplore.value
+})
+
+const askToAddToExplore = () => {
+  if (state.isAsked) { return }
+  if (!currentUserIsSignedIn.value) {
+    state.error.userNeedsToSignUpOrIn = true
+    return
+  }
+  store.dispatch('userNotifications/addAskToAddToExplore')
+  state.isAsked = true
+  emit('updateDialogHeight')
+}
+const triggerSignUpOrInIsVisible = () => {
+  store.dispatch('closeAllDialogs')
+  store.commit('triggerSignUpOrInIsVisible')
+}
+</script>
+
 <template lang="pug">
 .button-wrap.ask-to-add-to-explore(v-if="isVisible")
-  template(v-if="!isAsked")
+  template(v-if="!state.isAsked")
     button(@click.left.prevent="askToAddToExplore" @keydown.stop.enter="askToAddToExplore")
       img.icon.sunglasses(src="@/assets/sunglasses.svg")
       span Ask to Add to Explore
-    template(v-if="error.userNeedsToSignUpOrIn")
+    template(v-if="state.error.userNeedsToSignUpOrIn")
       p
         span.badge.info Sign Up or In
         span to propose spaces to be added to explore
@@ -12,47 +52,7 @@
 
   template(v-else)
     .badge.success Space Creator Notified – Thank you for sharing the love
-
 </template>
-
-<script>
-export default {
-  name: 'AskToAddToExploreButton',
-  emits: ['updateDialogHeight'],
-  data () {
-    return {
-      error: {
-        userNeedsToSignUpOrIn: false
-      },
-      isAsked: false
-    }
-  },
-  computed: {
-    showInExplore () { return this.$store.state.currentSpace.showInExplore },
-    currentUserIsSignedIn () { return this.$store.getters['currentUser/isSignedIn'] },
-    isSpaceMember () { return this.$store.getters['currentUser/isSpaceMember']() },
-    isVisible () {
-      return !this.isSpaceMember && !this.showInExplore
-    }
-  },
-  methods: {
-    askToAddToExplore () {
-      if (this.isAsked) { return }
-      if (!this.currentUserIsSignedIn) {
-        this.error.userNeedsToSignUpOrIn = true
-        return
-      }
-      this.$store.dispatch('userNotifications/addAskToAddToExplore')
-      this.isAsked = true
-      this.$emit('updateDialogHeight')
-    },
-    triggerSignUpOrInIsVisible () {
-      this.$store.dispatch('closeAllDialogs')
-      this.$store.commit('triggerSignUpOrInIsVisible')
-    }
-  }
-}
-</script>
 
 <style lang="stylus">
 .ask-to-add-to-explore
