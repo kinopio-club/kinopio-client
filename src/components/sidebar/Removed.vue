e728a59f372d0a6ff9ee28f1b7f9d7e2e6368faf
Pirijan <hi@pirijan.com>
2025-06-18 13:55:50 -0400

    on undelete card, highlight it and scroll into view

--- src/components/sidebar/Removed.vue
+++ src/components/sidebar/Removed.vue
@@ -156,6 +156,7 @@ const restoreCard = async (card) => {
   cardStore.restoreRemovedCard(card)
   await nextTick()
   scrollIntoView(card)
+  globalStore.updateFocusOnCardId(card.id)
   removeRemovedCard(card)
 }
 const deleteCard = (card) => {
