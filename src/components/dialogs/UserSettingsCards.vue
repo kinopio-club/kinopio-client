3446ea2ce445c0004304d2260efcd1439560d444
pirijan <hi@pirijan.com>
2025-07-17 10:28:34 -0400

    cardtip settings dialog inline

--- src/components/dialogs/UserSettingsCards.vue
+++ src/components/dialogs/UserSettingsCards.vue
@@ -21,10 +21,12 @@ const props = defineProps({
 </script>
 
 <template lang="pug">
-dialog.narrow.dialog-name(v-if="props.visible" :open="props.visible" @click.left.stop ref="dialogElement")
+dialog.narrow.cards-settings-dialog(v-if="props.visible" :open="props.visible" @click.left.stop ref="dialogElement")
   UserSettingsCards(:visible="props.visible")
 </template>
 
 <style lang="stylus">
-// dialog.cards-settings
+.cards-settings-dialog
+  &.narrow
+    width 200px
 </style>
