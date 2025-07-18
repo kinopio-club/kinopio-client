cf69a56e3f40a9cb2c53cb814d458fac020af263
Lucas Fischer <lucas.fischer@me.com>
2025-07-18 15:56:07 +0800

    revert router.js

--- src/router.js
+++ src/router.js
@@ -23,12 +23,10 @@ const router = {
       // which is lazy-loaded when the route is visited.
       component: () => import('./views/Add.vue'),
       beforeEnter: (to, from, next) => {
-        if (!import.meta.env.SSR) {
-          const globalStore = useGlobalStore()
-          window.document.title = 'Add Card'
-          const urlParams = new URLSearchParams(window.location.search)
-          globalStore.isAddPage = true
-        }
+        const globalStore = useGlobalStore()
+        window.document.title = 'Add Card'
+        const urlParams = new URLSearchParams(window.location.search)
+        globalStore.isAddPage = true
         next()
       }
     }, {
@@ -40,11 +38,9 @@ const router = {
       name: 'space',
       component: Space,
       beforeEnter: (to, from, next) => {
-        if (!import.meta.env.SSR) {
-          const globalStore = useGlobalStore()
-          const urlParams = new URLSearchParams(window.location.search)
-          globalStore.disableViewportOptimizations = urlParams.get('disableViewportOptimizations')
-        }
+        const globalStore = useGlobalStore()
+        const urlParams = new URLSearchParams(window.location.search)
+        globalStore.disableViewportOptimizations = urlParams.get('disableViewportOptimizations')
         next()
       }
     }, {
@@ -52,11 +48,9 @@ const router = {
       name: 'beta',
       component: Space,
       beforeEnter: (to, from, next) => {
-        if (!import.meta.env.SSR) {
-          const globalStore = useGlobalStore()
-          globalStore.isBeta = true
-          globalStore.addNotification({ message: 'No features currently in Beta' }) // 'No features currently in Beta'
-        }
+        const globalStore = useGlobalStore()
+        globalStore.isBeta = true
+        globalStore.addNotification({ message: 'No features currently in Beta' }) // 'No features currently in Beta'
         next()
       }
     }, {
@@ -64,12 +58,10 @@ const router = {
       name: 'confirm-email',
       component: Space,
       redirect: to => {
-        if (!import.meta.env.SSR) {
-          const globalStore = useGlobalStore()
-          const userStore = useUserStore()
-          userStore.updateUserEmailIsVerified()
-          globalStore.addNotification({ message: 'Email Confirmed', type: 'success' })
-        }
+        const globalStore = useGlobalStore()
+        const userStore = useUserStore()
+        userStore.updateUserEmailIsVerified()
+        globalStore.addNotification({ message: 'Email Confirmed', type: 'success' })
         return '/'
       }
     }, {
@@ -77,207 +69,213 @@ const router = {
       name: 'reset-password',
       component: Space,
       beforeEnter: (to, from, next) => {
-        if (!import.meta.env.SSR) {
-          const globalStore = useGlobalStore()
-          const urlParams = new URLSearchParams(window.location.search)
-          const apiKey = urlParams.get('apiKey')
-          if (apiKey) {
-            globalStore.updatePasswordApiKey = apiKey
-            globalStore.passwordResetIsVisible = true
-          }
-          history.replaceState({}, document.title, window.location.origin)
+        const globalStore = useGlobalStore()
+        const urlParams = new URLSearchParams(window.location.search)
+        const apiKey = urlParams.get('apiKey')
+        if (apiKey) {
+          globalStore.updatePasswordApiKey = apiKey
+          globalStore.passwordResetIsVisible = true
         }
+        history.replaceState({}, document.title, window.location.origin)
         next()
       }
     }, {
       path: '/update-arena-access-token',
       name: 'update-arena-access-token',
-      component: Space
-      // beforeEnter: (to, from, next) => {
-      //   const urlParams = new URLSearchParams(window.location.search)
-      //   const arenaReturnedCode = urlParams.get('code')
-      //   next()
-      //   history.replaceState({}, document.title, window.location.origin)
-      //   const userStore = useUserStore()
-      //   userStore.updateUserArenaAccessToken(arenaReturnedCode)
-      // }
+      component: Space,
+      beforeEnter: (to, from, next) => {
+        const urlParams = new URLSearchParams(window.location.search)
+        const arenaReturnedCode = urlParams.get('code')
+        next()
+        history.replaceState({}, document.title, window.location.origin)
+        const userStore = useUserStore()
+        userStore.updateUserArenaAccessToken(arenaReturnedCode)
+      }
     }, {
       path: '/explore',
-      component: Space
-      // beforeEnter: (to, from, next) => {
-      //   const globalStore = useGlobalStore()
-      //   globalStore.shouldShowExploreOnLoad = true
-      //   next()
-      // }
+      component: Space,
+      beforeEnter: (to, from, next) => {
+        const globalStore = useGlobalStore()
+        globalStore.shouldShowExploreOnLoad = true
+        next()
+      }
     }, {
       path: '/new',
-      component: Space
-      // beforeEnter: (to, from, next) => {
-      //   const globalStore = useGlobalStore()
-      //   globalStore.loadNewSpace = true
-      //   next()
-      // }
+      component: Space,
+      beforeEnter: (to, from, next) => {
+        const globalStore = useGlobalStore()
+        globalStore.loadNewSpace = true
+        next()
+      }
+    }, {
+      path: '/inbox',
+      component: Space,
+      beforeEnter: (to, from, next) => {
+        const globalStore = useGlobalStore()
+        globalStore.loadInboxSpace = true
+        next()
+      }
     }, {
       path: '/:space/:card',
-      component: Space
-      // beforeEnter: (to, from, next) => {
-      //   const globalStore = useGlobalStore()
-      //   const path = window.location.pathname
-      //   const urlParams = new URLSearchParams(window.location.search)
-      //   if (urlParams.get('present')) {
-      //     globalStore.isPresentationMode = true
-      //   }
-      //   if (urlParams.get('comment')) {
-      //     globalStore.isCommentMode = true
-      //   }
-      //   globalStore.updateSpaceAndCardUrlToLoad(path)
-      //   next()
-      // }
+      component: Space,
+      beforeEnter: (to, from, next) => {
+        const globalStore = useGlobalStore()
+        const path = window.location.pathname
+        const urlParams = new URLSearchParams(window.location.search)
+        if (urlParams.get('present')) {
+          globalStore.isPresentationMode = true
+        }
+        if (urlParams.get('comment')) {
+          globalStore.isCommentMode = true
+        }
+        globalStore.updateSpaceAndCardUrlToLoad(path)
+        next()
+      }
     }, {
       path: '/embed',
-      component: Space
-      // beforeEnter: (to, from, next) => {
-      //   const globalStore = useGlobalStore()
-      //   const urlParams = new URLSearchParams(window.location.search)
-      //   const spaceId = urlParams.get('spaceId')
-      //   const zoomLimit = {
-      //     min: 40,
-      //     max: 100
-      //   }
-      //   let zoom = urlParams.get('zoom')
-      //   zoom = Math.max(zoomLimit.min, zoom)
-      //   zoom = Math.min(zoomLimit.max, zoom)
-      //   globalStore.spaceUrlToLoad = spaceId
-      //   globalStore.spaceZoomPercent = zoom
-      //   globalStore.isEmbedMode = true
-      //   next()
-      // }
+      component: Space,
+      beforeEnter: (to, from, next) => {
+        const globalStore = useGlobalStore()
+        const urlParams = new URLSearchParams(window.location.search)
+        const spaceId = urlParams.get('spaceId')
+        const zoomLimit = {
+          min: 40,
+          max: 100
+        }
+        let zoom = urlParams.get('zoom')
+        zoom = Math.max(zoomLimit.min, zoom)
+        zoom = Math.min(zoomLimit.max, zoom)
+        globalStore.spaceUrlToLoad = spaceId
+        globalStore.spaceZoomPercent = zoom
+        globalStore.isEmbedMode = true
+        next()
+      }
     }, {
       path: '/donation-success',
       name: 'donation-success',
-      component: Space
-      // beforeEnter: (to, from, next) => {
-      //   const globalStore = useGlobalStore()
-      //   globalStore.notifyThanksForDonating = true
-      //   next()
-      // }
+      component: Space,
+      beforeEnter: (to, from, next) => {
+        const globalStore = useGlobalStore()
+        globalStore.notifyThanksForDonating = true
+        next()
+      }
     }, {
       path: '/subscription-success',
       name: 'subscription-success',
-      component: Space
-      // beforeEnter: (to, from, next) => {
-      //   const globalStore = useGlobalStore()
-      //   const urlParams = new URLSearchParams(window.location.search)
-      //   const sessionId = urlParams.get('sessionId')
-      //   if (sessionId) {
-      //     globalStore.notifyThanksForUpgrading = true
-      //   }
-      //   next()
-      // }
+      component: Space,
+      beforeEnter: (to, from, next) => {
+        const globalStore = useGlobalStore()
+        const urlParams = new URLSearchParams(window.location.search)
+        const sessionId = urlParams.get('sessionId')
+        if (sessionId) {
+          globalStore.notifyThanksForUpgrading = true
+        }
+        next()
+      }
     }, {
       path: '/group/invite',
       name: 'groupInvite',
-      component: Space
-      // beforeEnter: (to, from, next) => {
-      //   const globalStore = useGlobalStore()
-      //   const urlParams = new URLSearchParams(window.location.search)
-      //   const groupId = urlParams.get('groupId')
-      //   const collaboratorKey = urlParams.get('collaboratorKey')
-      //   globalStore.groupToJoinOnLoad = { groupId, collaboratorKey }
-      //   globalStore.shouldNotifyIsJoiningGroup = true
-      //   next()
-      // }
+      component: Space,
+      beforeEnter: (to, from, next) => {
+        const globalStore = useGlobalStore()
+        const urlParams = new URLSearchParams(window.location.search)
+        const groupId = urlParams.get('groupId')
+        const collaboratorKey = urlParams.get('collaboratorKey')
+        globalStore.groupToJoinOnLoad = { groupId, collaboratorKey }
+        globalStore.shouldNotifyIsJoiningGroup = true
+        next()
+      }
     }, {
       path: '/invite',
       name: 'invite',
-      component: Space
-      // beforeEnter: (to, from, next) => {
-      //   const globalStore = useGlobalStore()
-      //   const userStore = useUserStore()
-      //   const urlParams = new URLSearchParams(window.location.search)
-      //   if (urlParams.get('present')) {
-      //     globalStore.isPresentationMode = true
-      //   }
-      //   if (urlParams.get('comment')) {
-      //     globalStore.isCommentMode = true
-      //   }
-      //   const spaceId = urlParams.get('spaceId')
-      //   const collaboratorKey = urlParams.get('collaboratorKey')
-      //   const readOnlyKey = urlParams.get('readOnlyKey')
-      //   const isPresentationMode = urlParams.get('present') || false
-      //   const isDisableViewportOptimizations = Boolean(urlParams.get('disableViewportOptimizations'))
-      //   globalStore.disableViewportOptimizations = isDisableViewportOptimizations
-      //   userStore.initializeUser()
-      //   globalStore.isLoadingSpace = true
-      //   if (!spaceId) {
-      //     globalStore.addNotification({ message: 'Invalid invite URL', type: 'danger' })
-      //     next()
-      //     return
-      //   }
-      //   globalStore.isPresentationMode = isPresentationMode
-      //   // edit
-      //   if (collaboratorKey) {
-      //     inviteToEdit({ next, spaceId, collaboratorKey })
-      //   // read only
-      //   } else if (readOnlyKey) {
-      //     inviteToReadOnly({ next, spaceId, readOnlyKey })
-      //   // error
-      //   } else {
-      //     globalStore.addNotification({ message: 'Invalid invite URL', type: 'danger' })
-      //     next()
-      //   }
-      // }
+      component: Space,
+      beforeEnter: (to, from, next) => {
+        const globalStore = useGlobalStore()
+        const userStore = useUserStore()
+        const urlParams = new URLSearchParams(window.location.search)
+        if (urlParams.get('present')) {
+          globalStore.isPresentationMode = true
+        }
+        if (urlParams.get('comment')) {
+          globalStore.isCommentMode = true
+        }
+        const spaceId = urlParams.get('spaceId')
+        const collaboratorKey = urlParams.get('collaboratorKey')
+        const readOnlyKey = urlParams.get('readOnlyKey')
+        const isPresentationMode = urlParams.get('present') || false
+        const isDisableViewportOptimizations = Boolean(urlParams.get('disableViewportOptimizations'))
+        globalStore.disableViewportOptimizations = isDisableViewportOptimizations
+        userStore.initializeUser()
+        globalStore.isLoadingSpace = true
+        if (!spaceId) {
+          globalStore.addNotification({ message: 'Invalid invite URL', type: 'danger' })
+          next()
+          return
+        }
+        globalStore.isPresentationMode = isPresentationMode
+        // edit
+        if (collaboratorKey) {
+          inviteToEdit({ next, spaceId, collaboratorKey })
+        // read only
+        } else if (readOnlyKey) {
+          inviteToReadOnly({ next, spaceId, readOnlyKey })
+        // error
+        } else {
+          globalStore.addNotification({ message: 'Invalid invite URL', type: 'danger' })
+          next()
+        }
+      }
     }, {
       path: '/:space',
-      component: Space
-      // beforeEnter: (to, from, next) => {
-      //   const globalStore = useGlobalStore()
-      //   const path = window.location.pathname
-      //   const urlParams = new URLSearchParams(window.location.search)
-      //   if (urlParams.get('present')) {
-      //     globalStore.isPresentationMode = true
-      //   }
-      //   globalStore.updateSpaceAndCardUrlToLoad(path)
-      //   next()
-      // }
+      component: Space,
+      beforeEnter: (to, from, next) => {
+        const globalStore = useGlobalStore()
+        const path = window.location.pathname
+        const urlParams = new URLSearchParams(window.location.search)
+        if (urlParams.get('present')) {
+          globalStore.isPresentationMode = true
+        }
+        globalStore.updateSpaceAndCardUrlToLoad(path)
+        next()
+      }
     }
   ]
 }
 
 export default router
 
-// const inviteToEdit = async ({ next, spaceId, collaboratorKey }) => {
-//   const globalStore = useGlobalStore()
-//   const userStore = useUserStore()
-//   const apiStore = useApiStore()
-//   const apiKey = userStore.apiKey
-//   if (!apiKey) {
-//     globalStore.spaceUrlToLoad = spaceId
-//     globalStore.addToSpaceCollaboratorKeys({ spaceId, collaboratorKey })
-//     next()
-//     return
-//   }
-//   // join
-//   try {
-//     await apiStore.addSpaceCollaborator({ spaceId, collaboratorKey })
-//     globalStore.spaceUrlToLoad = spaceId
-//     globalStore.addNotification({ message: 'You can now edit this space', type: 'success' })
-//     globalStore.addToSpaceCollaboratorKeys({ spaceId, collaboratorKey })
-//   } catch (error) {
-//     console.error('🚒 inviteToEdit', error)
-//     if (error.status === 401) {
-//       globalStore.addNotification({ message: 'Space could not be found, or your invite was invalid', type: 'danger' })
-//     } else {
-//       globalStore.addNotification({ message: '(シ_ _)シ Something went wrong, Please try again or contact support', type: 'danger' })
-//     }
-//   }
-//   // load
-//   next()
-// }
+const inviteToEdit = async ({ next, spaceId, collaboratorKey }) => {
+  const globalStore = useGlobalStore()
+  const userStore = useUserStore()
+  const apiStore = useApiStore()
+  const apiKey = userStore.apiKey
+  if (!apiKey) {
+    globalStore.spaceUrlToLoad = spaceId
+    globalStore.addToSpaceCollaboratorKeys({ spaceId, collaboratorKey })
+    next()
+    return
+  }
+  // join
+  try {
+    await apiStore.addSpaceCollaborator({ spaceId, collaboratorKey })
+    globalStore.spaceUrlToLoad = spaceId
+    globalStore.addNotification({ message: 'You can now edit this space', type: 'success' })
+    globalStore.addToSpaceCollaboratorKeys({ spaceId, collaboratorKey })
+  } catch (error) {
+    console.error('🚒 inviteToEdit', error)
+    if (error.status === 401) {
+      globalStore.addNotification({ message: 'Space could not be found, or your invite was invalid', type: 'danger' })
+    } else {
+      globalStore.addNotification({ message: '(シ_ _)シ Something went wrong, Please try again or contact support', type: 'danger' })
+    }
+  }
+  // load
+  next()
+}
 
-// const inviteToReadOnly = ({ next, spaceId, readOnlyKey }) => {
-//   const globalStore = useGlobalStore()
-//   globalStore.spaceUrlToLoad = spaceId
-//   globalStore.spaceReadOnlyKey = { spaceId, key: readOnlyKey }
-//   next()
-// }
+const inviteToReadOnly = ({ next, spaceId, readOnlyKey }) => {
+  const globalStore = useGlobalStore()
+  globalStore.spaceUrlToLoad = spaceId
+  globalStore.spaceReadOnlyKey = { spaceId, key: readOnlyKey }
+  next()
+}
