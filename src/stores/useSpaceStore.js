05835745eb693e823691c7be05d9d187e111dc17
pirijan <hi@pirijan.com>
2025-08-20 13:25:25 -0400

    noop debug logs

--- src/stores/useSpaceStore.js
+++ src/stores/useSpaceStore.js
@@ -216,6 +216,9 @@ export const useSpaceStore = defineStore('space', {
       return value
     },
     updateItemWithItemType (item) {
+      console.log('🌷🌷🌷🌷', item.id, item)
+
+      if (!item) { return }
       const cardStore = useCardStore()
       const boxStore = useBoxStore()
       const card = cardStore.getCard(item.id)
