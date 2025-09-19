98d7b5d83036dcad45589968f1768d0ac00c8618
Pirijan <hi@pirijan.com>
2025-09-19 14:43:43 -0400

    faq copy cleanup

--- src/views/About.vue
+++ src/views/About.vue
@@ -36,9 +36,6 @@ const closeAllDialogs = () => {
 const closeDialogs = () => {
   state.appsAndExtensionsIsVisible = false
 }
-
-// page
-
 const toggleAppsAndExtensionsIsVisible = () => {
   const isVisible = state.appsAndExtensionsIsVisible
   closeAllDialogs()
@@ -64,7 +61,6 @@ const toggleAppsAndExtensionsIsVisible = () => {
 //     }
 //   ]
 // })
-
 </script>
 
 <template lang="pug">
@@ -79,7 +75,6 @@ const toggleAppsAndExtensionsIsVisible = () => {
 
       section.cta
         p Designed to work the way your mind works. No sign up required.
-          //- img.frame-image.pot-standing-side(src="@/assets/frames/lil-guys/pot-standing-side.png")
         p
           .button-wrap#download
             button.translucent-button(@click.left.stop="toggleAppsAndExtensionsIsVisible" :class="{active: state.appsAndExtensionsIsVisible}")
@@ -104,38 +99,30 @@ const toggleAppsAndExtensionsIsVisible = () => {
           img(src="https://d2w9rnfcy7mm78.cloudfront.net/7376365/original_958590f5203dbea243925880e3d09dd4.gif")
           p Gather notes, and connect them to their source URLs. Drag in files, like PDFs, to keep everything together. Categorize and label similar concepts with [[tags]].
 
-          //- video(autoplay loop muted playsinline)
-          //-   source(src="https://help.kinopio.club/assets/about/intro.mp4")
-        //- p ❶ Tap anywhere and type to add cards ❷ Drag cards to move them ❸ Drag to connect related cards together ❹ Paint select to edit together
-
-        //- p Jump in to start visually connect your thoughts together.
-        //- img(src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=276179&theme=neutral&period=daily&t=1758292855872")
-
       section.faq
         h2 FAQ
+
         WhoMakesKinopio(title="Who makes Kinopio")
-        //-
+
         details
           summary How does Kinopio compare to Miro, Milanote, Whimsical, and other whiteboard apps?
           section.subsection
             p Physical whiteboards are so productive because drawing and writing with markers is a naturally messy, chaotic process, which produces novel new ideas and shared understanding.
             p But the typical software whiteboard is designed around neatness and conformity. Expressiveness and personality is replaced with toolbars and sticky notes in one of 5 designer-approved pastel shades.
             p From customizable backgrounds, to colors, to embedding youtube videos, Kinopio spaces encourage creativity and experimentation.
-            //- TODO figure showing art
-            //- figure
-            //-   video(autoplay loop muted playsinline)
-            //-     source(src="@/assets/page/about/faq-example.mp4")
             p Like a physical whiteboard, the outputs produced this way tend to be{{' '}}
               a(href="https://uxdesign.cc/low-fidelity-design-is-higher-up-the-value-chain-fdf1824c6aa1") lower-fidelity
               span {{' '}}and conceptual – ideal for problem solving and consensus building.
             p The other major difference is that Kinopio is [organic software], paid for by the people who use it, rather than by VC investors looking for an{{' '}}
-              a(href="https://ourincrediblejourney.tumblr.com/") exit
+              a(href="https://ourincrediblejourney.tumblr.com/") easy exit
               span .
             table
               thead
                 tr
-                  th Company
-                  th VC Funding
+                  td
+                    strong Company
+                  td
+                    strong VC Funding (Debt)
               tbody
                 tr
                   td Figjam
@@ -159,35 +146,32 @@ const toggleAppsAndExtensionsIsVisible = () => {
             p To see how this plays out IRL, here's a video by a customer comparing{{' '}}
               a(href="https://www.youtube.com/watch?v=Colow9UCPqM") Kinopio vs Milanote
               span .
-        //- 3
+
         details
-          summary Why Does Kinopio Have No AI Features?
+          summary Why doesn't Kinopio Use AI?
           section.subsection
             p Because Kinopio is solely supported by the people who love it, I have the insane privilege of being able to only add features that fit into the product and truly help its users.
             p
-              span With whiteboarding, mind-mapping, or mood-boarding the journey of placing ideas and images, making connections, and figuring out what things should be grouped together yourself is vitally important to building up your{{' '}}
+              span With whiteboarding, mind-mapping, or mood-boarding, the journey of placing ideas and images, making connections, and figuring out what things should be grouped together yourself is vitally important to building up your{{' '}}
               a(href="https://en.wikipedia.org/wiki/Spatial_memory") spatial memory
               span . This is the magic that makes big ideas easier to recall and reason about.
 
-        //- 2
         details
-          summary Should I use Kinopio for work?
+          summary Why should I use Kinopio for work?
           section.subsection
             blockquote
               p There’s always been this myth that really neat, fun people at home all of a sudden get very dull and boring and serious when they come to work, and it’s simply not true.
               p – Steve Jobs
-
-            p Whether it's your favorite camera, fountain pen, or keyboard, we’re inspired by the tools we use. Well-made daily tools encourage us to do our best work.
+            p Exploring big ideas and tackling hard problems is anything but linear, it's a messy and chaotic process that uses both the creative (R) and analytical (L) sides of your brain.
+            p Using tools that embrace creativity and individuality prevents group-think and encourages collaborators to come up with new ways to solve hard problems. Features like trackable tasks and shared groups help turn brainstorming sessions into actionable project plans.
             p Kinopio is designed for nimble teams that want to build shared understanding, get projects started faster, be more flexible to change, and work better together.
-            p think of kinopio like scrap paper?
-            p groups, meetings, retrospectives ...
 
         details
           summary Who else uses Kinopio?
           section.subsection
             p
               span Over the years, it’s been amazing to hear how people use Kinopio to map out and make sense of their ideas, feelings, and plans. I recently started capturing the nice things people are saying about it on the{{' '}}
-              a(href="https://kinopio.club/love-wall-4Ry3Xwo8Giy7Jeul-s2TY") love wall
+              a(href="https://kinopio.club/love-wall-4Ry3Xwo8Giy7Jeul-s2TY") Community Love Wall
               span .
             p Used by students, teachers, and researchers, at ● NYU ● The New School (Parsons) ● Yale ● MIT ● Stanford ● Berkeley ● Columbia ●{{' '}}
                 a(href="https://x.com/sfpc/status/1597727116556390404") School for Poetic Computation
@@ -199,6 +183,9 @@ const toggleAppsAndExtensionsIsVisible = () => {
               span , and was the{{' '}}
               a(href="https://www.producthunt.com/products/kinopio") ProductHunt #1 Product of the Day
               span .
+
+      //- footer
+        //- api
 </template>
 
 <style lang="stylus">
@@ -264,5 +251,12 @@ main.page
         padding 12px !important
         padding-top 2px !important
         margin-bottom 0.5rem
+        blockquote
+          border-left 1px solid var(--tertiary-hover-background)
+          margin-left 0
+          padding-left 1rem
+        table
+          td
+            padding 5px 10px
 
 </style>
