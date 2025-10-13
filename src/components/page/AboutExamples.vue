cd1887bb2c2221989bf440dfb2863146733914f3
Pirijan <hi@pirijan.com>
2025-10-13 11:56:24 -0400

    remove unused circleprogress

--- src/components/page/AboutExamples.vue
+++ src/components/page/AboutExamples.vue
@@ -1,8 +1,6 @@
 <script setup>
 import { reactive, computed, onMounted, onBeforeUnmount, watch, ref, nextTick } from 'vue'
 
-import CircleProgress from '@/components/CircleProgress.vue'
-
 // mounted pause all videos
 const parentElement = ref(null)
 
