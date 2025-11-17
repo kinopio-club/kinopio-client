bcf29cde6f27f135501ebbce2c919749fc7f015d
Pirijan <hi@pirijan.com>
2025-11-17 10:48:17 -0500

    debounce received actions

--- src/stores/plugins/webSocketPlugin.js
+++ src/stores/plugins/webSocketPlugin.js
@@ -29,6 +29,8 @@ import debounce from 'lodash-es/debounce'
 import utils from '@/utils.js'
 import consts from '@/consts.js'
 
+const debouncedStoreActions = new Map()
+
 export default function webSocketPlugin () {
   let websocket, currentSpaceRoom, isConnected
   const clientId = nanoid()
@@ -126,6 +128,7 @@ export default function webSocketPlugin () {
     try {
       websocket.close()
       websocket = null
+      cleanupDebouncedActions()
     } catch (error) {
       console.error('🚒 closeWebsocket', error)
     }
@@ -238,6 +241,47 @@ export default function webSocketPlugin () {
     }
   }
 
+  const cleanupDebouncedActions = () => {
+    debouncedStoreActions.forEach((actionKey) => {
+      actionKey.cancel()
+    })
+    debouncedStoreActions.clear()
+  }
+  const actionDelay = (actionKey) => {
+    actionKey = actionKey.toLowerCase()
+    const criticalActionTypes = ['create', 'add', 'delete', 'remove']
+    let isCritical
+    criticalActionTypes.find(actionTypes => {
+      if (actionKey.includes(actionTypes)) {
+        isCritical = true
+        return true
+      }
+    })
+    if (isCritical) {
+      return 0 // immediate
+    } else {
+      return 16 // 60fps
+    }
+  }
+  const debouncedAction = (store, pinia, action, updates) => {
+    const userId = updates?.userId || updates?.user?.id
+    const actionKey = `${userId}:${action}`
+    if (!debouncedStoreActions.has(actionKey)) {
+      const delay = actionDelay(actionKey)
+      debouncedStoreActions.set(
+        actionKey,
+        debounce((store, pinia, action, updates) => {
+          const piniaStore = getPiniaStore(store, pinia)
+          piniaStore[action](updates)
+          checkIfShouldUpdateLinkToItem(pinia, { action, updates })
+          checkIfShouldNotifyOffscreenCardCreated(pinia, { action, updates })
+        }, delay)
+      )
+    } else {
+      const debouncedAction = debouncedStoreActions.get(actionKey)
+      debouncedAction(store, pinia, action, updates)
+    }
+  }
   const receiveMessage = (pinia, data) => {
     const globalStore = useGlobalStore(pinia)
     const spaceStore = useSpaceStore(pinia)
@@ -245,6 +289,7 @@ export default function webSocketPlugin () {
     const { message, user } = data
     const { name, updates = {}, action } = message
     updates.user = user
+    updates.isFromBroadcast = true
     const store = message.store || 'globalStore'
     const isAction = Boolean(action)
     if (name === 'connected') {
@@ -265,13 +310,10 @@ export default function webSocketPlugin () {
     } else if (name === 'updateSpaceClients') {
       spaceStore.updateSpaceClients()
     } else if (isAction) {
-      updates.isFromBroadcast = true
       const piniaStore = getPiniaStore(store, pinia)
       if (piniaStore) {
-        piniaStore[action](updates)
+        debouncedAction(store, pinia, action, updates)
       }
-      checkIfShouldUpdateLinkToItem(pinia, { action, updates })
-      checkIfShouldNotifyOffscreenCardCreated(pinia, { action, updates })
     } else {
       console.warn('🌚 unhandled message', data)
     }
