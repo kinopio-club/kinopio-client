693d54147fc6e2a07dd51f7b77a3746442b6666c
Pirijan <hi@pirijan.com>
2026-01-13 21:54:14 -0500

    stub keyboardshortshandler

--- src/components/KeyboardShortcutsHandler.vue
+++ src/components/KeyboardShortcutsHandler.vue
@@ -6,6 +6,7 @@ import { useCardStore } from '@/stores/useCardStore'
 import { useConnectionStore } from '@/stores/useConnectionStore'
 import { useBoxStore } from '@/stores/useBoxStore'
 import { useLineStore } from '@/stores/useLineStore'
+import { useListStore } from '@/stores/useListStore'
 import { useUserStore } from '@/stores/useUserStore'
 import { useSpaceStore } from '@/stores/useSpaceStore'
 import { useUploadStore } from '@/stores/useUploadStore'
@@ -22,6 +23,7 @@ const cardStore = useCardStore()
 const connectionStore = useConnectionStore()
 const boxStore = useBoxStore()
 const lineStore = useLineStore()
+const listStore = useListStore()
 const userStore = useUserStore()
 const spaceStore = useSpaceStore()
 const uploadStore = useUploadStore()
@@ -841,8 +843,12 @@ const selectAllItemsBelowCursor = (position) => {
   let lines = lineStore.getAllLines
   lines = lines.filter(line => (line.y * zoom) > position.y)
   const lineIds = lines.map(line => line.id)
+  // lists
+  let lists = listStore.getAllLists
+  lists = lists.filter(list => (list.y * zoom) > position.y)
+  const listIds = lists.map(list => list.id)
   // select
-  selectItemIds({ position, cardIds, boxIds, lineIds })
+  selectItemIds({ position, cardIds, boxIds, lineIds, listIds })
 }
 const selectAllItemsAboveCursor = (position) => {
   let zoom
@@ -865,8 +871,12 @@ const selectAllItemsAboveCursor = (position) => {
   let lines = lineStore.getAllLines
   lines = lines.filter(line => (line.y * zoom) < position.y)
   const lineIds = lines.map(line => line.id)
+  // lists
+  let lists = listStore.getAllLists
+  lists = lists.filter(list => (list.y * zoom) < position.y)
+  const listIds = lists.map(list => list.id)
   // select
-  selectItemIds({ position, cardIds, boxIds, lineIds })
+  selectItemIds({ position, cardIds, boxIds, lineIds, listIds })
 }
 const selectAllItemsRightOfCursor = (position) => {
   let zoom
@@ -887,7 +897,13 @@ const selectAllItemsRightOfCursor = (position) => {
     return (box.x * zoom) >= position.x
   })
   const boxIds = boxes.map(box => box.id)
-  selectItemIds({ position, cardIds, boxIds })
+  // lists
+  let lists = boxStore.getAllLists
+  lists = lists.filter(list => {
+    return (list.x * zoom) >= position.x
+  })
+  const listIds = lists.map(list => list.id)
+  selectItemIds({ position, cardIds, boxIds, listIds })
 }
 const selectAllItemsLeftOfCursor = (position) => {
   let zoom
@@ -908,24 +924,32 @@ const selectAllItemsLeftOfCursor = (position) => {
     return (box.x * zoom) <= position.x
   })
   const boxIds = boxes.map(box => box.id)
-  selectItemIds({ position, cardIds, boxIds })
+  // lists
+  let lists = boxStore.getAllLists
+  lists = lists.filter(list => {
+    return (list.x * zoom) <= position.x
+  })
+  const listIds = lists.map(list => list.id)
+  selectItemIds({ position, cardIds, boxIds, listIds })
 }
 
 // Select All Cards, Connections, and Boxes
 
-const selectItemIds = ({ position, cardIds, boxIds, lineIds }) => {
+const selectItemIds = ({ position, cardIds, boxIds, lineIds, listIds }) => {
   const preventMultipleSelectedActionsIsVisible = globalStore.preventMultipleSelectedActionsIsVisible
-  const isItemIds = Boolean(cardIds.length || boxIds.length || lineIds.length)
+  const isItemIds = Boolean(cardIds.length || boxIds.length || lineIds.length || listIds.length)
   if (isItemIds && preventMultipleSelectedActionsIsVisible) {
     globalStore.updateMultipleCardsSelectedIds(cardIds)
     globalStore.updateMultipleBoxesSelectedIds(boxIds)
     globalStore.updateMultipleLinesSelectedIds(lineIds)
+    globalStore.updateMultipleListsSelectedIds(listIds)
   } else if (isItemIds) {
     globalStore.multipleSelectedActionsPosition = position
     globalStore.updateMultipleSelectedActionsIsVisible(true)
     globalStore.updateMultipleCardsSelectedIds(cardIds)
     globalStore.updateMultipleBoxesSelectedIds(boxIds)
     globalStore.updateMultipleLinesSelectedIds(lineIds)
+    globalStore.updateMultipleListsSelectedIds(listIds)
   } else {
     globalStore.updateMultipleSelectedActionsIsVisible(false)
   }
