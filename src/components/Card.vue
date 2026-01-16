1e23b66b252a9b9a58e5827cbb3048b3023faebf
Pirijan <hi@pirijan.com>
2026-01-16 16:13:56 -0500

    Revert "move hover CardCommentPreview into Card , so it can be used inside list cards too"
    This reverts commit ce03a8b950a90a096ef31eb675efa43fdb33c62d.

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -32,7 +32,6 @@ import CardVote from '@/components/CardVote.vue'
 import TiltResize from '@/components/TiltResize.vue'
 import UrlPreviewCard from '@/components/UrlPreviewCard.vue'
 import ImageOrVideo from '@/components/ImageOrVideo.vue'
-import CardCommentPreview from '@/components/CardCommentPreview.vue'
 
 import dayjs from 'dayjs'
 import isToday from 'dayjs/plugin/isToday'
@@ -274,22 +273,6 @@ const removeCommentBrackets = (name) => {
   })
   return name
 }
-
-// card comment preview
-
-const cardCommentPreviewIsVisible = computed(() => {
-  if (!isComment.value) { return }
-  if (shouldPrevent.value) { return }
-  if (currentCardDetailsIsVisible.value) { return }
-  return globalStore.currentUserIsHoveringOverCardId === props.card.id
-})
-const shouldPrevent = computed(() => {
-  const isHoveringOverConnector = globalStore.currentUserIsHoveringOverConnectorItemId
-  const isHoveringOverCheckbox = globalStore.currentUserIsHoveringOverCheckboxCardId
-  const isHoveringOverLinkButton = globalStore.currentUserIsHoveringOverUrlButtonCardId
-  const isInteractingWithItem = globalStore.getIsInteractingWithItem
-  return isInteractingWithItem || isHoveringOverConnector || isHoveringOverCheckbox || isHoveringOverLinkButton
-})
 
 // checkbox
 
@@ -2228,7 +2211,7 @@ const clearFocus = () => {
     .badge.secondary.button-badge(v-if="filterShowDateUpdated" @click.left.prevent.stop="toggleFilterShowAbsoluteDates" @touchend.prevent.stop="toggleFilterShowAbsoluteDates" :class="{'date-is-today': dateIsToday}")
       img.icon.time(src="@/assets/time.svg")
       .name {{dateUpdatedAt}}
-CardCommentPreview(:visible="cardCommentPreviewIsVisible" :card="props.card")
+
 </template>
 
 <style lang="stylus">
--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -5,30 +5,55 @@ import { useGlobalStore } from '@/stores/useGlobalStore'
 import { useCardStore } from '@/stores/useCardStore'
 
 import Card from '@/components/Card.vue'
+import CardCommentPreview from '@/components/CardCommentPreview.vue'
 import ItemSnapGuide from '@/components/ItemSnapGuide.vue'
 
 const globalStore = useGlobalStore()
 const cardStore = useCardStore()
 
-const allCards = computed(() => cardStore.getAllCards)
-const lockedCards = computed(() => allCards.value.filter(card => card.isLocked))
-const cards = computed(() => {
-  return allCards.value.filter(card => {
-    return !card.isLocked && !card.listId
-  })
+const cards = computed(() => cardStore.getAllCards)
+const lockedCards = computed(() => cards.value.filter(card => card.isLocked))
+const unlockedCards = computed(() => cards.value.filter(card => !card.isLocked))
+
+// card comment preview
+
+const currentHoveredCard = computed(() => {
+  const cardId = globalStore.currentUserIsHoveringOverCardId
+  if (!cardId) { return }
+  const card = cardStore.getCard(cardId)
+  return card
+})
+const currentHoveredCardIsComment = computed(() => {
+  const card = currentHoveredCard.value
+  if (!card) { return }
+  return cardStore.getIsCommentCard(card)
+})
+const cardCommentPreviewIsVisible = computed(() => {
+  if (shouldPrevent.value) { return }
+  const cardId = globalStore.currentUserIsHoveringOverCardId
+  const cardDetailsIsVisible = cardId === globalStore.cardDetailsIsVisibleForCardId
+  if (cardDetailsIsVisible) { return }
+  return currentHoveredCardIsComment.value
+})
+const shouldPrevent = computed(() => {
+  const isHoveringOverConnector = globalStore.currentUserIsHoveringOverConnectorItemId
+  const isHoveringOverCheckbox = globalStore.currentUserIsHoveringOverCheckboxCardId
+  const isHoveringOverLinkButton = globalStore.currentUserIsHoveringOverUrlButtonCardId
+  const isInteractingWithItem = globalStore.getIsInteractingWithItem
+  return isInteractingWithItem || isHoveringOverConnector || isHoveringOverCheckbox || isHoveringOverLinkButton
 })
 </script>
 
 <template lang="pug">
 .cards
-  template(v-for="card in cards" :key="card.id")
+  template(v-for="card in unlockedCards" :key="card.id")
     Card(:card="card")
     ItemSnapGuide(:card="card")
+  CardCommentPreview(:visible="cardCommentPreviewIsVisible" :card="currentHoveredCard")
   //- locked cards rendered in ItemsLocked
   template(v-for="card in lockedCards" :key="card.id")
     teleport(to="#locked-cards")
       Card(:card="card")
-  //- list cards rendered in List
 </template>
 
 <style lang="stylus">
