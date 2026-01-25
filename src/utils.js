abbf671f0436de39bd954c5323f2787781005332
Pirijan <hi@pirijan.com>
2026-01-24 21:44:27 -0500

    Revert "add .mov video format"
    This reverts commit cc8fa0dc86191da6ee17aef007ed86cffe6f4872.

--- src/utils.js
+++ src/utils.js
@@ -2234,7 +2234,7 @@ export default {
   urlIsVideo (url) {
     if (!url) { return }
     url = url + ' '
-    const videoUrlPattern = new RegExp(/(?:\.mp4|\.webm|\.mov)(?:\n| |\?|&)/igm)
+    const videoUrlPattern = new RegExp(/(?:\.mp4|\.webm)(?:\n| |\?|&)/igm)
     const isVideo = url.match(videoUrlPattern)
     return Boolean(isVideo)
   },
