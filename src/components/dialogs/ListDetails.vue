b2ee24be53f91b8b3fb7f0ae0ff3e58c8173cada
Pirijan <hi@pirijan.com>
2026-02-05 16:03:05 -0500

    add lists to minimap

--- src/components/dialogs/ListDetails.vue
+++ src/components/dialogs/ListDetails.vue
@@ -214,7 +214,7 @@ dialog.narrow.link-details(v-if="visible" :open="visible" :style="styles" @click
       .button-wrap(v-if="canEditSpace")
         button.danger(@click.left="removeList" title="Remove List")
           img.icon(src="@/assets/remove.svg")
-    ItemDetailsDebug(:item="currentList" :keys="['y', 'height', 'resizeWidth']")
+    ItemDetailsDebug(:item="currentList" :keys="['y', 'height', 'width', 'resizeWidth']")
 </template>
 
 <style lang="stylus">
