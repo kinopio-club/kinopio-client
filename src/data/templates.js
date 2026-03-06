394e329256b7e9d0ca530cf75fc2a6e5ed52ba71
Pirijan <hi@pirijan.com>
2026-03-06 10:45:25 -0500

    add moodboard template, names

--- src/data/templates.js
+++ src/data/templates.js
@@ -28,27 +28,31 @@ export default {
   spaces () {
     return [
       {
+        name: 'Personal Dashboard',
         id: 'I4x2de1LCW2P25d_21Tsy',
         categoryId: 1
       },
       {
+        name: 'Project & Issue Tracker',
         id: 'p56fqtZUlNtFJ6td-uovI',
         categoryId: 2
       },
       {
+        name: 'Roadmap',
         id: 'J4vbhO646y4296cLRQEU3',
         categoryId: 2
       },
       {
+        name: 'Org Chart',
         id: 'HubC-58kjXefGBfyCRohk',
         categoryId: 2
       },
-
       {
-        temp: true,
+        name: 'Moodboard',
         id: 'GgDOqKvEEzTA93EYmAZrn',
-        categoryId: 3
+        categoryId: 2
       },
+
       {
         temp: true,
         id: 'eodGGinm01LcP2hfTJ4wi',
@@ -60,20 +64,15 @@ export default {
     return []
   }
   // {
-  //   id: '1t4616FBkaJOxMdrt9O_p',
-  //   categoryId: 4,
   //   name: 'Software Architecture'
+  //   id: '1t4616FBkaJOxMdrt9O_p',
+  //   categoryId: 2,
   // },
   // {
-  //   id: 'WSzpRkVqSdv8GKCSCrwrg',
-  //   categoryId: 4,
   //   name: 'Database Schema'
+  //   id: 'WSzpRkVqSdv8GKCSCrwrg',
+  //   categoryId: 2,
   // },
-  // {
-  //   id: 'J4vbhO646y4296cLRQEU3',
-  //   categoryId: 4,
-  //   name: 'Roadmap'
-  // }
 
   // 🚑 missing templates: Note Taking, Research, Journal, Making Hard Decisions, Mind Map, Weekly Planner,
   // 102293910caf3bbc3cdee8f07dd5e720ffd4e6e9
