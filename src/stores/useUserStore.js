0bb61c4be9658d671ab57d3934bf11261a65e4f5
Pirijan <hi@pirijan.com>
2026-04-09 16:35:59 -0400

    fix saving user promocode

--- src/stores/useUserStore.js
+++ src/stores/useUserStore.js
@@ -82,6 +82,7 @@ export const useUserStore = defineStore('user', {
     disabledKeyboardShortcuts: ['newSpace'],
     shouldShowMinimapJumpToList: true,
     cardDetailsResizeWidth: null,
+    affiliatePromoCode: null,
 
     // billing
 
