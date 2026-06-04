54bb3a498772c6849e5af1577e669801b5630d1e
Pirijan <hi@pirijan.com>
2026-06-04 16:04:47 -0400

    add markAllCheckboxCardsChecked, called by dev console only rn

--- src/stores/useCardStore.js
+++ src/stores/useCardStore.js
@@ -862,6 +862,16 @@ export const useCardStore = defineStore('cards', {
       await apiStore.updateCards([update])
       this.toggleCardChecked(card.id, value)
     },
+    // console utility method
+    markAllCheckboxCardsChecked () {
+      const cards = this.getAllCards
+      cards.forEach(card => {
+        const isUnchecked = card.name.startsWith('[] ')
+        if (isUnchecked) {
+          this.toggleCardChecked(card.id, true)
+        }
+      })
+    },
 
     // tilt
 
