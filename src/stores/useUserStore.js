826966e5a378e9187586b1aebecfa335c338a86e
Pirijan <hi@pirijan.com>
2026-08-06 16:32:15 -0400

    Revert "Revert "minimap is disabled by default on mobile""
    This reverts commit d01a5b366856e906dd169efdb5a25122adcdb2ad.

--- src/stores/useUserStore.js
+++ src/stores/useUserStore.js
@@ -324,6 +324,9 @@ export const useUserStore = defineStore('user', {
       console.info('🌸 Create new user')
       this.themeIsSystem = true
       this.appleAppAccountToken = self.crypto.randomUUID()
+      if (utils.isMobile()) {
+        this.shouldShowMinimap = false
+      }
       const allState = { ...this.$state }
       cache.saveUser(allState)
     },
