0819a6b7c3ac1693670c68b1d2cfa2cef337f7bb
Pirijan <hi@pirijan.com>
2026-09-04 13:04:20 -0400

    copy

--- src/components/pages/about/AboutCollaborate.vue
+++ src/components/pages/about/AboutCollaborate.vue
@@ -143,6 +143,8 @@ section.for-work
     //-   span {{' '}}projects could go straight from A to B. Every technical issue would be anticipated. Clients would never change their minds. The new feature would be as intuitive as it seemed in the mockup…
     //- p Kinopio is designed for real-world collaborative projects where teams need to be able to iterate and adapt to new information as they build.
     p Capture and connect ideas together, build them up into plans, kanban lists, and tasks assigned to team-mates. Planning and progress in the same space keeps everyone aligned.
+    p
+      em More visible projects make more effective teams.
     figure
       .button-wrap.play-button-wrap.badge.secondary(@click="toggleVideoIsPaused")
         button.small-button(title="Pause or Play Video")
