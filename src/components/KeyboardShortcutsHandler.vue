2289d48fd03a9845b66358ec76634e7569077c9a
pirijan <hi@pirijan.com>
2026-09-11 11:43:22 -0400

    copy/move shortcut

--- src/components/KeyboardShortcutsHandler.vue
+++ src/components/KeyboardShortcutsHandler.vue
@@ -830,6 +830,8 @@ const handlePasteEvent = async (event) => {
     items = utils.updateSpaceItemsAddPosition(itemsData, position)
     items = await spaceStore.getNewItems(items)
     await spaceStore.createSpaceItems(items)
+    const currentSpaceItemIds = cardStore.allIds.concat(boxStore.allIds, listStore.allIds)
+    items = utils.removeOrphanedConnections(items, currentSpaceItemIds)
     // select new items
     await nextTick()
     globalStore.closeAllDialogs()
