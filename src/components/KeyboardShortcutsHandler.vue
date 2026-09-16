364b94cec400bf1f04f1344cfb28d88dd7e3030e
Pirijan <hi@pirijan.com>
2026-09-16 13:55:19 -0400

    fix compile err

--- src/components/KeyboardShortcutsHandler.vue
+++ src/components/KeyboardShortcutsHandler.vue
@@ -832,7 +832,6 @@ const handlePasteEvent = async (event) => {
     const currentSpaceItemIds = cardStore.allIds.concat(boxStore.allIds, listStore.allIds)
     items = utils.removeOrphanedConnections(items, currentSpaceItemIds)
     await spaceStore.createSpaceItems(items)
-    const currentSpaceItemIds = cardStore.allIds.concat(boxStore.allIds, listStore.allIds)
     items = utils.removeOrphanedConnections(items, currentSpaceItemIds)
     // select new items
     await nextTick()
