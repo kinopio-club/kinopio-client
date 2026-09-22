e3a29d6ee77ee0b2526371695a1e49697ad6ffb1
Pirijan <hi@pirijan.com>
2026-09-21 20:46:24 -0400

    about copy

--- src/components/pages/about/AboutSocialProof.vue
+++ src/components/pages/about/AboutSocialProof.vue
@@ -58,10 +58,9 @@ section.social-proof
     a(href="https://appstacks.club/kinopio") App Stacks
     span , and was the{{' '}}
     a(href="https://www.producthunt.com/products/kinopio") ProductHunt #1 Product of the Day
-    span .
-  //- p But it's the{{' '}}
-  //-   a(href="https://kinopio.club/love-wall-4Ry3Xwo8Giy7Jeul-s2TY") love from customers
-  //-   span {{' '}}that means the most to me.
+    span . But it's the{{' '}}
+    a(href="https://kinopio.club/love-wall-4Ry3Xwo8Giy7Jeul-s2TY") love from customers
+    span {{' '}}that means the most to me.
 </template>
 
 <style lang="stylus">
